fce1e2214e403921d8844659216cc6b1f5e161af
thiesa <thiesa@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2004-08-31 10:24:28 +0000

    untested version of the time tracker gui viewer
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@9 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ TimeTracker/src/org/cs3/timetracker/TimeTrackerGUIViewer.java
@@ -0,0 +1,125 @@
+package org.cs3.timetracker;
+
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+
+
+/**
+ * This sample class demonstrates how to plug-in a new
+ * workbench view. The view shows data obtained from the
+ * model. The sample creates a dummy model on the fly,
+ * but a real implementation would connect to the model
+ * available either in this or another plug-in (e.g. the workspace).
+ * The view is connected to the model using a content provider.
+ * <p>
+ * The view uses a label provider to define how model
+ * objects should be presented in the view. Each
+ * view can present the same model objects using
+ * different labels and icons, if needed. Alternatively,
+ * a single label provider can be shared between views
+ * in order to ensure that objects of the same type are
+ * presented in the same way everywhere.
+ * <p>
+ */
+
+public class TimeTrackerGUIViewer extends ViewPart implements ITimeObserver{
+	private TableViewer viewer;
+
+	/*
+	 * The content provider class is responsible for
+	 * providing objects to the view. It can wrap
+	 * existing objects in adapters or simply return
+	 * objects as-is. These objects may be sensitive
+	 * to the current input of the view, or ignore
+	 * it and always show the same content 
+	 * (like Task List, for example).
+	 */
+	
+	private TimeEvent currentTimeEvent;
+	 
+	class ViewContentProvider implements IStructuredContentProvider {
+		
+		public ViewContentProvider(){			
+			System.out.println("DEBUG");
+		}
+		
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		}
+		public void dispose() {
+		}
+		public Object[] getElements(Object parent) {
+
+			String[] elements = {"--:--"};
+			
+			if(currentTimeEvent==null){return elements;}
+			
+			if(currentTimeEvent.getMinutes()==0 && currentTimeEvent.getSeconds()==0)
+				elements[0]="Time out!";
+			else elements[0] = currentTimeEvent.getFormattedString();
+			return elements;
+			
+		}
+	}
+	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
+		public String getColumnText(Object obj, int index) {
+			return getText(obj);
+		}
+		public Image getColumnImage(Object obj, int index) {
+			return getImage(obj);
+		}
+		public Image getImage(Object obj) {
+			return PlatformUI.getWorkbench().
+					getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
+		}
+	}
+
+	/**
+	 * The constructor.
+	 */
+	public TimeTrackerGUIViewer() {
+	}
+
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new ViewLabelProvider());
+		viewer.setInput(getViewSite());
+	}
+
+
+	private void showMessage(String message) {
+		MessageDialog.openInformation(
+			viewer.getControl().getShell(),
+			"Time Tracker",
+			message);
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+
+
+	public void notify(TimeEvent time) {
+		currentTimeEvent = time;
+		viewer.setInput(getViewSite());
+	}
+	
+}
---
+++ TimeTracker/src/org/cs3/timetracker/TimeTrackerGUIViewerTest.java
@@ -0,0 +1,36 @@
+/*
+ * Created on 30.08.2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.cs3.timetracker;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.internal.Workbench;
+
+import junit.framework.TestCase;
+
+/**
+ * @author thiesa
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class TimeTrackerGUIViewerTest extends TestCase {
+
+	/**
+	 * @throws PartInitException
+	 * 
+	 */
+	private void testInitialization() throws PartInitException {
+//		Workbench.getInstance().
+		TimeTrackerGUIViewer viewer = (TimeTrackerGUIViewer)TimeTrackerPlugin.getDefault()
+				.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+				.showView("org.cs3.timetracker.TimeTrackerGUIViewer");
+		
+		
+		
+	}
+}
