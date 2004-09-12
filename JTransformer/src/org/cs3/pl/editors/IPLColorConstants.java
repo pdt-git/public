ca5405b2447c4513dcb8332d5dabc7125d8e42d3
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2004-09-12 00:13:58 +0000

    fixed colors
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@55 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer/src/org/cs3/pl/editors/IPLColorConstants.java
+++ JTransformer/src/org/cs3/pl/editors/IPLColorConstants.java
@@ -3,7 +3,7 @@ package org.cs3.pl.editors;
 import org.eclipse.swt.graphics.RGB;
 
 public interface IPLColorConstants {
-	RGB PL_DYNAMIC = new RGB(100, 50, 50);
+	RGB PL_DYNAMIC = new RGB(110, 40, 40);
 	RGB PL_VAR = new RGB(0, 0, 255);
 	RGB PL_COMMENT = new RGB(80, 80,80);
 	RGB STRING = new RGB(0, 128, 0);
