595b3f588af8b3fbc52727c4f73a43e30e4bea6b
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2004-10-08 18:10:08 +0000

    *** empty log message ***
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@112 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ PDT/src/org/cs3/pdt/startup/StartUp.java
@@ -0,0 +1,15 @@
+package org.cs3.pdt.startup;
+
+import org.cs3.pl.common.Debug;
+import org.eclipse.ui.IStartup;
+
+public class StartUp implements IStartup {
+
+		/* (non-Javadoc)
+	 * @see org.eclipse.ui.IStartup#earlyStartup()
+	 */
+	public void earlyStartup() {
+		Debug.info("To early to say anything concrete...");
+	}
+
+}
