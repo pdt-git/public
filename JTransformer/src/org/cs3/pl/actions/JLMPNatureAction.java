4065d04b9d67cfccadb586c01fe7aee5f35470d1
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2004-10-10 21:37:43 +0000

    Automatic restart of prolog engine after setting the JT nature and nature removal from all other open projects where the nature was assigned. Next step: make this functionality configurable.
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@118 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer/src/org/cs3/pl/actions/JLMPNatureAction.java
+++ JTransformer/src/org/cs3/pl/actions/JLMPNatureAction.java
@@ -1,6 +1,12 @@
 package org.cs3.pl.actions;
+import java.io.IOException;
+
 import org.cs3.pl.Debug;
+import org.cs3.pl.PDTPerspective;
+import org.cs3.pl.PDTPlugin;
 import org.cs3.pl.natures.JLMPProjectNature;
+import org.cs3.pl.prolog.PrologManager;
+import org.eclipse.core.internal.resources.Workspace;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
@@ -10,6 +16,7 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.internal.Workbench;
 /**
  * Called by Eclipse to implement real-time updates and building 
  * of JLMP-Projects.
@@ -44,35 +51,77 @@ public class JLMPNatureAction implements IObjectActionDelegate {
 			return;
 		}
 		try {
-			IProjectDescription ipd = project.getDescription();
-			String[] oldNIDs = ipd.getNatureIds();
-			String[] newNIDs = null;
+			IProjectDescription ipd = project.getDescription();	
 			if (ipd.hasNature(JLMPProjectNature.NATURE_ID)) {
-				newNIDs = new String[oldNIDs.length - 1];
-				int j = 0;
-				for (int i = 0; i < newNIDs.length; i++) {
-					if (oldNIDs[j].equals(JLMPProjectNature.NATURE_ID))
-						j++;
-					newNIDs[i] = oldNIDs[j];
-					j++;
-				}
+				removeJLMPNature(project);
+				action.setChecked(false);
 			} else {
-				newNIDs = new String[oldNIDs.length + 1];
-				newNIDs[0] = JLMPProjectNature.NATURE_ID;
-				System.arraycopy(oldNIDs, 0, newNIDs, 1, oldNIDs.length);
+			    removeNatureFromAllOtherProjects();
+			    PrologManager.getInstance().restart();
+			    addJLMPNature(project);
+			    action.setChecked(true);
 			}
-			ipd.setNatureIds(newNIDs);
-			project.setDescription(ipd, null);//TODO: add real
-											  // ProgressMonitor
-											  // here!
 			action.setChecked(project.getDescription().hasNature(
 					JLMPProjectNature.NATURE_ID));
 		} catch (CoreException e) {
 			Debug.report(e);
-		}
+		} catch (IOException e) {
+			Debug.report(e);
+        }
 	}
 	
-	/* TODO CLEAN UP THIS METHOD! von ld, fuer ld :-)
+	/**
+     * @param ipd
+     * @return
+	 * @throws CoreException
+     */
+    private void addJLMPNature(IProject project) throws CoreException {
+        IProjectDescription ipd = project.getDescription();
+        String[] oldNIDs = ipd.getNatureIds();
+        String[] newNIDs = new String[oldNIDs.length + 1];
+        newNIDs[0] = JLMPProjectNature.NATURE_ID;
+        System.arraycopy(oldNIDs, 0, newNIDs, 1, oldNIDs.length);
+		ipd.setNatureIds(newNIDs);
+		project.setDescription(ipd, null);//TODO: add real
+										  // ProgressMonitor
+										  // here!
+    }
+
+    /**
+     * @param project
+     * @return
+     * @throws CoreException
+     */
+    private void removeJLMPNature(IProject project) throws CoreException {
+        if(project.hasNature(JLMPProjectNature.NATURE_ID)) {
+	        IProjectDescription ipd = project.getDescription();
+	        String[] oldNIDs = ipd.getNatureIds();
+	        String[] newNIDs;
+	        newNIDs = new String[oldNIDs.length - 1];
+	        int j = 0;
+	        for (int i = 0; i < newNIDs.length; i++) {
+	        	if (oldNIDs[j].equals(JLMPProjectNature.NATURE_ID))
+	        		j++;
+	        	newNIDs[i] = oldNIDs[j];
+	        	j++;
+	        }
+			ipd.setNatureIds(newNIDs);
+			project.setDescription(ipd, null);//TODO: add real
+        }
+    }
+
+    /**
+     * @param project2
+     * @throws CoreException
+     */
+    private void removeNatureFromAllOtherProjects() throws CoreException {
+        IProject[] projects = PDTPlugin.getDefault().getWorkspace().getRoot().getProjects();
+        for (int i = 0; i < projects.length; i++)
+            if(projects[i].isOpen()) 
+                removeJLMPNature(projects[i]);
+    }
+
+    /* TODO CLEAN UP THIS METHOD! von ld, fuer ld :-)
 	 * (non-Javadoc)
 	 * 
 	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction,
