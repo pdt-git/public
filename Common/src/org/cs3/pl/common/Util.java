1a31fb656d99da680d2aaa2663247afc6019864b
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2004-11-02 22:34:45 +0000

    ADD: getLogFile generates/returns a log file ADD: exec executes shell command and returns stderr/stdout as string tupple
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@183 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- Common/src/org/cs3/pl/common/Util.java
+++ Common/src/org/cs3/pl/common/Util.java
@@ -1,5 +1,7 @@
 package org.cs3.pl.common;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.util.Iterator;
 import java.util.Map;
@@ -45,5 +47,54 @@ public class Util {
         }
 	    sb.append("}");
 	    return sb.toString();
+	}
+	
+	public static File getLogFile(String name) throws IOException{
+	    File logFile = new File(System.getProperty("java.io.tmpdir")+File.separator+name);
+	    if (!logFile.exists()) {
+            logFile.getParentFile().mkdirs();
+            logFile.createNewFile();
+        }
+	    return logFile.getCanonicalFile();
+	}
+	
+	/**
+	 * 
+	 * @param cmd
+	 * @return an array of two strings: th 0th one contains process output, the 1th one error.
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static String[] exec(String cmd) throws IOException, InterruptedException{
+	    Process process=null;
+	    try{
+	        
+            process = Runtime.getRuntime().exec(cmd);
+	    }
+	    catch(Throwable t){
+	        Debug.report(t);
+	        return new String[]{"ERROR",""};
+	    }
+	    
+	    class _InputStreamPump extends InputStreamPump {
+	        StringBuffer sb = new StringBuffer();
+	        public _InputStreamPump(InputStream s) {
+	            super(s);        
+	        }
+	        protected void dataAvailable(char[] buffer, int length) {
+	            String string = new String(buffer,0,length);
+                sb.append(string);
+	    
+	        }
+
+	    }
+	    _InputStreamPump errPump= new _InputStreamPump(process.getErrorStream());
+	    _InputStreamPump outPump= new _InputStreamPump(process.getInputStream());
+	    errPump.start();
+	    outPump.start();
+	    process.waitFor();
+	    outPump.join();
+	    errPump.join();
+        return new String[]{outPump.sb.toString(),errPump.sb.toString()};
 	}
 }
