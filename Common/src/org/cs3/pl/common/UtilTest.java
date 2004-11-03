ad90d093c307dbd08bd098fb0ccd2c188da2f9ed
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2004-11-03 13:33:30 +0000

    *** empty log message ***
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@192 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- Common/src/org/cs3/pl/common/UtilTest.java
+++ Common/src/org/cs3/pl/common/UtilTest.java
@@ -6,6 +6,8 @@ import junit.framework.TestCase;
 
 public class UtilTest extends TestCase {
     public void testExec() throws IOException, InterruptedException {
-        assertEquals("hallo",Util.exec("echo hallo")[0].trim());
+    	String[] r = org.cs3.pl.common.Util.exec("plwin -t source_file(A),concat(X,'/plwin.rc',A),write(X),nl,flush_output. -g halt.");
+		System.out.println("'"+r[0]+"'\n'"+r[1]+"'" );
+    	assertEquals("hallo",Util.exec("echo hallo")[0].trim());
     }
 }
