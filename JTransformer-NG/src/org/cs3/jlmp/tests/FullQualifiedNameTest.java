d50908b5f4fce050f5def2e49eb6659e74c58457
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-01-12 15:56:23 +0000

    ADD: test to cover jt-107
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@281 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ JTransformer-NG/src/org/cs3/jlmp/tests/FullQualifiedNameTest.java
@@ -0,0 +1,58 @@
+/*
+ */
+package org.cs3.jlmp.tests;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.cs3.jlmp.JLMPPlugin;
+import org.cs3.pl.common.ResourceFileLocator;
+import org.cs3.pl.common.Util;
+import org.cs3.pl.prolog.PrologInterface;
+import org.cs3.pl.prolog.PrologSession;
+import org.eclipse.core.runtime.CoreException;
+
+
+/**
+ * covers jt-107
+ */
+public class FullQualifiedNameTest extends FactGenerationTest {
+    public FullQualifiedNameTest(String name) {
+        super(name);
+
+    }
+
+    public void setUp() {
+        
+        super.setUpOnce();
+    
+        ResourceFileLocator l = JLMPPlugin.getDefault().getResourceLocator("");
+        File r = l.resolve("testdata-facts.zip");
+        setTestDataLocator(JLMPPlugin.getDefault().getResourceLocator("testdata-facts"));
+        Util.unzip(r);
+        setAutoBuilding(false);
+        PrologInterface pif = getTestJLMPProject().getPrologInterface();
+        try {            
+            install("jt107");
+            pif.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }       
+    }
+    
+    public void testIt() throws CoreException {
+        build();
+        PrologSession s =getTestJLMPProject().getPrologInterface().getSession();
+        
+        List l = s.queryAll("classDefT(Cid,_,'Test',_),fullQualifiedName(Cid,Fqn)");
+        assertEquals(1,l.size());
+        Map r = (Map) l.get(0);
+        assertEquals("jt107.Test",(String)r.get("Fqn"));
+        
+        l = s.queryAll("classDefT(Cid,_,'Object',_),fullQualifiedName(Cid,Fqn)");
+        assertEquals(1,l.size());
+         r = (Map) l.get(0);
+        assertEquals("java.lang.Object",(String)r.get("Fqn"));
+    }
+}
