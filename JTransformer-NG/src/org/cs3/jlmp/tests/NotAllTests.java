699cccd253d5bcdb1f0451da5f60e371b59a7beb
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-01-25 23:43:20 +0000

    *** empty log message ***
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@344 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ JTransformer-NG/src/org/cs3/jlmp/tests/NotAllTests.java
@@ -0,0 +1,26 @@
+/*
+ */
+package org.cs3.jlmp.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ */
+public class NotAllTests {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for org.cs3.jlmp.tests");
+        //$JUnit-BEGIN$
+        suite.addTestSuite(FullQualifiedNameTest.class);
+        suite.addTestSuite(NormalizeTest.class);
+       // suite.addTestSuite(MultiDimensionalArraysTest.class);
+        suite.addTestSuite(SuperFieldAccessTest.class);
+        suite.addTestSuite(SuperMethodInvocationTest.class);
+        suite.addTestSuite(PrimitiveTypeLiteralsTest.class);
+        suite.addTestSuite(SelfTest.class);
+        suite.addTestSuite(MultiFragForLoopInitializerTest.class);
+        //$JUnit-END$
+        return suite;
+    }
+}
