8e5a27101e4e9fad65857444a62447dacd9b4506
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-01-29 23:46:38 +0000

    moved Option metainfo classes to Common
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@353 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ Common/src/org/cs3/pl/common/SimpleOption.java
@@ -0,0 +1,63 @@
+/*
+ */
+package org.cs3.pl.common;
+
+
+/**
+ */
+public class SimpleOption implements Option {
+    
+    private String id;
+    private String label;
+    private String description;
+    private int type;
+    private String defaultValue;
+    private String[][] enumValues;
+    
+    
+    /**
+     * @param id
+     * @param label
+     * @param description
+     * @param type
+     * @param defaultValue
+     */
+    public SimpleOption(String id, String label, String description, int type,
+            String defaultValue) {
+        super();
+        this.id = id;
+        this.label = label;
+        this.description = description;
+        this.type = type;
+        this.defaultValue = defaultValue;
+        this.enumValues=null;
+    }
+    public SimpleOption(String id, String label, String description, int type,
+            String defaultValue,String[][] enumValues) {
+        super();
+        this.id = id;
+        this.label = label;
+        this.description = description;
+        this.type = type;
+        this.defaultValue = defaultValue;
+        this.enumValues=enumValues;
+    }
+    public String getId(){
+        return id;
+    }
+    public  String getLabel(){
+        return label;
+    }
+    public  String getDescription(){
+        return description;
+    }
+    public  int getType (){
+        return type;
+    }
+    public  String getDefault(){
+        return defaultValue;
+    }
+    public String[][] getEnumValues() {       
+        return enumValues;
+    }
+}
