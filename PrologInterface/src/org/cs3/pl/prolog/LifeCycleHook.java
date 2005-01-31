1d0f6f6ce67da27898c0a830bce4ea091b64fb7f
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-01-31 21:35:17 +0000

    ADD: api docs
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@364 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- PrologInterface/src/org/cs3/pl/prolog/LifeCycleHook.java
+++ PrologInterface/src/org/cs3/pl/prolog/LifeCycleHook.java
@@ -1,7 +1,46 @@
 package org.cs3.pl.prolog;
 
+
+/**
+ * hook into the PIFs lifecycle.
+ */
 public interface LifeCycleHook{
+    /**
+     * called by the PrologInterface during startup phase.
+     * <br>
+     * This method executes on the same thread that starts the prolog interface.
+     * No interaction via regular PrologSessions will take place before
+     * this method is called on all registered LifeCycleHooks.
+     * <br><b>Important Note:</b> Only access the pif through the
+     * initSession argument. In particular, take care that this method
+     * does not indirectly trigger a call to PrologInterface.getSession() on the same thread,
+     * or you will very propably couse a dead lock. The initial session cannot be 
+     * disposed.  
+     * @param initSession safe-mode session for startup phase.
+     */
 	abstract void onInit(PrologSession initSession);
+	
+	/**
+     * called by the PrologInterface  after the startup is complete.
+     * <br>
+     * This method executes asynchronously to the thread that started
+     * the prolog interface. By the time it is called, the pif is guaranteed to be
+     * up and ready for normal operation (getSession() and friends).
+     * <br>
+     * 
+     */	
 	abstract void afterInit();
+	
+	/**
+     * called by the PrologInterface  before the pif shuts down.
+     * <br>
+     * This method is called on the same thread that stops the prolog interface.
+     * There are no other sessions running. (FIXME verify this!!)
+     * <br><b>Important Note:</b> Only access the pif through the
+     * initSession argument. In particular, take care that this method
+     * does not indirectly trigger a call to PrologInterface.getSession() on the same thread,
+     * or you will very propably couse a dead lock. The cleanup session cannot be 
+     * disposed.  
+     */		
 	abstract void beforeShutdown(PrologSession session);	
 }
