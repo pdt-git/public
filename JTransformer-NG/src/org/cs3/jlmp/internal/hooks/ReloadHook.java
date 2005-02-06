c6d917f0959f6a4ea498b5f0bc5caa5c870c5689
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-02-06 16:37:47 +0000

    made use of persistence a configurable option
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@389 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer-NG/src/org/cs3/jlmp/internal/hooks/ReloadHook.java
+++ JTransformer-NG/src/org/cs3/jlmp/internal/hooks/ReloadHook.java
@@ -2,17 +2,23 @@
  */
 package org.cs3.jlmp.internal.hooks;
 
+import java.io.File;
+
 import org.cs3.jlmp.JLMP;
+import org.cs3.jlmp.JLMPPlugin;
 import org.cs3.jlmp.JLMPProject;
 import org.cs3.jlmp.internal.natures.JLMPProjectNature;
 
 import org.cs3.pl.common.Debug;
+import org.cs3.pl.common.Util;
 import org.cs3.pl.prolog.LifeCycleHook;
 import org.cs3.pl.prolog.PrologInterface;
 import org.cs3.pl.prolog.PrologSession;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceDescription;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -28,53 +34,23 @@ public class ReloadHook implements LifeCycleHook {
      * 
      * @see org.cs3.pl.prolog.LifeCycleHook#onInit(org.cs3.pl.prolog.PrologSession)
      */
-    public void onInit(final PrologSession initSession) {
+    public void onInit(PrologInterface pif, final PrologSession initSession) {
         try {
-//            Job j = new Job("JLMP Reload Job") {
-//                protected IStatus run(IProgressMonitor monitor) {
-//                    //monitor.beginTask("reloading state",IProgressMonitor.UNKNOWN);
-//                    try {
-                        IProject[] projects = ResourcesPlugin.getWorkspace()
-                                .getRoot().getProjects();
-                        for (int i = 0; i < projects.length; i++) {
-                            IProject project = projects[i];
 
-                            if (project.isAccessible()
-                                    && project.hasNature(JLMP.NATURE_ID)) {
-                                JLMPProjectNature jlmpProject = (JLMPProjectNature) project
-                                        .getNature(JLMP.NATURE_ID);
+            JLMPProject[] jlmpProjects = JLMP.getJLMPProjects(pif);
+            for (int i = 0; i < jlmpProjects.length; i++) {
+                // XXX: ld: i don't like that cast. Any idee?
+                JLMPProjectNature jlmpProject = (JLMPProjectNature) jlmpProjects[i];
 
-                                PrologInterface pif = jlmpProject
-                                        .getPrologInterface();
-                                /*
-                                 * simply touching the cs should trigger
-                                 * creation and reload if neccesary.
-                                 */
-                                pif.getConsultService(JLMP.SRC).setRecording(
-                                        true);
-                                pif.getConsultService(JLMP.EXT).setRecording(
-                                        true);
-                                pif.getConsultService(JLMP.EXT)
-                                        .setAppendingRecords(true);
-                                jlmpProject.reconfigure(initSession);
-                            }
-                        }
+                jlmpProject.reconfigure(initSession);
 
-//                    } catch (Throwable e) {//well... uh.....eclipse
-//                                                    // eats my
-//                        // exceptions :-(
-//                        Debug.report(e);
-//                        return Status.CANCEL_STATUS;
-//                    }finally{
-//                        monitor.done();
-//                    }
-//                    return Status.OK_STATUS;
-//                }
-//            };
-//            j.schedule();
-//            j.join();
-            
-
+            }
+            JLMPPlugin plugin = JLMPPlugin.getDefault();
+            String v = plugin.getPreferenceValue(JLMP.PREF_USE_PEF_STORE,
+                    "false");
+            if (Boolean.valueOf(v).booleanValue()) {
+                plugin.reload(initSession);
+            }
         } catch (Throwable e) {
             Debug.report(e);
             throw new RuntimeException(e);
@@ -86,50 +62,25 @@ public class ReloadHook implements LifeCycleHook {
      * 
      * @see org.cs3.pl.prolog.LifeCycleHook#afterInit()
      */
-    public void afterInit() {
+    public void afterInit(PrologInterface pif) {
         try {
-            IProgressMonitor monitor =new NullProgressMonitor();
+            IWorkspaceDescription wd = ResourcesPlugin.getWorkspace()
+                    .getDescription();
+            if (! wd.isAutoBuilding()) {
+                return;
+            }
+            IProgressMonitor monitor = new NullProgressMonitor();
             IProject[] projects = ResourcesPlugin.getWorkspace().getRoot()
                     .getProjects();
-            for (int i = 0; i < projects.length; i++) {
-                final IProject project = projects[i];
+            JLMPProject[] jlmpProjects = JLMP.getJLMPProjects(pif);
+            for (int i = 0; i < jlmpProjects.length; i++) {
+                // XXX: ld: i don't like that cast. Any idee?
+                JLMPProjectNature jlmpProject = (JLMPProjectNature) jlmpProjects[i];
+                IProject project = jlmpProject.getProject();
+                project.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
 
-                if (project.isAccessible() && project.hasNature(JLMP.NATURE_ID)) {
-                    JLMPProject jlmpProject = (JLMPProject) project
-                            .getNature(JLMP.NATURE_ID);
-                    PrologInterface pif = jlmpProject.getPrologInterface();
-                    /*
-                     * finaly, we trigger full build of the project, so that any
-                     * modifications that happend while while the pif was down
-                     * are respected. This sounds more work than it actualy is:
-                     * most of the pefs should be cached on the local
-                     * filesystem.
-                     */
-//
-//                    Job j = new Job("JLMP Builder Job") {
-//                        protected IStatus run(IProgressMonitor monitor) {
-//                            try {
-                                //JLMPProjectNature nature =
-                                // (JLMPProjectNature)
-                                // p.getNature(JLMPProjectNature.NATURE_ID);
-                                //nature.getBuilder().build(null,0,monitor);
-                                project.build(
-                                        IncrementalProjectBuilder.FULL_BUILD,
-                                        monitor);
-//                            } catch (Throwable e) {//well...
-//                                // uh.....eclipse
-//                                // eats my
-//                                // exceptions :-(
-//                                Debug.report(e);
-//                                return Status.CANCEL_STATUS;
-//                            }
-//
-//                            return Status.OK_STATUS;
-//                        }
-//                    };
-//                    j.schedule();
-                }
             }
+
         } catch (Throwable e) {
             Debug.report(e);
             throw new RuntimeException(e);
@@ -141,8 +92,12 @@ public class ReloadHook implements LifeCycleHook {
      * 
      * @see org.cs3.pl.prolog.LifeCycleHook#beforeShutdown(org.cs3.pl.prolog.PrologSession)
      */
-    public void beforeShutdown(PrologSession session) {
-        ;
+    public void beforeShutdown(PrologInterface pif, PrologSession session) {
+        JLMPPlugin plugin = JLMPPlugin.getDefault();
+        String v = plugin.getPreferenceValue(JLMP.PREF_USE_PEF_STORE, "false");
+        if (Boolean.valueOf(v).booleanValue()) {
+            JLMPPlugin.getDefault().save(session);
+        }
     }
 
 }
