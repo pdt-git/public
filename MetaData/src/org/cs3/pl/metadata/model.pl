abfa2a426811fd60eb598572a49af9e896ea4410
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-02-14 10:11:08 +0000

    work in progress
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@426 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- MetaData/src/org/cs3/pl/metadata/model.pl
+++ MetaData/src/org/cs3/pl/metadata/model.pl
@@ -1,12 +1,16 @@
 :- module(model,[write_node/1,
 	write_tree/1,
-	write_tree2/1,
+	delete_tree/1,
+	type_property/2,
+	edge_property/3,
 	node/1,
 	node/2,
 	node_property/2,
 	parse_property/2,
 	parse_property_list/2,
-	child/2]
+	child/2,
+	is_source/1,
+	is_runtime/1]
 	).
 :- import(plparser:node_id(_)).	
 :- import(plparser:node_attr(_,_)).
@@ -23,18 +27,34 @@ parse_property(Functor,[],Functor->true).
 
 parse_property(Functor,[Arg|[]],Functor->Arg).		    
 
-node(Id):-
-    my_writeln('node(predicate_node(M:F/A))'),
-    node_id(Id).
-    
+is_runtime(N):-
+    node_property(N,is_runtime).
+is_source(A):-
+    node_property(A,is_source).
+
+
+node(source_folder_node(N)):-
+    node_id(source_folder_node(N)).
+node(compilation_unit_node(N)):-
+    node_id(compilation_unit_node(N)).
+node(atom_node(N)):-
+    node_id(atom_node(N)).
+node(variable_node(N)):-
+    node_id(variable_node(N)).
+node(string_node(N)):-
+    node_id(string_node(N)).
+node(brace_node(N)):-
+    node_id(brace_node(N)).
+node(list_node(N)):-
+    node_id(list_node(N)).
+node(compound_node(N)):-
+    node_id(compound_node(N)).                     
 node(predicate_node(M:F/A)):-
     my_writeln('node(predicate_node(M:F/A))'),    
 	predicates_defined_in(M,_,F,A).
-
 node(module_node(M)):-
     my_writeln('node(module_node(M))'),    
 	current_module(M).
-
 node(clause_node(M:F/A,I)):-
     my_writeln('node(clause_node(M:F/A,I))'),    
     clause_of(M,F,A,I).
@@ -60,55 +80,86 @@ node(Id,[HeadAttr|TailAttr]):-
    	node(Id,TailAttr).
    	
 
-
-
-node_property(Id,Prop):-
-  	my_writeln('node_property(Id,Prop)'),
-    node_attr(Id,Prop).
-    
-node_property(source_node(Num),is_source):-
-    my_writeln('node_property(source_node(Num),is_source)'),
-    node(source_node(Num)).
-    
 node_property(module_node(M),type(module)):-
-    my_writeln('node_property(module_node(M),type(module))'),
     node(module_node(M)).
+node_property(predicate_node(P),type(predicate)):-
+    node(predicate_node(P)).
+node_property(clause_node(M),type(clause)):-
+    node(clause_node(M)).
 
+node_property(module_node(M),is_runtime):-
+    node(module_node(M)).
+node_property(predicate_node(P),is_runtime):-
+    node(predicate_node(P)).
+node_property(clause_node(M),is_runtime):-
+    node(clause_node(M)).
+
+
+    
+node_property(source_folder_node(Num),type(source_folder)):-
+    node(source_folder_node(Num)).
+node_property(compilation_unit_node(Num),type(compilation_unit)):-
+    node(compilation_unit_node(Num)).
+node_property(atom_node(Num),type(atom)):-
+    node(atomt_node(Num)).
+node_property(variable_node(Num),type(variable)):-
+    node(variable_node(Num)).
+node_property(string_node(Num),type(string)):-
+    node(string_node(Num)).
+node_property(brace_node(Num),type(brace)):-
+    node(brace_node(Num)).
+node_property(list_node(Num),type(list)):-
+    node(list_node(Num)).
+node_property(compound_node(Num),type(compound)):-
+    node(compound_node(Num)).
+    
+node_property(source_folder_node(Num),is_source):-
+    node(source_folder_node(Num)).
+node_property(compilation_unit_node(Num),is_source):-
+    node(compilation_unit_node(Num)).
+node_property(atom_node(Num),is_source):-
+    node(atomt_node(Num)).
+node_property(variable_node(Num),is_source):-
+    node(variable_node(Num)).
+node_property(string_node(Num),is_source):-
+    node(string_node(Num)).
+node_property(brace_node(Num),is_source):-
+    node(brace_node(Num)).
+node_property(list_node(Num),is_source):-
+    node(list_node(Num)).
+node_property(compound_node(Num),is_source):-
+    node(compound_node(Num)).
+node_property(Id,Prop):-%%applies to all source nodes.
+    node_attr(Id,Prop).    
+
+node_property(module_node(M),type(module)):-
+    node(module_node(M)).
 node_property(module_node(M),file(File)):-
-    my_writeln('node_property(module_node(M),file(File))'),
     current_module(M,File).
-
 node_property(module_node(M),predicate(predicate_node(M:_X))):-
-    my_writeln('node_property(module_node(M),predicate(predicate_node(M:_X)))'),
     node(predicate_node(M:_X)).
 
 node_property(predicate_node(T),type(predicate)):-
     my_writeln('node_property(predicate_node(T),type(predicate))'),
     node(predicate_node(T)).
-
 node_property(predicate_node(M:_X),parent(module_node(M)))    :-
     my_writeln('node_property(predicate_node(M:_X),parent(module_node(M)))'),
     node(predicate_node(M:_X)).
-    
 node_property(predicate_node(M:F/A),name(F)):-
     my_writeln('node_property(predicate_node(M:F/A),name(F))'),
 	node(predicate_node(M:F/A)).
-
 node_property(predicate_node(M:F/A),arity(A)):-
     my_writeln('node_property(predicate_node(M:F/A),arity(A))'),
-	node(predicate_node(M:F/A)).
-    
+	node(predicate_node(M:F/A)).    
 node_property(predicate_node(M:F/A),term(Term)):-
     my_writeln('node_property(predicate_node(M:F/A),term(Term))'),
 	node(predicate_node(M:F/A)),
 	functor(Term,F,A).
-
 node_property(predicate_node(M:F/A),clause(clause_node(M:F/A,I))):-
     my_writeln('node_property(predicate_node(M:F/A),clause(clause_node(M:F/A,I)))'),
     node(predicate_node(M:F/A)),
 	functor(Term,F,A),
 	M:nth_clause(Term,I,_).
-
 node_property(predicate_node(M:F/A),real_line_count(RealLine)):-
     my_writeln('predicate_property(clause_node(M:F/A),real_line_count(Line))'),
     node(predicate_node(M:F/A)),
@@ -117,7 +168,6 @@ node_property(predicate_node(M:F/A),real_line_count(RealLine)):-
 	M:predicate_property(Term,file(File)),
 	starts_at(File,Offset),
 	RealLine is Line - Offset + 1.
-
 node_property(predicate_node(M:F/A),Prop):-
     my_writeln('node_property(predicate_node(M:F/A),Prop)'),
     node(predicate_node(M:F/A)),
@@ -129,37 +179,29 @@ node_property(predicate_node(M:F/A),Prop):-
 node_property(clause_node(M:F/A,I),type(clause)):-
     my_writeln('node_property(clause_node(M:F/A,I),type(clause))'),
     node(clause_node(M:F/A,I)).
-
 node_property(clause_node(M:F/A,I),clause_ref(Ref)):-
     my_writeln('node_property(clause_node(Ref),clause_ref(M:F/A,I))'),
     node(clause_node(M:F/A,I)),
     functor(Term,F,A),
-	M:nth_clause(Term,I,Ref).
-
-    
+	M:nth_clause(Term,I,Ref).    
 node_property(clause_node(M:F/A,I),parent(predicate_node(M:F/A))):-
     my_writeln('node_property(clause_node(M:F/A,I),parent(predicate_node(M:F/A)))'),
     node(clause_node(M:F/A,I)).
-
 node_property(clause_node(M:F/A,I),clause_number(I)):-
     my_writeln('node_property(clause_node(M:F/A,I),clause_number(I))'),
 	node(clause_node(M:F/A,I)).
-
 node_property(clause_node(M:F/A,I),head_term(Head)):-
     my_writeln('node_property(clause_node(M:F/A,I),head_term(Term))'),
     node(clause_node(M:F/A,I)),
     functor(P,F,A),
     M:nth_clause(P,I,Ref),
-    M:clause(Head,_,Ref).
-    
-
+    M:clause(Head,_,Ref).    
 node_property(clause_node(M:F/A,I),body_term(Body)):-
     my_writeln('node_property(clause_node(M:F/A,I),body_term(Body))'),
     node(clause_node(M:F/A,I)),
     functor(P,F,A),
     M:nth_clause(P,I,Ref),
     M:clause(_,Body,Ref).
-
 node_property(clause_node(M:F/A,I),real_line_count(RealLine)):-
     my_writeln('node_property(clause_node(M:F/A,I),real_line_count(Line))'),
     node(clause_node(M:F/A,I)),
@@ -169,8 +211,6 @@ node_property(clause_node(M:F/A,I),real_line_count(RealLine)):-
 	clause_property(Ref,file(File)),
 	starts_at(File,Offset),
 	RealLine is Line - Offset + 1.
-
-
 node_property(clause_node(M:F/A,I),Prop):-
     my_writeln('node_property(clause_node(M:F/A,I),Prop)'),
     node(clause_node(M:F/A,I)),
@@ -183,22 +223,25 @@ write_node(Id):-
     format("<<~w>>~n",Id),
     forall(node_property(Id,P),format(" --> ~w~n",[P])).
    	
+
 write_tree(Id):-
 	write_tree(Id,'','').
 	   	
 write_tree(Id,Indent,Arrow):-
     format("~a~a<<~w>>~n",[Indent,Arrow,Id]),
     atom_concat(Indent,'|    ',ChildIndent),
-    forall(node_property(Child,parent(Id)),write_tree(Child,ChildIndent,'|--')).    
-
-write_tree2(Id):-
-	write_tree(Id,'','').
-	   	
-write_tree2(Id,Indent,Arrow):-
-    format("~a~a<<~w>>~n",[Indent,Arrow,Id]),
-    atom_concat(Indent,'|    ',ChildIndent),
     forall(child(Id,Child),write_tree(Child,ChildIndent,'|--')).    
 
+delete_tree(Node):-
+    is_source(Node),
+    delete_tree_rec(Node).
+
+delete_tree_rec(Node):-
+	forall(child(Node,Child),delete_tree_rec(Child)),
+	retractall(node_attr(Node,_)),
+	retractall(node_id(Node)).
+			
+    
 
 child(module_node(M),predicate_node(M:X)):-
     node(predicate_node(M:X)).
@@ -206,8 +249,62 @@ child(module_node(M),predicate_node(M:X)):-
 child(predicate_node(X),Y):-
 	node_property(predicate_node(X),clause(Y)).    
 
+child(source_folder_node(P),compilation_unit_node(C)):-
+    node_attr(source_folder_node(P),compilation_unit(compilation_unit_node(C))).
+
+
+child(compilation_unit_node(P),Clause):-
+    node_attr(compilation_unit_node(P),clause(Clause)).
+
+child(atom_node(_),_):-
+	!,
+	fail.
+
+child(variable_node(_),_):-
+	!,
+	fail.
+
+child(string_node(_),_):-
+	!,
+	fail.
+	
+child(brace_node(P),Arg):-
+	node_attr(brace_node(P),argument(Arg)),!.
+
+child(list_node(P),Elm):-
+    node_attr(list_node(P),elements(Elms)),
+    member(Elm,Elms).
+child(list_node(P),Tail):-
+    node_attr(list_node(P),tail(Tail)),!.
+    
+child(compound_node(P),Arg):-
+    node_attr(compound_node(P),arguments(Args)),
+    !,
+    member(Arg,Args).
+
+
+implicit_edge(module,predicate).
+
+simple_edge(predicate,clause).
+simple_edge(source_folder,compilation_unit).
+simple_edge(compilation_unit,clause).
+simple_edge(brace,argument).
+simple_edge(list,tail).
+
+list_edge(list,elements).
+list_edge(compound,arguments).
+
+type_property(atom,leaf).
+type_property(variable,leaf).
+type_property(string,leaf).
+
+edge_property(predicate,clause,explicit).
+edge_property(predicate,clause,multiplicity(*)).
+	
 my_writeln(Term):-
 	true.
 %    writeln(Term).
+
+
 
   
--- MetaData/src/org/cs3/pl/metadata/plparser-bak.pl
+++ MetaData/src/org/cs3/pl/metadata/plparser-bak.pl
@@ -7,16 +7,15 @@
 :- dynamic node_id/1.
 
 
-%%parse('/home/lukas/eclipse/workspace/MetaData/src/org/cs3/pl/metadata/plparser.pl').  
+  
 
 parse(File,InStream):-
     writeln(ta),
-    unused_id(compilation_unit,Id),
+    unused_id(Id),
     my_assert(node_attr(Id,type(compilation_unit))),
     my_assert(node_attr(Id,file(File))),
     source_folder(FolderId,File),
     my_assert(node_attr(Id,parent(FolderId))),
-    my_assert(node_attr(FolderId,compilation_unit(Id))),
 %    catch(
 	!,
     	parse_clauses(Id,File,InStream,user).
@@ -35,18 +34,18 @@ source_folder(FolderId,File):-
     (	(	node_attr(FolderId,file(Dir)),
 			node_attr(FolderId,type(source_folder))
 		)
-	;	unused_id(source_folder,FolderId),
+	;	unused_id(FolderId),
 		my_assert(node_attr(FolderId,file(Dir))),
 		my_assert(node_attr(FolderId,type(source_folder)))
 	).
 
 parse_clauses(FileId,File,InStream,Module):-
     writeln(to),
+    unused_id(Id),
+    my_assert(node_attr(Id,parent(FileId))),
+    !,
 	parse_clause(Id,File,Module,InStream,NextModule),
 	!,
-    my_assert(node_attr(Id,parent(FileId))),
-    my_assert(node_attr(FileId,clause(Id))),
-    !,
 	parse_clauses(FileId,File,InStream,NextModule).
 
     
@@ -68,11 +67,10 @@ parse_clause(Id,File,Module,InStream,NextModule):-
 	parse_subterm_positions(Id,Term,Sub),
    	my_assert(node_attr(Id,file(File))),
    	my_assert(node_attr(Id,line(Line))),      
-   	%%(	find_clauseref(File,Line,Term,ClauseRef)
-%%   	->	my_assert(node_attr(Id,clause(ClauseRef)))
-%%   	;	my_assert(node_attr(Id,no_clauseref_found))
-%%   	).
-	my_assert(node_attr(Id,clause)).
+   	(	find_clauseref(File,Line,Term,ClauseRef)
+   	->	my_assert(node_attr(Id,clause(ClauseRef)))
+   	;	my_assert(node_attr(Id,no_clauseref_found))
+   	).
 
 
 
@@ -117,27 +115,23 @@ handle_error(Id,Error,Context,File):-
 	my_assert(node_attr(Id,file(File))).
 	
 parse_subterm_positions(Id,Term, From-To):-
-
-	( term_variables(Term,[])
-	->unused_id(atom,Id),
-	  my_assert(node_attr(Id,type(atom)))
-	; unused_id(variable,Id),
-	  my_assert(node_attr(Id,type(variable)))
-	),
 	my_assert(node_attr(Id,From-To)),
+	( term_variables(Term,[])
+	->my_assert(node_attr(Id,type(atom)))
+	; my_assert(node_attr(Id,type(variable)))
+	),
 	my_assert(node_attr(Id,term(Term))).
 	
 parse_subterm_positions(Id,Term, string_position(From,To)):-
-	unused_id(string,Id),
 	my_assert(node_attr(Id,From-To)),
 	my_assert(node_attr(Id,type(string))),
 	my_assert(node_attr(Id,term(Term))).
 
 parse_subterm_positions(Id,Term,  brace_term_position(From, To, Arg)):-
-    unused_id(brace,Id),
    	my_assert(node_attr(Id,From-To)),
 	my_assert(node_attr(Id,type(brace))),
 	my_assert(node_attr(Id,term(Term))),
+	unused_id(ArgId),
 	arg(1,Term,ArgTerm),
 	parse_subterm_positions(ArgId,ArgTerm,Arg),
 	my_assert(node_attr(Id,term(Term))),
@@ -145,19 +139,14 @@ parse_subterm_positions(Id,Term,  brace_term_position(From, To, Arg)):-
 	my_assert(node_attr(ArgId,parent(Id))).
 
 parse_subterm_positions(Id,Term,  list_position(From, To, Elms, Tail)):-
-   	unused_id(list,Id),
    	my_assert(node_attr(Id,From-To)),
 	my_assert(node_attr(Id,type(list))),
 	my_assert(node_attr(Id,term(Term))),
 	parse_elm_positions(Id,Term,Elms,Tail,ElmIDs,TailId),
 	my_assert(node_attr(Id,elements(ElmIDs))),
-	(	nonvar(TailId)
-	->	my_assert(node_attr(Id,tail(TailId)))
-	;	true
-	).
+	my_assert(node_attr(Id,tail(TailId))).
     
 parse_subterm_positions(Id,Term,  term_position(From, To, FFrom, FTo, SubPos)):-
-    unused_id(compound,Id),
     my_assert(node_attr(Id,From-To)),
     my_assert(node_attr(Id,functor_position(FFrom-FTo))),
     functor(Term,Name,Arity),
@@ -171,19 +160,22 @@ parse_subterm_positions(Id,Term,  term_position(From, To, FFrom, FTo, SubPos)):-
 parse_arg_positions(_,[],[],[]).   	
 
 parse_arg_positions(ParentId,[Arg|Args],[Position|Positions],[Id|Ids]):-
-   	parse_subterm_positions(Id,Arg,Position),
+    unused_id(Id),
    	my_assert(node_attr(Id,parent(ParentId))),
+   	parse_subterm_positions(Id,Arg,Position),
    	parse_arg_positions(ParentId,Args,Positions,Ids).
 
 parse_elm_positions(_,[],[],_,[],_).   	
 
 parse_elm_positions(ParentId,Elm,[],TailPosition,[],TailId):-
-   	parse_subterm_positions(TailId,Elm,TailPosition),
-   	my_assert(node_attr(TailId,parent(ParentId))).
+    unused_id(TailId),
+   	my_assert(node_attr(TailId,parent(ParentId))),
+   	parse_subterm_positions(TailId,Elm,TailPosition).
    	
 parse_elm_positions(ParentId,[Elm|Elms],[Position|Positions],TailPosition,[Id|Ids],TailId):-
-   	parse_subterm_positions(Id,Elm,Position),
+    unused_id(Id),
    	my_assert(node_attr(Id,parent(ParentId))),
+   	parse_subterm_positions(Id,Elm,Position),
    	parse_elm_positions(ParentId,Elms,Positions,TailPosition,Ids,TailId).
 
 my_assert(node_id(Id)):-
@@ -200,18 +192,15 @@ my_assert(node_attr(Id,Attr)):-
     ).
 
 unused_id(Type,Id):-
-    atom_concat(Type,'_node',Fun),
 	Num is random(1073741824),
-	Try=..[Fun,Num],
+	Try=..[Type,Num],
     ( node_id(Try)
     ->unused_id(Type,Id)
     ; Id=Try
     ).	
-/*
 unused_id(source_node(Num)):-
     Try is random(1073741824),
     ( node_id(source_node(Try))
     ->unused_id(source_node(Num))
     ; Num=Try
     ).
-*/
