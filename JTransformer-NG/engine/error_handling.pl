6077a8ab71fffc1df1cd2145ca42c4039feaa916
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-03-23 12:34:39 +0000

    LogicAJ specific changes - error handling creates isErrorMessage facts on CT apply errors Could be moved to JT in the future
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@471 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer-NG/engine/error_handling.pl
+++ JTransformer-NG/engine/error_handling.pl
@@ -4,14 +4,21 @@
 :- dynamic errorhandling/0.
 :- dynamic halt_on_error/0.
 
-error_handling(_term, _) :-
+% temporary necessary while aspects are not 
+% completely represented in prolog factbase:
+:- dynamic slAspectT/4. 
+:- multifile slAspectT/4.
+
+error_handling_add_error_term(_,_term, _) :-
     call(_term),
     !.
-error_handling(_,_term) :-
-    term_to_atom(_term,_err),
-    sformat(ErrorString, 'err ~a',[_err]),
+error_handling_add_error_term(SourceLocation,_, Err) :-
+    %term_to_atom(_term,_err),
+    sformat(ErrorString, 'err ~w',[Err]),
     write(ErrorString),
+    addErrorFacts(SourceLocation,Err),  
     flush_output,
+    debugme,
     halt_on_error ->
 	    halt;
 	    throw(ErrorString).
@@ -22,5 +29,10 @@ error_handling(_term, _,_) :-
 error_handling(_,_format,_term) :-
     term_to_atom(_term,_atom),
     sformat(_err,_format,[_atom]),
-    error_handling(fail,_err).
+    error_handling_add_error_term(null,fail,_err).
 
+addErrorFacts(null,_).
+addErrorFacts(sourceLocation(File, Start, Length),Err):-
+   new_id(ID),
+   add(isErrorWarningMessage('declare error', ID, Err)), 
+   add(slAspectT(ID, File,Start, Length)).
