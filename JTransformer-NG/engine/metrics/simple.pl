a24867ed0e682370fbf54f83c4befb33d8587a1c
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-03-30 12:10:45 +0000

    *** empty log message ***
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@481 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer-NG/engine/metrics/simple.pl
+++ JTransformer-NG/engine/metrics/simple.pl
@@ -1,3 +1,28 @@
+num_classes_extern(Num):-
+	findall(C,(class(C,_,_),externT(C)),L), 
+	length(L, Num). 
+
+num_classes(Num):-
+	findall(C,class(C,_,_),L), 
+	length(L, Num).
+
+num_field(Num):-
+	findall(C,fieldDefT(C,_,_,_,_),L), 
+	length(L, Num).
+num_field_extern(Num):-
+	findall(C,(fieldDefT(C,Class,_,_,_),externT(Class)),L), 
+	length(L, Num).
+	
+num_method(Num):-
+	findall(C,methodDefT(C,_,_,_,_,_,_),L), 
+	length(L, Num).
+num_method_extern(Num):-
+	findall(C,(methodDefT(C,Class,_,_,_,_,_),externT(Class)),L), 
+	length(L, Num).
+	
+num_method_calls(Num):-
+	findall(C,(applyT(C,_,_,_,_,_,_)),L), 
+	length(L, Num).
 
 overlong_method_signature(Max,FQN,Name,ParamList,Len):-
    methodDefT(_, Class, Name,Param,_,_,_),
@@ -5,5 +30,4 @@ overlong_method_signature(Max,FQN,Name,ParamList,Len):-
    Len > Max,
    fullQualifiedName(Class, FQN),
    gen_tree_list(Param,ParamList).
-	 
 	 
