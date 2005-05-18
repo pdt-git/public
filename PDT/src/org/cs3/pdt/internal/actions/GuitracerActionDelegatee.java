30d4377bc57203e463683b9670ba53d733bed1c0
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-05-18 13:16:06 +0000

    PDT-48
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@546 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ PDT/src/org/cs3/pdt/internal/actions/GuitracerActionDelegatee.java
@@ -0,0 +1,10 @@
+package org.cs3.pdt.internal.actions;
+
+
+public class GuitracerActionDelegatee extends QueryActionDelegatee {
+
+	public GuitracerActionDelegatee() {
+		super("guitracer", "activating GUI tracer");
+	}
+
+}
