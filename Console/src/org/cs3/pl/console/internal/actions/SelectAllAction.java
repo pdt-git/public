26c55d9dfe300a93dfc9013a316eac10996cbba3
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-07-04 17:40:04 +0000

    PDT-65
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@616 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ Console/src/org/cs3/pl/console/internal/actions/SelectAllAction.java
@@ -0,0 +1,24 @@
+package org.cs3.pl.console.internal.actions;
+
+import org.cs3.pl.console.SelectionContextAction;
+import org.eclipse.jface.action.Action;
+import org.eclipse.swt.custom.StyledText;
+
+public class SelectAllAction extends Action implements SelectionContextAction {
+
+	private StyledText output;
+
+
+	public boolean validate() {
+		return output.getText().length() != 0;
+	}
+
+	public void init(StyledText output) {
+		this.output = output;
+	}
+
+	public void run() {
+		output.selectAll();
+	}
+
+}
