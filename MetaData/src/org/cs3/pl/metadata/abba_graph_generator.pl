3752721cf8f0e79f22a3e3badcad2018c6759f69
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-07-05 12:20:31 +0000

    PDT-58:PROGRESS:abba local->global id mapping
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@624 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- MetaData/src/org/cs3/pl/metadata/abba_graph_generator.pl
+++ MetaData/src/org/cs3/pl/metadata/abba_graph_generator.pl
@@ -1,7 +1,25 @@
-:- module(abba_graph_generator,[abba_assert_global/1]).
+:- module(abba_graph_generator,[
+	abba_assert_data/1, 
+	abba_put_local_symbol/2, 
+	abba_clear_local_symbols/0
+]).
 
 :- dynamic last_id/1,global_id/2, local_symbol/2, local_id/2.
 :- thread_local local_symbol/2, local_id/2.
+
+
+abba_assert_data(Term):-
+	replace_local_ids(Term,GlobalTerm),
+	assert(GlobalTerm).
+abba_put_local_symbol(local_id(Local),Symbol):-
+	(	local_symbol(Local,Symbol)
+	->	true
+	;	assert(local_symbol(Local,Symbol))
+	).	
+abba_clear_local_symbols:-
+	retractall(local_symbols(_,_)).
+
+
 
 last_id(0).
 map_id(Local,Global):-
@@ -23,17 +41,9 @@ map_id(Local,Global):-
 	    assert(last_id(Global))
     ).
     
-abba_assert_global(Term):-
-	replace_local_ids(Term,GlobalTerm),
-	assert(GlobalTerm).
-	
-	
+		
 replace_lcoal_ids([],[]).
-replace_local_ids([edge_id(LId)|LT],	[edge_id(GId)|GT]):-
-    !,
-    map_id(LId,GId),
-    replace_local_ids(LT,GT).
-replace_local_ids([node_id(LId)|LT],	[node_id(GId)|GT]):-
+replace_local_ids([local_id(LId)|LT],	[GId|GT]):-
     !,
     map_id(LId,GId),
     replace_local_ids(LT,GT).
