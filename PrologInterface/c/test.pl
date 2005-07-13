409f89b8ccb768e39d5cb2acf7e0a2a32bb9c0bb
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-07-13 22:46:00 +0000

    SPIKE: towards a better console integration
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@641 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ PrologInterface/c/test.pl
@@ -0,0 +1,10 @@
+:- module(decorator_test,[]).
+:- load_foreign_library('stream_decorator.so').
+:- dynamic deco/1.
+%stream_decorator_read_hook(_,Orig,_,_,raw_tty,C):-
+%	get_char(Orig,C),
+%	skip(Orig,'\n').
+	
+
+	
+
