cb3f5328ae97b32fc07176511587ba36a0ea6a70
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-07-15 07:42:47 +0000

    EXPERIMENTAL: idea for an solution to the TTY_MODE probem.
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@646 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- PrologInterface/c/test.pl
+++ PrologInterface/c/test.pl
@@ -1,9 +1,12 @@
 :- module(decorator_test,[]).
 :- load_foreign_library('stream_decorator.so').
-:- dynamic deco/1.
-%stream_decorator_read_hook(_,Orig,_,_,raw_tty,C):-
-%	get_char(Orig,C),
-%	skip(Orig,'\n').
+
+stream_decorator_read_hook(Stream,_,_,raw_tty,[C]):-
+	orig_write_all(current_output, ['\\','s']),
+	orig_read(Stream,2,[C,'\n']).
+	
+stream_decorator_write_hook(Stream,_,['\\'|Tail],Tail):-
+	orig_write_all(Stream,['\\','\\']).	
 	
 
 	
