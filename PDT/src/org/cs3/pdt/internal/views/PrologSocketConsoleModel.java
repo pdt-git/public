8777b1aeec21fa49d4db934e3625513a58bd7948
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-07-17 23:32:09 +0000

    FIXED: character escaping in output stream
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@653 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- PDT/src/org/cs3/pdt/internal/views/PrologSocketConsoleModel.java
+++ PDT/src/org/cs3/pdt/internal/views/PrologSocketConsoleModel.java
@@ -24,7 +24,7 @@ public class PrologSocketConsoleModel implements ConsoleModel {
         
     private class ConsoleReader implements Runnable {
         
-        private static final char ESCAPE_CHAR = '\\';
+        private static final char ESCAPE_CHAR = '§';
 		private static final char RAW_MODE_CHAR = 's';
 		private BufferedReader reader;
 
