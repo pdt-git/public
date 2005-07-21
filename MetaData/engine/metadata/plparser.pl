4008e586688afdcf611e4da47c6957eceee0f033
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-07-21 22:42:33 +0000

    some old spike revisited
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@665 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- MetaData/engine/metadata/plparser.pl
+++ MetaData/engine/metadata/plparser.pl
@@ -1,14 +1,19 @@
-:- module(plparser,[
-	parse/1,
-	parse/2	
-]).
-
+:- module(plparser,[parse/1,parse/2]). 
 :- dynamic node_attr/2.
 :- dynamic node_id/1.
 
-
-%%parse('/home/lukas/eclipse/workspace/MetaData/src/org/cs3/pl/metadata/plparser.pl').  
-
+node_attr(source_folder_node(P),child(C)):-
+    node_attr(source_folder_node(P),compilation_unit(C)).
+node_attr(compilation_unit_node(P),child(C)):-
+    node_attr(compilation_unit_node(P),clause(C)).
+node_attr(brace_node(P),child(C)):-
+    node_attr(brace_node(P),argument(C)).    
+node_attr(list_node(P),child(C)):-
+    node_attr(list_node(P),elements(L)),
+    member(C,L).            
+node_attr(compound_node(P),child(C)):-
+    node_attr(compound_node(P),arguments(L)),
+    member(C,L).            
 parse(File,InStream):-
     writeln(ta),
     unused_id(compilation_unit,Id),
@@ -17,12 +22,8 @@ parse(File,InStream):-
     source_folder(FolderId,File),
     my_assert(node_attr(Id,parent(FolderId))),
     my_assert(node_attr(FolderId,compilation_unit(Id))),
-%    catch(
-	!,
-    	parse_clauses(Id,File,InStream,user).
-%    	eof,
-%    	(!,true)
-%    ).
+   	parse_clauses(InStream,Id,user).
+
 
 parse(File):-
     writeln(tu),
@@ -40,115 +41,74 @@ source_folder(FolderId,File):-
 		my_assert(node_attr(FolderId,type(source_folder)))
 	).
 
-parse_clauses(FileId,File,InStream,Module):-
-    writeln(to),
-	parse_clause(Id,File,Module,InStream,NextModule),
-	!,
-    my_assert(node_attr(Id,parent(FileId))),
-    my_assert(node_attr(FileId,clause(Id))),
-    !,
-	parse_clauses(FileId,File,InStream,NextModule).
+parse_clauses(InStream,FileId,CurrentModule):-    
+	parse_clause(InStream,FileId,CurrentModule,NextModule,ClauseId),
+	(	ClauseId==end_of_file
+	->	true
+	;	my_assert(node_attr(FileId,clause(ClauseId))),
+		parse_clauses(InStream,FileId,NextModule)
+	).
 
     
 
-parse_clause(Id,File,Module,InStream,NextModule):-
-    writeln(tem),
+parse_clause(InStream,ParentId,CurrentModule,NextModule,ClauseId):-
 	catch(
-		read_term(InStream,Term,[term_position(Pos),subterm_positions(Sub),module(Module)]),
+		read_term(InStream,Term,[term_position(Pos),subterm_positions(Sub),module(CurrentModule)]),
 		error(Error,Context),
-		(handle_error(Id,Error,Context,File),fail)
+		(handle_error(ClauseId,Error,Context),fail)
 	),	
     format("processing ~w~n",[Term]),
 	arg(2,Pos,Line)	,
-    writeln(te1),
-	!,
-	\+ Term==end_of_file,
-    writeln(te2),
-	next_module(Term,NextModule),	    
-	parse_subterm_positions(Id,Term,Sub),
-   	my_assert(node_attr(Id,file(File))),
-   	my_assert(node_attr(Id,line(Line))),      
-   	%%(	find_clauseref(File,Line,Term,ClauseRef)
-%%   	->	my_assert(node_attr(Id,clause(ClauseRef)))
-%%   	;	my_assert(node_attr(Id,no_clauseref_found))
-%%   	).
-	my_assert(node_attr(Id,clause)).
+	(	Term==end_of_file
+	->	ClauseId=end_of_file
+	;   next_module(Term,NextModule),	    
+		parse_subterm_positions(ParentId,Term,Sub,ClauseId),
+	   	my_assert(node_attr(ClauseId,line(Line))),      
+		my_assert(node_attr(ClauseId,clause))
+	).
 
 
 
 next_module(Term,NextModule):-
-    writeln(li),
     Term=..[:-,Directive],
     Directive=..[module,NextModule,_],
     !.
-next_module(_,user):-
-	    writeln(lo).    
-% find the smallest clause reference that 
-% matches the term, aswell as the file and the line
-% and is not already associated to some node.
-%
-% Warning:
-% We assume that the order in which identical clauses are encountered
-% in a file correpsonds to the order of their references. This is
-% a rather strong assumption, which i would love to get rid of. Any suggestions?    
-find_clauseref(File,Line,Term,ClauseRef):-
-    writeln(lum),
-    Term=..[Functor|Args],   
-    setof(Canidate,find_clauseref(File,Line,Functor,Args,Canidate),[ClauseRef|_]).
-
-find_clauseref(File,Line,:-,[Head,Body],ClauseRef):-
-    writeln(re),
-    clause(Head,Body,ClauseRef),
-    clause_property(ClauseRef,file(File)),
-    clause_property(ClauseRef,line_count(Line)),
-    \+ node_attr(_,clause(ClauseRef)).
-       
-find_clauseref(File,Line,Fact,_,ClauseRef):-
-    writeln(ro),    
-    clause(Fact,true,ClauseRef),
-    clause_property(ClauseRef,file(File)),
-    clause_property(ClauseRef,line_count(Line)),
-    \+ node_attr(_,clause(ClauseRef)).    
-
+next_module(_,user).
     
 handle_error(Id,Error,Context,File):-
+    unused_id(error,Id),
 	my_assert(node_attr(Id,error(Error))),    
 	my_assert(node_attr(Id,context(Context))),
 	my_assert(node_attr(Id,file(File))).
 	
-parse_subterm_positions(Id,Term, From-To):-
-
-	( term_variables(Term,[])
+assert_common_attrs(Id,Type,Parent,From-To,Term):-
+    my_assert(node_attr(Id,type(Type))),
+    my_assert(node_attr(Id,parent(Parent))),
+    my_assert(node_attr(Id,From-To)),
+    my_assert(node_attr(Id,term(Term))).
+    
+parse_subterm_positions(ParentId,Term, From-To,Id):-	
+	( atom(Term)
 	->unused_id(atom,Id),
-	  my_assert(node_attr(Id,type(atom)))
+	  assert_common_attrs(Id,atom,ParentId,From-To,Term)
 	; unused_id(variable,Id),
-	  my_assert(node_attr(Id,type(variable)))
-	),
-	my_assert(node_attr(Id,From-To)),
-	my_assert(node_attr(Id,term(Term))).
+	  assert_common_attrs(Id,variable,ParentId,From-To,Term)
+	).
 	
-parse_subterm_positions(Id,Term, string_position(From,To)):-
+parse_subterm_positions(ParentId,Term, string_position(From,To),Id):-
 	unused_id(string,Id),
-	my_assert(node_attr(Id,From-To)),
-	my_assert(node_attr(Id,type(string))),
-	my_assert(node_attr(Id,term(Term))).
+	assert_common_attrs(Id,string,ParentId,From-To,Term).
 
-parse_subterm_positions(Id,Term,  brace_term_position(From, To, Arg)):-
+parse_subterm_positions(ParentId,Term,  brace_term_position(From, To, Arg),Id):-
     unused_id(brace,Id),
-   	my_assert(node_attr(Id,From-To)),
-	my_assert(node_attr(Id,type(brace))),
-	my_assert(node_attr(Id,term(Term))),
+	assert_common_attrs(Id,brace,ParentId,From-To,Term),
 	arg(1,Term,ArgTerm),
-	parse_subterm_positions(ArgId,ArgTerm,Arg),
-	my_assert(node_attr(Id,term(Term))),
-	my_assert(node_attr(Id,argument(ArgId))),
-	my_assert(node_attr(ArgId,parent(Id))).
+	parse_subterm_positions(Id,ArgTerm,Arg,ArgId),
+	my_assert(node_attr(Id,argument(ArgId))).
 
-parse_subterm_positions(Id,Term,  list_position(From, To, Elms, Tail)):-
+parse_subterm_positions(ParentId,Term,  list_position(From, To, Elms, Tail),Id):-
    	unused_id(list,Id),
-   	my_assert(node_attr(Id,From-To)),
-	my_assert(node_attr(Id,type(list))),
-	my_assert(node_attr(Id,term(Term))),
+	assert_common_attrs(Id,list,ParentId,From-To,Term),
 	parse_elm_positions(Id,Term,Elms,Tail,ElmIDs,TailId),
 	my_assert(node_attr(Id,elements(ElmIDs))),
 	(	nonvar(TailId)
@@ -156,14 +116,12 @@ parse_subterm_positions(Id,Term,  list_position(From, To, Elms, Tail)):-
 	;	true
 	).
     
-parse_subterm_positions(Id,Term,  term_position(From, To, FFrom, FTo, SubPos)):-
+parse_subterm_positions(ParentId,Term,  term_position(From, To, FFrom, FTo, SubPos),Id):-
     unused_id(compound,Id),
-    my_assert(node_attr(Id,From-To)),
+    assert_common_attrs(Id,compound,ParentId,From-To,Term),
     my_assert(node_attr(Id,functor_position(FFrom-FTo))),
     functor(Term,Name,Arity),
     my_assert(node_attr(Id,functor(Name/Arity))),
-	my_assert(node_attr(Id,type(compound))),
-	my_assert(node_attr(Id,term(Term))),
     Term=..[_|Args],
    	parse_arg_positions(Id,Args,SubPos,ArgIDs),
    	my_assert(node_attr(Id,arguments(ArgIDs))).
@@ -171,29 +129,24 @@ parse_subterm_positions(Id,Term,  term_position(From, To, FFrom, FTo, SubPos)):-
 parse_arg_positions(_,[],[],[]).   	
 
 parse_arg_positions(ParentId,[Arg|Args],[Position|Positions],[Id|Ids]):-
-   	parse_subterm_positions(Id,Arg,Position),
-   	my_assert(node_attr(Id,parent(ParentId))),
+   	parse_subterm_positions(ParentId,Arg,Position,Id),
    	parse_arg_positions(ParentId,Args,Positions,Ids).
 
 parse_elm_positions(_,[],[],_,[],_).   	
 
 parse_elm_positions(ParentId,Elm,[],TailPosition,[],TailId):-
-   	parse_subterm_positions(TailId,Elm,TailPosition),
-   	my_assert(node_attr(TailId,parent(ParentId))).
+   	parse_subterm_positions(ParentId,Elm,TailPosition,TailId).
    	
 parse_elm_positions(ParentId,[Elm|Elms],[Position|Positions],TailPosition,[Id|Ids],TailId):-
-   	parse_subterm_positions(Id,Elm,Position),
-   	my_assert(node_attr(Id,parent(ParentId))),
+   	parse_subterm_positions(ParentId,Elm,Position,Id),
    	parse_elm_positions(ParentId,Elms,Positions,TailPosition,Ids,TailId).
 
 my_assert(node_id(Id)):-
-    %format("assert(~w)~n",[Term]),
     ( 	node_id(Id)
 	;   assert(node_id(Id))
 	).
 
 my_assert(node_attr(Id,Attr)):-
-    %format("assert(~w)~n",[Term]),
     my_assert(node_id(Id)),
     (	node_attr(Id,Attr)
     ;	assert(node_attr(Id,Attr))
@@ -207,11 +160,3 @@ unused_id(Type,Id):-
     ->unused_id(Type,Id)
     ; Id=Try
     ).	
-/*
-unused_id(source_node(Num)):-
-    Try is random(1073741824),
-    ( node_id(source_node(Try))
-    ->unused_id(source_node(Num))
-    ; Num=Try
-    ).
-*/
