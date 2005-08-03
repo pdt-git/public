cecd3aafc2c3cc79036ee835bd9fc95381105fc7
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-08-03 08:12:39 +0000

    http://roots.iai.uni-bonn.de/jira32/browse/JT-186#action_12080
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@691 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer-NG/src/org/cs3/jtransformer/internal/astvisitor/GenericAnnotation.java
+++ JTransformer-NG/src/org/cs3/jtransformer/internal/astvisitor/GenericAnnotation.java
@@ -7,4 +7,12 @@ public interface GenericAnnotation {
 	String getName();
 	List getArgs();
 	String getPredicate();
+	void setInline(boolean isInlineComment);
+	/**
+	 * The annotation is inlined. This means it
+	 * is a annotation on a statement or an expression.
+	 * 
+	 * @return
+	 */
+	boolean isInline();
 }
