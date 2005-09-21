d236e0f9f2b2e6856903371917ed004b4e7aa32c
gk <gk@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-09-21 13:08:01 +0000

    Changed binary --> ascii.
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@713 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer-NG/engine/ast/main.pl
+++ JTransformer-NG/engine/ast/main.pl
@@ -1,9 +1,9 @@
 :- [languageIndependentSyntax, % must be loaded first because it contains all 'multifile' definitions.
     languageIndependentSemantics,
     languageAbstractions,
-    javaFactbase,
-    javaSyntax,
-    lajSyntax,
+    java/javaFactbase,
+    java/javaSyntax,
+    laj/lajSyntax,
     fq_api,
     fq_api_tests
    ].
