54757779c4b36943a04e17dc49b6a27118e252ea
appeltau <appeltau@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-10-18 11:33:31 +0000

    *** empty log message ***
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@719 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer-NG/engine/ast/main.pl
+++ JTransformer-NG/engine/ast/main.pl
@@ -1,9 +1,9 @@
 :- [languageIndependentSyntax, % must be loaded first because it contains all 'multifile' definitions.
     languageIndependentSemantics,
     languageAbstractions,
-    java/javaFactbase,
-    java/javaSyntax,
-    laj/lajSyntax,
+    javaFactbase,
+    javaSyntax,
+    lajSyntax,
     fq_api,
     fq_api_tests
    ].
