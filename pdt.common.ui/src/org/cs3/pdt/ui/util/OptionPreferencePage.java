0af37715712dc873b3911188a9f180cf1b8a28a9
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-10-21 17:12:37 +0000

    outfactored common ui bits into new project
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@725 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ pdt.common.ui/src/org/cs3/pdt/ui/util/OptionPreferencePage.java
@@ -0,0 +1,131 @@
+package org.cs3.pdt.ui.util;
+
+
+import org.cs3.pl.common.Option;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.PreferenceStore;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public abstract class OptionPreferencePage extends FieldEditorPreferencePage implements
+IWorkbenchPreferencePage{
+
+	private IPreferenceStore store;
+	private Option[] options;
+
+	public OptionPreferencePage(IPreferenceStore store, Option[] options, String descr){
+		super(GRID);
+        this.store=store;
+        this.options = options;
+		setPreferenceStore(store);
+        setDescription(descr);
+	}
+	/**
+     * Creates the field editors. Field editors are abstractions of the common
+     * GUI blocks needed to manipulate various types of preferences. Each field
+     * editor knows how to save and restore itself.
+     */
+    public void createFieldEditors() {        
+        Composite parent = getFieldEditorParent();
+        addEditorsForOptions(parent, options);
+    }
+
+    private void addEditorsForOptions(Composite parent, final Option[] options) {
+        FieldEditor editor = null;
+        for(int i=0;i<options.length;i++){
+            String name = options[i].getId();
+            String label = options[i].getLabel();
+            final int j =i;
+            switch(options[i].getType()){
+            	case Option.DIR:
+            	    editor = new DirectoryFieldEditor(name,label,parent){
+            		protected boolean doCheckState() {
+            			String errmsg=options[j].validate(getStringValue());
+            			if(errmsg==null){
+            				return super.doCheckState();
+            			}
+            			if(errmsg.length()!=0){
+            				setErrorMessage(errmsg);
+            				return false;
+            			}
+            			return true;
+            		}
+            	};            	    
+            	break;
+            	case Option.FLAG:
+            	    editor = new BooleanFieldEditor(name,label,parent);
+            	    //TODO: flags are not validated right now. 
+            	break;
+            	case Option.NUMBER:
+            	    editor = new IntegerFieldEditor(name,label,parent){
+                		protected boolean doCheckState() {
+                			String errmsg=options[j].validate(getStringValue());
+                			if(errmsg==null){
+                				return super.doCheckState();
+                			}
+                			if(errmsg.length()!=0){
+                				setErrorMessage(errmsg);
+                				return false;
+                			}
+                			return true;
+                		}
+                	};
+            	break;
+            	case Option.ENUM:
+            	    editor = new RadioGroupFieldEditor(name,label,4,options[i].getEnumValues(),parent,true);
+            	    //TODO: enums are not validated right now. 
+            	break;
+            	default:
+            	    editor = new StringFieldEditor(name,label,parent){
+                		protected boolean doCheckState() {
+                			String errmsg=options[j].validate(getStringValue());
+                			if(errmsg==null){
+                				return super.doCheckState();
+                			}
+                			if(errmsg.length()!=0){
+                				setErrorMessage(errmsg);
+                				return false;
+                			}
+                			return true;
+                		}
+                	};
+            		break;
+            		
+            }
+            //disable the editor, if the value is overridden per sys prop.
+            editor.setEnabled(System.getProperty(name)==null,parent);
+            addField(editor);
+        }
+    }
+    public void init(IWorkbench workbench) {
+        //setPreferenceStore(PDTPlugin.getDefault().getPreferenceStore());
+    }
+
+    /**
+     * @see org.eclipse.jface.preference.PreferencePage#performDefaults()
+     */
+    protected void performDefaults() {
+        super.performDefaults();
+    }
+
+    /**
+     * @see org.eclipse.jface.preference.IPreferencePage#performOk()
+     */
+    public boolean performOk() {
+
+        boolean res = super.performOk();
+        reconfigure();
+        return res;
+    }
+	protected abstract void reconfigure(); 
+
+
+}
---
+++ pdt.common.ui/src/org/cs3/pdt/ui/util/StringEditor.java
@@ -0,0 +1,96 @@
+/*
+ */
+package org.cs3.pdt.ui.util;
+
+import org.cs3.pl.common.Option;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ */
+public class StringEditor extends OptionEditor implements PropertyEditor {
+    private static final int TEXT_FIELD_WIDTH = 20;
+    private Text valueField;
+    public StringEditor(Composite parent, Option option) {
+        super(parent, option);
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.cs3.jtransformer.internal.properties.OptionEditor#createControls(org.eclipse.swt.widgets.Composite)
+     */
+    protected void createControls(Composite composite) {
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 2;
+        composite.setLayout(layout);
+        
+        GridData data = new GridData(GridData.FILL);
+        data.grabExcessHorizontalSpace = true;
+        data.verticalAlignment = GridData.FILL;
+        data.horizontalAlignment = GridData.FILL;
+        composite.setLayoutData(data);
+
+        //info text
+//        Label info = new Label(composite, SWT.READ_ONLY | SWT.MULTI | SWT.WRAP);        
+//        info.setText(option.getDescription());
+//        GridData gd = new GridData();
+//        gd.horizontalAlignment = GridData.FILL;
+//        gd.verticalAlignment = GridData.CENTER;
+//        gd.grabExcessHorizontalSpace = true;
+//        gd.grabExcessVerticalSpace = false;
+//        gd.horizontalSpan = 2;
+//        info.setLayoutData(gd);
+
+        //Field label
+        Label label = new Label(composite, SWT.NONE);
+        label.setText(option.getLabel());
+        GridData gd = new GridData();
+        gd.horizontalAlignment = GridData.FILL;
+        gd.verticalAlignment = GridData.CENTER;
+        gd.grabExcessHorizontalSpace = false;
+        gd.grabExcessVerticalSpace = false;
+        gd.widthHint =convertWidthInCharsToPixels(label.getText().length() + 4);        
+        label.setLayoutData(gd);
+
+        valueField = new Text(composite, SWT.SINGLE | SWT.BORDER);
+        gd = new GridData();
+        gd.horizontalAlignment = GridData.FILL;
+        gd.verticalAlignment = GridData.FILL;
+        gd.grabExcessHorizontalSpace = true;
+        gd.grabExcessVerticalSpace = false;
+        gd.widthHint = convertWidthInCharsToPixels(TEXT_FIELD_WIDTH);
+        valueField.setLayoutData(gd);
+        valueField.addModifyListener(new ModifyListener() {
+            String old="";
+            public void modifyText(ModifyEvent e) {
+                if(old.equals(e.data)) {
+                    return;
+                }
+                firePropertyChange(old,(String) e.data);
+                old=""+e.data;
+            }
+        });
+    }
+
+    /* (non-Javadoc)
+     * @see org.cs3.jtransformer.internal.properties.PropertyEditor#setValue(java.lang.String)
+     */
+    public void setValue(String value) {
+        valueField.setText(value);
+    }
+
+    
+    /* (non-Javadoc)
+     * @see org.cs3.jtransformer.internal.properties.PropertyEditor#getValue()
+     */
+    public String getValue() {      
+        return valueField.getText();
+    }
+
+}
