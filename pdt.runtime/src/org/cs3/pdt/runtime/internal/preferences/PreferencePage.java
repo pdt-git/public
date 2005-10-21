7df984938ea3132f26de0972257a651a66e8ae97
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-10-21 20:22:03 +0000

    OptionPreferencePage needed more flexible init to handle pif config
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@728 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.runtime/src/org/cs3/pdt/runtime/internal/preferences/PreferencePage.java
+++ pdt.runtime/src/org/cs3/pdt/runtime/internal/preferences/PreferencePage.java
@@ -1,15 +1,27 @@
 package org.cs3.pdt.runtime.internal.preferences;
 
+import org.cs3.pdt.runtime.PrologRuntime;
 import org.cs3.pdt.runtime.PrologRuntimePlugin;
 import org.cs3.pdt.ui.util.OptionPreferencePage;
+import org.cs3.pl.common.Option;
+import org.cs3.pl.prolog.PrologInterfaceFactory;
 
 public class PreferencePage extends OptionPreferencePage {
 
 	public PreferencePage() {
-		super(PrologRuntimePlugin.getDefault().getPreferenceStore(), PrologRuntimePlugin
-				.getDefault().getOptions(),
-				"Preferences for the PDT Plugin");
-
+		super();
+		
+		PrologRuntimePlugin plugin = PrologRuntimePlugin.getDefault();
+		setPreferenceStore(plugin.getPreferenceStore());
+		setDescription("Preferences for the Prolog Interface");
+		Option[] pluginOptions = plugin.getOptions();
+		String fqn = plugin.getPreferenceValue(PrologRuntime.PREF_PIF_IMPLEMENTATION,null);
+		PrologInterfaceFactory factory = fqn==null?PrologInterfaceFactory.newInstance():PrologInterfaceFactory.newInstance(fqn);
+		Option[] factoryOptions = factory.getOptions();
+		Option[] allOptions = new Option[factoryOptions.length+pluginOptions.length];
+		System.arraycopy(pluginOptions,0,allOptions,0,pluginOptions.length);
+		System.arraycopy(factoryOptions,0,allOptions,pluginOptions.length,factoryOptions.length);
+		setOptions(allOptions);
 	}
 
 	protected void reconfigure() {
