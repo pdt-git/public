7bb7c28df9e6832e53d6fa2ef8292629cfa1bf49
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2005-10-27 14:12:35 +0000

    renamed tests.pl to punit.pl and prepared refactoring to module
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@756 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer-NG/engine/test/punit.pl
+++ JTransformer-NG/engine/test/punit.pl
@@ -1,5 +1,38 @@
-% Author: Tobias
-% Date: 31.07.04
+/**
+ * (Module) punit.
+ *
+ * Test framework for Prolog.
+ * Use runTests/0 to start the testing.
+ * For every predicate test/1 with optional setUp/1 and tearDown/1
+ *  * predicates the test is executed.
+ * e.g.:
+ *   punit:test(name1) :- ...
+ *   punit:setUp(name1) :- ...
+ *   punit:tearDown(name1) :- ...
+ * will be excecuted in the order setUp, test, tearDown.
+ * Every predicate may fail or throw exceptions, 
+ * the other predicates will run nevertheless.
+ * Every exception will be printed to the screen.
+ * After the execution of all tests a summary is shown.
+ * Presenting all failed test names and how many test were
+ * failed und how many succeeded.
+ * 
+ * Use assert_true/2, assert_fail/2 and assert_bound/2 for
+ * your test cases to check your predicates.
+ *
+ * @author Tobias Rho
+ * @created 31.07.04
+ * @updated 27.10.05
+ */
+
+/* TODO: uncomment after JT refactoring:
+:- module(punit, 
+     [runTests/0, 
+      runTestsStartingWith/1,
+      test/1,
+      setUp/1,
+      tearDown/1]).
+*/
 
 :- multifile test/1.
 :- multifile setUp/1.
@@ -7,40 +40,51 @@
 
 :- dynamic failed/0.
 
-/*
-runTests
+/**
+ * runTests
+ * 
+ * Run all consulted
+ * punit:test cases.
+ * 
+ */
 
-Test framework for JTransformer.
-Use runTests/0 to start the testing.
-For every predicate test/1 with optional setUp/1 and tearDown/1
-predicates the test is executed.
-e.g.:
-test(name1) :- ...
-setUp(name1) :- ...
-tearDown(name1) :- ...
-will be excecuted in the order setUp, test, tearDown.
-Every predicate may fail or throw exceptions, 
-the other predicates will run nevertheless.
-Every exception will be printed to the screen.
-After the execution of all tests a summary is shown.
-Presenting all failed test names and how many test were
-failed und how many succeeded.
+runTests :- 
+    runTestsStartingWith(_).
 
-Use assert_true/2, assert_fail/2 and assert_bound/2 for
-your test cases to check your predicates.
-
-*/
-
-runTests :- failed(_list, _length, _all),
+/**
+ * runTestsStartingWith(+Prefix)
+ * 
+ * run all tests starting with Prefix
+ */
+runTestsStartingWith(Prefix):- 
+        failed(FailedTests, Failed, All,Prefix),
 		counter(0),
         writef("\n=======================\nfailed tests:\n"),
-        write(_list),
-        writef("\n=======================\nnumber of failed tests: (%d/%d)\n", [_length, _all]).
+        write(FailedTests),
+        plus(Failed,Success,All),
+        writef("\n=======================\nsuccessful tests: (%d/%d)\n", [Success, All]).
 
+/**
+ * runTest(+Testname)
+ * 
+ * evaluate the test case Testname.
+ */
 runTest(Testname):-
 	not(testfailed(Testname)).
         
 
+/*************** internal ****************/
+
+test_case(Name,Prefix):-
+   var(Prefix),
+   !,
+   clause(test(Name), _).
+
+test_case(Name,Prefix):-
+   clause(test(Name),_), 
+   term_to_atom(Name,Atom),
+   atom_concat(Prefix,_,Atom).
+   
 testfailed(Testname) :- 
 %    format('the test case ~a does not exist.',[Testname]),
     not(clause(test(Testname), _)),
@@ -80,16 +124,16 @@ reportException(Pred,Testname):-
 % testfailed(_testname) :- test(_testname), writef('.').
 
 
-failed(_list, _length, _all) :- findall(
+failed(_list, _length, _all,Prefix) :- findall(
                                     [_testname],
-                                    (clause(test(_testname), _),
+                                    (test_case(_testname,Prefix),
                                      testfailed(_testname)
                                     ), 
                                     _list
                                   ),
                                   length(_list, _length),
                                   findall([_test],
-                                          clause(test(_test),_),
+                                          test_case(_testname,Prefix),
                                           _tests
                                   ),
                                   length(_tests, _all).
