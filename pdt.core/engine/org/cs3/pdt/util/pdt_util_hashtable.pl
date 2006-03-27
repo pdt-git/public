30eb756b2babb80ca6b39215a9fe9a4eeae09d8c
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-03-27 14:03:28 +0000

    w.i.p.
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@922 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.core/engine/org/cs3/pdt/util/pdt_util_hashtable.pl
+++ pdt.core/engine/org/cs3/pdt/util/pdt_util_hashtable.pl
@@ -4,6 +4,20 @@
 %I am not completely sure how to do it, or if it will make much difference in terms
 %of performance, so i regard this as an abstraction. Its current implementation is ased on the
 %records database.
+%
+% random access is implemented straight forward by usint hash_term/2
+% all operation should be O(1), assuming that collision lists stay
+% reasonably short. Currently the implementation does nothing to 
+% enforce this (i.e. no re-hashing, splitting, etc.) Yet todo.
+%
+% To enable efficient iteration of all entries, the individual buckets
+% (or collision lists, i.e. the elements of the image of the hash function)
+% maintain references to each other in a way that they form a linked list.
+% So if pdt_get/3 is called with a variable key, it will successively create 
+% all entries of the hash table.
+%
+%
+
 
 :-module(pdt_util_hashtable,[
 	pdt_put/3,
@@ -14,29 +28,24 @@
 	pdt_remove_all/2
 ]).
 
+
+pdt_get(HT,Key,Value):-
+    ground(Key),
+	hash_key(HT,Key,HashKey),
+	recorded(HashKey,entry(Key,Value)).
+pdt_get(HT,Key,Value):-
+    var(Key),
+    first_bucket(HT,First).
+    
+
 pdt_set(HT,Key,Value):-
     pdt_remove_all(HT,Key),
     pdt_put(HT,Key,Value).
 
 pdt_put(HT,Key,Value):-
 	hash_key(HT,Key,HashKey),
-	recordz(HashKey,entry(Key,Value,Pre,Post)),
-	link_entry(HT,entry(Key,Value,Pre,Post)).
-
-pdt_get(HT,Key,Value):-
-    ground(Key),
-	hash_key(HT,Key,HashKey),
-	recorded(HashKey,entry(Key,Value,_,_)).
-pdt_get(HT,Key,Value):-
-    var(Key),
-    current_head(HT,Head),
-    next_entry(HT,Head,entry(Key,Value,_,_)).
-    
-
-next_entry(_,Entry,Entry):-
-	nonvar(Entry).
-next_entry(HT,entry(_,_,_,Post),Next):-
-    next_entry(HT,Post,Next).
+	use_bucket(HT,HashKey),
+	recordz(HashKey,entry(Key,Value,Pre,Post)).
     
   
 
@@ -44,29 +53,31 @@ pdt_remove(HT,Key,Value):-
     hash_key(HT,Key,HashKey),
     recorded(HashKey,entry(Key,Value,Pre,Post),Ref),
     erase(Ref),
-    unlink_entry(HT,entry(Key,Value,Pre,Post)).
+	unuse_bucket(HT,HashKey).
 
 
 pdt_remove_all(HT,Key,Value):-
     hash_key(HT,Key,HashKey),
-    remove_all(HT,HashKey,Key,Value).
+    remove_all(HT,HashKey,Key,Value),
+    unuse_bucket(HT,HashKey).
+
+pdt_remove_all(HT,Key):-
+    hash_key(HT,Key,HashKey),
+    remove_all(HT,HashKey,Key),
+    unuse_bucket(HT,HashKey).
+
 
 remove_all(HT,HashKey,Key,Value):-
     recorded(HashKey,entry(Key,Value,Pre,Post),Ref),
     erase(Ref),
-    unlink_entry(HT,entry(Key,Value,Pre,Post)),
     \+ recorded(HashKey,entry(Key,Value,_,_)),
     !.
 remove_all(_,_,_,_).    
 
-pdt_remove_all(HT,Key):-
-    hash_key(HT,Key,HashKey),
-    remove_all(HT,HashKey,Key).
 
 remove_all(HT,HashKey,Key):-    
     recorded(HashKey,entry(Key,_,Pre,Post),Ref),
     erase(Ref),
-    unlinke_entry(HT,entry(Key,_,Pre,Post)),
     \+ recorded(HashKey,entry(Key,_,_,_)).
 remove_all(_,_,_).    
 
@@ -74,58 +85,6 @@ hash_key(HT,Key,HashKey):-
     hash_term(Key,HVal),
     atom_concat(HT,HVal,HashKey).
     
-link_entry(HT,entry(Key,Value,OldTail,_)):-
-    current_tail(HT,OldTail),
-    link_pre(HT,entry(Key,Value,OldTail,_)),
-    set_tail(HT,entry(Key,Value,OldTail,_)).
-
-link_pre(HT,entry(Key,Value,Pre,Post)):-
-    var(Pre),!,
-    set_head(HT,entry(Key,Value,Pre,Post)).
-link_pre(_,entry(Key,Value,Pre,Post)):-
-    nb_setarg(4,Pre,entry(Key,Value,Pre,Post)).
-    
-ensure_head_exists(HT,_):-
-    current_head(HT,_),!.
-ensure_head_exists(HT,Init):-
-    set_head(HT,Init).
-
-ensure_tail_exists(HT):-
-    current_tail(HT,_),!.
-ensure_tail_exists(HT):-
-    set_tail(HT,_).
-
-    
-unlink_entry(HT,entry(_,_,Pre,Post)):-
-	unlink_pre(HT,Pre,Post),
-	unlink_post(HT,Pre,Post).
-
-unlink_pre(HT,Pre,Post):-
-	current_head(HT,Pre),
-	set_head(Post).
-unlink_pre(_,Pre,Post):-
-	nb_setarg(	4,Pre,Post).
-
-unlink_post(_,_,Post):-
-	var(Post).
-unlink_post(_,Pre,Post):-
-	nb_setarg(	3,Post,Pre).	
-
-current_head(HT,Head):-
-    atom_concat(HT,'$head',K),
-    recorded(K,Head),!.
-current_head(_,_).
-
-current_tail(HT,Tail):-
-    atom_concat(HT,'$tail',K),
-    recorded(K,Tail),!.
-current_tail(_,_).    
-
-set_head(HT,Head):-
-    atom_concat(HT,'$head',K),
-	eraseall(K),
-    recordz(K,Head).
-
 eraseall(K):-
     recorded(K,_,Ref),
     erase(Ref),
@@ -135,8 +94,3 @@ eraseall(K):-
     \+ recorded(K,_).
 
 
-set_tail(HT,Tail):-
-    atom_concat(HT,'$tail',K),
-	eraseall(K),
-    recordz(K,Tail).
-    
