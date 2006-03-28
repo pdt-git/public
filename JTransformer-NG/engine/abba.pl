ad6b8818f8837437ce89d728889b29e5daa8ddb4
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-03-28 09:12:55 +0000

    moved abba.pl to ensure that it is loaded before error_handling.pl
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@924 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ JTransformer-NG/engine/abba.pl
@@ -0,0 +1,16 @@
+:- module(abba, [
+    edge/5,
+    node/3,
+    property/2]).
+    
+:- multifile edge/5.
+:- dynamic edge/5.
+
+:- multifile within/2.
+:- dynamic within/2.
+
+:- multifile node/3.
+:- dynamic node/3.
+
+:- multifile property/2.
+:- dynamic property/2.
