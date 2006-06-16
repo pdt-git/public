4d13b395605a6549853730baccb02a75889b2af1
dehaan <dehaan@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-06-16 15:23:24 +0000

    common elements extracted to AbstractStringBufferWriter
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1017 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- JTransformer-NG/src/org/cs3/jtransformer/internal/actions/FilteredStringBufferWriter.java
+++ JTransformer-NG/src/org/cs3/jtransformer/internal/actions/FilteredStringBufferWriter.java
@@ -11,59 +11,13 @@ import org.cs3.jtransformer.internal.astvisitor.IPrologWriter;
  * generated Stream. This is mainly useful for the implementation of the 
  * FactGenerator tests.
  */
-public class FilteredStringBufferWriter implements IPrologWriter {
+public class FilteredStringBufferWriter extends AbstractStringBufferWriter {
 	
-	int indention = 0;
-	boolean indent = true;
-	StringBuffer buf = new StringBuffer();
-	private boolean interpret=false;
 	private char clauseSeparator;
-	private Set filter;
 	
 	public FilteredStringBufferWriter(Set filter,char clauseSeparator){
-		this.filter = filter;
+		super(filter, clauseSeparator);
 		this.clauseSeparator = clauseSeparator;
-	}
-
-	/**
-	 * @see IPrologWriter#setIndentionLevel(int)
-	 */
-	
-	public void disableIndention(){
-		indent = false;
-	}
-	
-	public void setIndentionLevel(int i) {
-		if (i < 0)
-			throw new IllegalArgumentException("Negative Indention");
-		indention = i;
-	}
-	
-	/**
-	 * @see IPrologWriter#addIndention()
-	 */
-	
-	public void addIndention() {
-		indention++;
-	}
-	
-	/**
-	 * @see IPrologWriter#reduceIndention()
-	 */
-	
-	public void reduceIndention() {
-		if (indention - 1 < 0)
-			throw new IllegalStateException("Negative Indention");
-		indention--;
-	}
-	
-	/**
-	 * @see IPrologWriter#writeFact(String, String[])
-	 */
-	
-	public void writeFact(String string, String[] param) {
-		if(!filter.contains(string))
-			writeRule(string, param, null);
 	}
 	
 	/**
@@ -107,61 +61,4 @@ public class FilteredStringBufferWriter implements IPrologWriter {
 		this.buf.append(buf.toString());
 	}
 	
-	/**
-	 * This method removes any saved information from the buffer, and resets it
-	 * to an empty string.
-	 */
-	
-	public void reset(){
-		buf = new StringBuffer();
-	}
-	
-	/**
-	 * This method returns the facts and rules written to the Interface since
-	 * the creation of the Object, or the last call to reset(). This does <b>
-	 * not</b> reset the buffer!
-	 * @return the last string, including any newlines.
-	 */
-	
-	public String getLast(){
-		return buf.toString();
-	}
-	
-	/**
-	 * @see IPrologWriter#close()
-	 */
-	
-	public void close() {
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see org.cs3.pl.fileops.IPrologWriter#flush()
-	 */
-	public void flush() {
-		//nothing to do :-)
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see org.cs3.pl.fileops.IPrologWriter#setInterpretMode(boolean)
-	 */
-	public void setInterpretMode(boolean interpret) {
-		this.interpret = interpret;
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see org.cs3.pl.fileops.IPrologWriter#getInterpretMode()
-	 */
-	public boolean getInterpretMode() {
-		return interpret;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.cs3.pl.fileops.IPrologWriter#writeQuery(java.lang.String)
-	 */
-	public void writeQuery(String query) {
-		buf.append(":- " + query +".");
-	}
 }
