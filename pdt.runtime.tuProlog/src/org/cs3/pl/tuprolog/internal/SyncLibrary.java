6698bcbe8772acd0da3e6b886babf5e6937951b8
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-10-12 14:18:40 +0000

    added synchronization code for with_mutex_2
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1232 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/SyncLibrary.java
+++ pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/SyncLibrary.java
@@ -5,15 +5,18 @@ import alice.tuprolog.Term;
 
 public class SyncLibrary extends Library {
 	
+	private Object monitor = new Object();
+
 	public boolean sync_0() {
 		System.out.println("Sync was Called");
 		return true;
 	}
 	
 	public boolean with_mutex_2(Term permissions, Term goal) {
-		
-		this.getEngine().solve(goal);
-		return true;
+		synchronized (monitor) {
+			this.getEngine().solve(goal);
+			return true;
+		}
 	}
 
 }
