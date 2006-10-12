383819434c6d6b4aeabf6ab1c3100c83fe2f3dc0
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-10-12 14:24:23 +0000

    added sychronization via keys (atoms)
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1233 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/SyncLibrary.java
+++ pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/SyncLibrary.java
@@ -1,22 +1,38 @@
 package org.cs3.pl.tuprolog.internal;
 
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
+
 import alice.tuprolog.Library;
+import alice.tuprolog.Struct;
 import alice.tuprolog.Term;
 
 public class SyncLibrary extends Library {
 	
-	private Object monitor = new Object();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 476854551154694310L;
+	private Hashtable monitors = new Hashtable();
 
 	public boolean sync_0() {
 		System.out.println("Sync was Called");
 		return true;
 	}
-	
-	public boolean with_mutex_2(Term permissions, Term goal) {
+	/**
+	 * 
+	 * @param permissions key to synchronize on.
+	 * @param goal
+	 * @return
+	 */
+	public boolean with_mutex_2(Struct permissions, Term goal) {
+		Object monitor = monitors.get(permissions.getName());
+		if(monitor == null) {
+			monitor = new Object();
+			monitors.put(permissions.getName(), monitor);
+		}
 		synchronized (monitor) {
 			this.getEngine().solve(goal);
 			return true;
 		}
 	}
-
 }
