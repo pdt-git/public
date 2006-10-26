a13f345076c0d20293cda43a334167cb62cf7707
degenerl <degenerl@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-10-26 08:22:56 +0000

    PDT-166
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1301 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.core/engine/org/cs3/pdt/model/test_pdt_scope.pl
+++ pdt.core/engine/org/cs3/pdt/model/test_pdt_scope.pl
@@ -73,4 +73,21 @@ test(remote_resolve_contrib_inherit_heap,[true(fooval-bauval-baumval-bazval-BarN
     pdt_scope_resolve(S4,baz,Baz).
 
 
+test(remote_resolve_contrib_overide_stack,[true(fooval-bauval-otherbaumval-bazval-BarNs1==Foo,Bau,Baum,Baz,Bar)]):-
+    testscope(S),
+    pdt_namespace_new(bar_nsid,BarNs0),
+    pdt_namespace_bind(BarNs0,baum,baumval,BarNs1),
+    pdt_scope_bind(S,foo,fooval,S1),
+    pdt_scope_bind(S1,bar:bau,bauval,S2),
+    pdt_scope_bind(S2,bar,BarNs1,S3),
+    pdt_scope_bind(S3,baz,bazval,S4),
+    pdt_scope_bind(S4,bar:baum,otherbaumval,S5),
+    pdt_scope_resolve(S5,foo,Foo),  
+    pdt_scope_resolve(S5,bar:bau,Bau),
+    pdt_scope_resolve(S5,bar,Bar),
+    pdt_scope_resolve(S5,bar:baum,Baum),
+    pdt_scope_resolve(S5,baz,Baz).
+
+
+
 :- end_tests(scope).
