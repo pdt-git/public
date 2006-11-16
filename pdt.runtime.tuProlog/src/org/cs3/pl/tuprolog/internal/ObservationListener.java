e845c325e787f7bc5e884135b3d357f102aa0902
abdelhal <abdelhal@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-11-16 14:34:08 +0000

    Added Observation library
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1395 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/ObservationListener.java
@@ -0,0 +1,6 @@
+package org.cs3.pl.tuprolog.internal;
+
+public interface ObservationListener {
+
+	public void onUpdate(String msg);
+}
--- pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/ObserverLibrary.java
+++ pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/ObserverLibrary.java
@@ -1,6 +1,8 @@
 package org.cs3.pl.tuprolog.internal;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
 
 import alice.tuprolog.Library;
 import alice.tuprolog.MalformedGoalException;
@@ -8,56 +10,168 @@ import alice.tuprolog.SolveInfo;
 import alice.tuprolog.Term;
 
 public class ObserverLibrary extends Library {
-	private HashMap listeners = new HashMap();
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
-	public boolean observe_3(Term thread, Term subject, Term key) {
-		
-		SolveInfo info;
-		
-		try {
-			info = engine.solve("recorded( observer," +
-										" observation("+ thread + "," +" OtherSubject, "+ key+"))," +
-										"OtherSubject =@= "+subject+".");
-			if (info.isSuccess())
-				return false;
+	/**
+	 * Observations class is a hashtable used to store 
+	 * observations that we are interested, along with their observers thread.
+	 *  
+	 * @author Hasan Abdel Halim
+	 *
+	 */
+	private class Observations extends Hashtable{
+
+		private static final long serialVersionUID = 3533380814249692805L;
+
+		/**
+		 * Replaces the default containsKey method of HashTable so that it 
+		 * finds whether there is a key structually equal to the key provided
+		 * or not.
+		 * 
+		 * @param key to look for.
+		 * @return true if a structually equal key was found, false otherwise.
+		 */
+		public boolean containsKey( String key ){
 			
-			info = engine.solve("recordz(observer, observation("+thread+","+subject+","+key+")).");
-			
-			return  info.isSuccess();
-			
-		} catch (MalformedGoalException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			for (Iterator iter = keySet().iterator(); iter.hasNext();) {
+				
+				String subject = (String) iter.next();
+				try {
+					SolveInfo info = engine.solve(subject+"=@="+key+".");
+					
+					if (info.isSuccess())
+						return true;
+					
+				} catch (MalformedGoalException e) {
+					e.printStackTrace();
+				}				
+			}
+			return false;
 		}
 		
-		return false;
+		public Object get( String key ){
+			
+			for (Iterator iter = keySet().iterator(); iter.hasNext();) {
+				
+				String subject = (String) iter.next();
+				try {
+					SolveInfo info = engine.solve(subject+"=@="+key+".");
+					
+					if (info.isSuccess())
+						return super.get(subject);
+					
+				} catch (MalformedGoalException e) {
+					e.printStackTrace();
+				}				
+			}
+			
+			return null;
+		}
 		
 	}
-	
-	public boolean unobserve_2(Thread thread, Term subject) {
-		SolveInfo info ;
-		
-		try {
-			info = engine.solve("recorded( observer," +
-					" observation("+ thread + "," +" OtherSubject, Key), Ref)," +
-					"OtherSubject =@= "+subject+", erase(Ref).");
-			
-			return info.isSuccess();
-		} catch (MalformedGoalException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+	/**
+	 * Listeners class is a linkedlist which stores a key we are interested in,
+	 * along with its observers.
+	 * 
+	 * @author Hasan Abdel Halim
+	 *
+	 */
+	private class Listeners extends ArrayList{
+
+		private static final long serialVersionUID = 1L;
+		private Term key;
+
+		public Listeners(Term key) {
+			super();
+			this.key = key;
 		}
 		
-		return false;
+		public Term getKey() {
+			return key;
+		}
+
+		public void setKey(Term key) {
+			this.key = key;
+		}		
+	}
+	
+	/**
+	 * Notifier Thread is a thread used to inform all observers related to a specific subject
+	 * with a provided msg. 
+	 * 
+	 * @author Hasan Abdel Halim
+	 *
+	 */
+	private class Notifier extends Thread{
+		private String subject = "";
+		private String msg= "";
+		
+		public Notifier(String subject, String msg) {
+			this.subject = subject;
+			this.msg = msg;
+		}
+		
+		public void run(){
+			if ( subject == "" )
+				return;
+			
+			if ( observations.containsKey(subject) ){
+				
+				Listeners list = (Listeners) observations.get(subject);
+				
+				if (list == null )
+					return;
+				
+				for (Iterator iter = list.iterator(); iter.hasNext();) {
+					ObservationListener ls = (ObservationListener) iter.next();
+					ls.onUpdate(msg);					
+				}
+			}
+		}
+	}
+	
+	private Observations observations = new Observations();
+	
+	public boolean observe_2(Term subject, Term key) {
+		
+		if ( observations.containsKey( subject.toString() ) )
+			return false;
+
+		observations.put(subject.toString(), new Listeners(key) );	
+		return  true;	
+	}
+	
+	public boolean unobserve_1(Term subject) {
+
+		if ( !observations.containsKey( subject.toString() ))
+			return false;
+		
+		observations.remove(subject.toString());
+		return true;
 	}
 
 	public boolean notify_2(Term subject, Term msg){
 		
+		Notifier thread = new Notifier(subject.toString(), msg.toString());
+		
+		thread.start();
+		
 		return true;
 	}
+	
+	public void addListener(String subject, ObservationListener listener){
+		if ( observations.containsKey(subject) ){
+			Listeners list = (Listeners) observations.get(subject);
+			list.add(listener);
+		}
+	}
+	
+	public void removeListener(String subject, ObservationListener listener){
+		if ( observations.containsKey(subject) ){
+			Listeners list = (Listeners) observations.get(subject);
+			list.remove(listener);
+		}
+	}
+
 }
