7e9769ddc26f187d807d09873e403d0a907d7fe8
abdelhal <abdelhal@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-11-16 14:34:08 +0000

    Added Observation library
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1395 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/test/ObserverLibraryTest.java
+++ pdt.runtime.tuProlog/src/org/cs3/pl/tuprolog/internal/test/ObserverLibraryTest.java
@@ -1,6 +1,8 @@
 package org.cs3.pl.tuprolog.internal.test;
 
+import org.cs3.pl.tuprolog.internal.ObservationListener;
 import org.cs3.pl.tuprolog.internal.ObserverLibrary;
+import org.cs3.pl.tuprolog.internal.SWICompatibilityLibrary;
 
 import junit.framework.TestCase;
 import alice.tuprolog.MalformedGoalException;
@@ -14,6 +16,7 @@ public class ObserverLibraryTest extends TestCase {
 		super.setUp();
 		if ( engine == null ){
 			engine = new Prolog();
+			engine.loadLibrary(new SWICompatibilityLibrary());
 			engine.loadLibrary(new ObserverLibrary());
 		}
 	}
@@ -22,8 +25,38 @@ public class ObserverLibraryTest extends TestCase {
 		super.tearDown();
 	}
 	
-	public void testObserver() throws MalformedGoalException {
-		SolveInfo info = engine.solve("observe('thread' , test, hi).");
+	public void testObserve() throws MalformedGoalException {
+		SolveInfo info = engine.solve("observe(test, hi).");
+		assertTrue(info.isSuccess());
+	}
+	
+	public void testUnObserve() throws MalformedGoalException {
+		SolveInfo info = engine.solve("observe(observe_test(_,Y), firstKey).");
+		assertTrue(info.isSuccess());
+		
+		info = engine.solve("observe(observe_test(_,X), firstKey).");
+		assertFalse(info.isSuccess());
+		
+		info = engine.solve("unobserve(observe_test(_,_)).");
+		assertTrue(info.isSuccess());
+	}
+	
+	public void testNotify() throws MalformedGoalException{
+		SolveInfo info = engine.solve("observe(observe_test(_,Y), firstKey).");
+		assertTrue(info.isSuccess());
+
+		ObserverLibrary lib = (ObserverLibrary) engine.getLibrary("org.cs3.pl.tuprolog.internal.ObserverLibrary");
+		ObservationListener ls = new ObservationListener(){
+
+			public void onUpdate(String msg) {
+				System.err.println(msg);				
+			}
+			
+		};
+		
+		lib.addListener("observe_test(X,_)", ls );
+
+		info = engine.solve("notify(observe_test(_,_), 'just test for messages').");
 		assertTrue(info.isSuccess());
 	}
 
