60a8d10530119ca365e1ff6d2d9e2a080606617f
abdelhal <abdelhal@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2006-11-16 16:00:23 +0000

    removed module from module:predicate till we fix the problem of module operator (The warning about redefinition of ':' operator ).
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1397 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.runtime.tuProlog/src/resources/localisation.pl
+++ pdt.runtime.tuProlog/src/resources/localisation.pl
@@ -5,46 +5,46 @@
        [company_nearby/3,
        company_nearby/4]).
 
-sync:depends(magicmap:location/4, localisation:company_nearby/3).
-sync:depends(magicmap:location/4, localisation:company_nearby/4).
+depends(location/4, company_nearby/3).
+depends(location/4, company_nearby/4).
 
 :- use_module(magicmap).
 
 %------ BIT: --------
-%magicmap:position('StandIBM', gps(50.72031111111, 7.120469444444, 0)).
+%position('StandIBM', gps(50.72031111111, 7.120469444444, 0)).
 %IBM: vor Eingang von 1.32
 %
-%magicmap:position('StandPhilips', gps(50.720494444, 7.120583333333, 0)).
+%position('StandPhilips', gps(50.720494444, 7.120583333333, 0)).
 %Philips: vor Tür zum Treppenhaus
 %
-% magicmap:position('StandSamsung', gps(50.7204027777777, 7.12040277777, 0)).
+% position('StandSamsung', gps(50.7204027777777, 7.12040277777, 0)).
 %Samsung: an der Balkontür in der Küche
 %--------------------
 
 %altbau: 50.757  7.0769
-% magicmap:distance(gps(50.7511158333, 7.0971722, 0),gps(50.757,  7.0769,0),2,34445).
+% distance(gps(50.7511158333, 7.0971722, 0),gps(50.757,  7.0769,0),2,34445).
 
 
 % IBM: Ende zum Neubau
-magicmap:position('StandIBM', gps(50.7511158333, 7.0971722, 0)).
-magicmap:position_attr('StandIBM', 'name', 'IBM').
+position('StandIBM', gps(50.7511158333, 7.0971722, 0)).
+position_attr('StandIBM', 'name', 'IBM').
 
 % Philips: Mitte
-magicmap:position('StandPhilips', gps(50.751410, 7.097026, 0)).
-magicmap:position_attr('StandPhilips', 'name', 'Philips').
+position('StandPhilips', gps(50.751410, 7.097026, 0)).
+position_attr('StandPhilips', 'name', 'Philips').
 
 % Samsung: Aussenende Altbau
-magicmap:position('StandSamsung', gps(50.751622, 7.0969027, 0)).
-magicmap:position_attr('StandSamsung', 'name', 'Samsung').
+position('StandSamsung', gps(50.751622, 7.0969027, 0)).
+position_attr('StandSamsung', 'name', 'Samsung').
 
 
 
 company_nearby_tmp(MyMAC, Company, Distance) :-
-%  magicmap:position(MyMAC, Gpscurr),
-  magicmap:location(MyMAC,Lat,Long,Alt),
-  magicmap:position(MAC, Gpscomp),
-  magicmap:distance(gps(Lat,Long,Alt), Gpscomp, Distance),
-  magicmap:position_attr(MAC, 'name', Company).
+%  position(MyMAC, Gpscurr),
+  location(MyMAC,Lat,Long,Alt),
+  position(MAC, Gpscomp),
+  distance(gps(Lat,Long,Alt), Gpscomp, Distance),
+  position_attr(MAC, 'name', Company).
 
 company_nearby(MAC,Company, Distance) :-
   setof([Distance, Company], company_nearby_tmp(MAC, Company, Distance), Cs),
--- pdt.runtime.tuProlog/src/resources/sync_test.pl
+++ pdt.runtime.tuProlog/src/resources/sync_test.pl
@@ -6,67 +6,67 @@
 :- [localisation].
 
 test(separate_functor_arity_module_safe):-
-    sync:separate_functor_arity_module_safe(m:f/1, m:f, 1),
-    sync:separate_functor_arity_module_safe('m:f/1', m:f, 1),
-    sync:separate_functor_arity_module_safe(f/2, f, 2).
+    separate_functor_arity_module_safe(m:f/1, m:f, 1),
+    separate_functor_arity_module_safe('m:f/1', m:f, 1),
+    separate_functor_arity_module_safe(f/2, f, 2).
     
 test(evaluate_and_store_idb):-
-    sync:evaluate_and_store_idb(mymodule:non_existing(_t)),
-    clause(sync:idb(mymodule:non_existing(_A)),fail).
+    evaluate_and_store_idb(mymodule:non_existing(_t)),
+    clause(idb(mymodule:non_existing(_A)),fail).
 
 tearDown(evaluate_and_store_idb):-
-    retractall(sync:idb(_)).
+    retractall(idb(_)).
 
 
 test(update_idb_and_changed):-
     Term = testmodule:newpred(_A,b),
     term_to_atom(Term, Atom),
-    sync:init_idb(Atom),
+    init_idb(Atom),
     assert(testmodule:newpred(a,b)),
-    sync:term_ref(Term,Ref),
-	sync:update_idb(Term, Ref),
+    term_ref(Term,Ref),
+	update_idb(Term, Ref),
 	testmodule:newpred(a,b),
-    clause(sync:idb_copy(Ref,testmodule:newpred(_,_)),fail),
-    sync:changed(Ref).
+    clause(idb_copy(Ref,testmodule:newpred(_,_)),fail),
+    changed(Ref).
 
 tearDown(update_idb_and_changed):-
-	sync:term_ref(testmodule:newpred(_A,b), Ref),
-	sync:remove_term_idb(Ref),
+	term_ref(testmodule:newpred(_A,b), Ref),
+	remove_term_idb(Ref),
 	retractall(testmodule:newpred(_,_)).
     
 tearDown('update_idb and changed'):-
-    retractall(sync:idb(_)),
-    retractall(sync:idb_copy(_)),
+    retractall(idb(_)),
+    retractall(idb_copy(_)),
     retractall(testmodule:newpred(_,_)).
 
 setUp(term_ref) :-
-    sync:init_term_ref(term(a,_B),_Ref).
+    init_term_ref(term(a,_B),_Ref).
     	
 test(term_ref):-
-    assert_true(sync:term_ref(term(a,_C),_Ref)).
+    assert_true(term_ref(term(a,_C),_Ref)).
     
 tearDown(term_ref):-
-	sync:term_ref(term(a,_C),Ref),
-	sync:remove_term_idb(Ref).
+	term_ref(term(a,_C),Ref),
+	remove_term_idb(Ref).
 
 setUp(init_idb):-
-    assert(magicmap:location('00-09-2D-53-27-3A', 50.73, 7.12, 0)),
-    retractall(sync:idb(_,_)).
+    assert(location('00-09-2D-53-27-3A', 50.73, 7.12, 0)),
+    retractall(idb(_,_)).
     
 test(init_idb) :-
-  sync:init_idb('localisation:company_nearby(_,_,_,1000)'),
-  sync:term_ref(localisation:company_nearby(_,_,_,1000), Ref),
+  init_idb('company_nearby(_,_,_,1000)'),
+  term_ref(company_nearby(_,_,_,1000), Ref),
 
 
-  retract(magicmap:location('00-09-2D-53-27-3A', 50.73, 7.12, 0)),
-  assert(magicmap:location('00-09-2D-53-27-3A', 50.73, 7.123, 0)),
+  retract(location('00-09-2D-53-27-3A', 50.73, 7.12, 0)),
+  assert(location('00-09-2D-53-27-3A', 50.73, 7.123, 0)),
 
-  sync:update_idb(localisation:company_nearby(_,_,_,1000), Ref),
+  update_idb(company_nearby(_,_,_,1000), Ref),
 
-  sync:changed(Ref).
+  changed(Ref).
   
 tearDown(init_idb):-
-    retractall(magicmap:location(_,_,_,_)),
-    sync:term_ref(localisation:company_nearby(_,_,_,1000), Ref),
-    sync:remove_term_idb(Ref).
+    retractall(location(_,_,_,_)),
+    term_ref(localisation:company_nearby(_,_,_,1000), Ref),
+    remove_term_idb(Ref).
  
