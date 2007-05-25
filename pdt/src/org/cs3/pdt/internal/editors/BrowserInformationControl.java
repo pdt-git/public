213ec5719fb22dcf6f198f8f4f2fd732ebfa57be
rho <rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2007-05-25 16:27:08 +0000

    organized imports
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1526 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt/src/org/cs3/pdt/internal/editors/BrowserInformationControl.java
+++ pdt/src/org/cs3/pdt/internal/editors/BrowserInformationControl.java
@@ -17,7 +17,12 @@ import java.util.Iterator;
 
 import org.cs3.pdt.ui.util.UIUtils;
 import org.eclipse.core.runtime.ListenerList;
-
+import org.eclipse.jdt.internal.ui.text.HTML2TextReader;
+import org.eclipse.jdt.internal.ui.text.IInformationControlExtension4;
+import org.eclipse.jface.text.IInformationControl;
+import org.eclipse.jface.text.IInformationControlExtension;
+import org.eclipse.jface.text.IInformationControlExtension3;
+import org.eclipse.jface.text.TextPresentation;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.SWTError;
 import org.eclipse.swt.browser.Browser;
@@ -46,16 +51,6 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.Shell;
-
-import org.eclipse.jface.text.IInformationControl;
-import org.eclipse.jface.text.IInformationControlExtension;
-import org.eclipse.jface.text.IInformationControlExtension3;
-import org.eclipse.jface.text.TextPresentation;
-
-
-import org.eclipse.jdt.internal.ui.text.HTML2TextReader;
-import org.eclipse.jdt.internal.ui.text.HTMLPrinter;
-import org.eclipse.jdt.internal.ui.text.IInformationControlExtension4;
 
 
 /**
