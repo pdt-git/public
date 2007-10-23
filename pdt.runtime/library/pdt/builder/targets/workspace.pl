b7eaa28d1b437cb9c47355d77d33b9ce27842a9a
lukas.degener <lukas.degener@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2007-10-23 16:15:39 +0000

    w.i.p.
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1651 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.runtime/library/pdt/builder/targets/workspace.pl
+++ pdt.runtime/library/pdt/builder/targets/workspace.pl
@@ -1,7 +1,7 @@
 :- module(workspace,[]).
 :-use_module(library('builder/builder')).
 :-use_module(library('pef/pef_base')).
-:-use_module(library('pef/pef_api')).
+:-use_module(library('util/pdt_regex')).
 
 pdt_builder:build_hook(directory(Abs,Include,Exclude)):-
     (	exists_directory(Abs)
@@ -12,17 +12,49 @@ pdt_builder:build_hook(directory(Abs,Include,Exclude)):-
     	pef_property_assert([pef=Dir,key=file_count,value=Count])
     ;	throw(no_such_dir(Abs))
     ).
+pdt_builder:build_hook(file(Abs)):-
+	(	exists_file(Abs)
+    ->	pef_reserve_id(pef_file,FID),
+    	pef_file_assert([id=FID,path=Abs])    	
+    ;	throw(no_such_dir(Abs))
+    ).
+pdt_builder:build_hook(project(Name)):-
+    (	pef_project_query([name=Name, id=Project])
+    ->	forall(
+    		pef_source_path_query([project=Project, 
+    								include_pattern=IP,
+    								exclude_pattern=EP,
+    								path=Path]
+    							),
+    	    pdt_request_target(directory(Path,IP,EP))
+    	)
+    ;	throw(no_such_project(Name))
+    ).
+pdt_builder:build_hook(workspace):-
+    forall(pef_project_query([name=Name]),pdt_request_target(project(Name))).
+    
+    
 
 
-
-process_entries([],C,C).
-process_entries([file(Abs)|Entries],C,C2):-
-    C1 is C + 1,
+process_entries([],_,C,C).
+process_entries([file(Abs)|Entries],Dir,C,C2):-
+    !,
+    C1 is C + 1,  
+    pdt_request_target(file(Abs)),  
+    pef_file_query([path=Abs,id=FID]),
+    pef_directory_entry_assert([parent=Dir,child=FID]),
+    process_entries(Entries,Dir,C1,C2).
+process_entries([directory(Abs,IP,EP)|Entries],Dir,C,C2):-
+    pdt_request_target(directory(Abs,IP,EP)),
+    pef_directory_query([path=Abs,include_pattern=IP,exclude_pattern=EP,id=SubDir]),
+    pef_property_query([pef=SubDir,key=file_count,value=SubC]),
+    C1 is C + SubC,
+    pef_directory_entry_assert([parent=Dir,child=SubDir]),
     pdt_request_target(file(Abs)),
-    process_entries(Entries,C1,C2).
+    process_entries(Entries,Dir,C1,C2).
     	
 find_entries(Abs,IP,EP,Deps):-
-	atom_concat(Dir,'/*',LsPattern),
+	atom_concat(Abs,'/*',LsPattern),
 	expand_file_name(LsPattern,Files),
 	filter(Files,IP,EP,Deps).
 	
