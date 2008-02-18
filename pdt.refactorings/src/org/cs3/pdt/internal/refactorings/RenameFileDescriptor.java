1999c4a13fb2946e86b66bd7e92e86d5c893866b
lukas.degener <lukas.degener@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2008-02-18 10:14:27 +0000

    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1736 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ pdt.refactorings/src/org/cs3/pdt/internal/refactorings/RenameFileDescriptor.java
@@ -0,0 +1,13 @@
+package org.cs3.pdt.internal.refactorings;
+
+import org.cs3.pdt.transform.PrologRefactoringDescriptor;
+
+
+
+public class RenameFileDescriptor extends PrologRefactoringDescriptor {
+
+	public RenameFileDescriptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
