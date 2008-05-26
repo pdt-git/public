d7fc7b49e6f8c38fc44f6d054f10b4f8424857af
lukas.degener <lukas.degener@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2008-05-26 14:58:26 +0000

    added Testcase for PDT-295
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1906 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ pdt.runtime/src/org/cs3/pl/prolog/tests/LifeCycleHookTest.java
@@ -0,0 +1,102 @@
+package org.cs3.pl.prolog.tests;
+
+import org.cs3.pl.common.Debug;
+import org.cs3.pl.prolog.LifeCycleHook3;
+import org.cs3.pl.prolog.PrologInterface;
+import org.cs3.pl.prolog.PrologInterface2;
+import org.cs3.pl.prolog.PrologInterfaceException;
+import org.cs3.pl.prolog.PrologInterfaceFactory;
+import org.cs3.pl.prolog.PrologSession;
+
+import junit.framework.TestCase;
+
+public class LifeCycleHookTest extends TestCase {
+
+
+	private class MyHook implements LifeCycleHook3{
+
+		private int lateInit;
+		private int onError;
+		private int setData;
+		private Object data;
+		private int afterInit;
+		private int beforeShutdown;
+		private int onInit;
+
+		public void lateInit(PrologInterface pif) {
+			lateInit++;
+			
+		}
+
+		public void onError(PrologInterface pif) {
+			onError++;
+			
+		}
+
+		public void setData(Object data) {
+			this.data=data;
+			setData++;
+			
+		}
+
+		public void afterInit(PrologInterface pif)
+				throws PrologInterfaceException {
+			afterInit++;
+			
+		}
+
+		public void beforeShutdown(PrologInterface pif, PrologSession session)
+				throws PrologInterfaceException {
+			beforeShutdown++;
+			
+		}
+
+		public void onInit(PrologInterface pif, PrologSession initSession)
+				throws PrologInterfaceException {
+			onInit++;
+			
+		}
+		
+	}
+	
+	
+	private PrologInterface2 pif;
+
+	protected void setUp() throws Exception {
+		Debug.setDebugLevel(Debug.LEVEL_DEBUG);
+		this.pif=(PrologInterface2) PrologInterfaceFactory.newInstance().create();
+		
+	}
+
+	public void testPDT_295_00() throws Exception{
+		MyHook X = new MyHook();
+		pif.addLifeCycleHook(X, "X", new String[0]);
+		pif.getSession(PrologInterface.NONE).dispose();
+		pif.stop();
+		assertEquals(1,X.onInit);
+		assertEquals(1,X.afterInit);
+		assertEquals(1,X.beforeShutdown);
+		/*
+		 * -Dpif.file_search_path="library=/home/lukas/workspace/pdt.runtime.pifcom/library/pifcom"
+-Dpif.implementation="org.cs3.pifcom.Factory"
+		 */
+		
+	}
+	
+	public void testPDT_295_01() throws Exception{
+		MyHook X = new MyHook();
+		pif.addLifeCycleHook(X, "X", new String[0]);
+		pif.removeLifeCycleHook(X,"X");
+		pif.addLifeCycleHook(X, "X", new String[0]);
+		pif.getSession(PrologInterface.NONE).dispose();
+		pif.stop();
+		assertEquals(1,X.onInit);
+		assertEquals(1,X.afterInit);
+		assertEquals(1,X.beforeShutdown);
+		/*
+		 * -Dpif.file_search_path="library=/home/lukas/workspace/pdt.runtime.pifcom/library/pifcom"
+-Dpif.implementation="org.cs3.pifcom.Factory"
+		 */
+		
+	}
+}
