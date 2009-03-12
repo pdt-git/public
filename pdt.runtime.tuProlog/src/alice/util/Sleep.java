c1a2aa96ea3fb240d8680602557b8aa284e7b7a8
eva.stoewe <eva.stoewe@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2009-03-12 16:26:47 +0000

    "Merged" the former BRANCH_JT_2_4_X (working branch) to the actual former trunk (Lukas Degeners DA) so the sources of the branch are now the ones in the trunk.
    I tried to let the projects not affected by the branch stay in the trunk but it did not work as I supposed it would.
    
    You can find the former trunk sources in BRANCH_LUKAS_DA/
    
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@1979 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.runtime.tuProlog/src/alice/util/Sleep.java
+++
@@ -1,8 +0,0 @@
-package alice.util;
-
-public class Sleep {
-    static public void main(String[] args) throws Exception {
-        Thread.currentThread().sleep(Integer.parseInt(args[0]));
-        System.exit(0);
-    }
-}
