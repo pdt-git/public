c6360a9d560474683b10e43b4e0370fb28a07e25
frank.muehlschlegel <frank.muehlschlegel@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2009-11-20 15:09:05 +0000

    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@2250 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ pdt.runtime.pifcom/src/org/cs3/preferences/PIFComConstants.java
@@ -0,0 +1,8 @@
+package org.cs3.pdt.runtime.pifcom.preferences;
+
+public class PIFComConstants {
+
+	public static final String PIF_PORT_UDP = "pif.udp_port";
+	public static final String PIF_PORT_TCP = "pif.tcp_port";
+
+}
