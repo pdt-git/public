891f4a39394c5474eba783cf463c6abc12a53bc4
eva.stoewe <eva.stoewe@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2010-02-18 11:53:04 +0000

    a standalone SocketPrologInterface to get Cultivate ready to start headless tests
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@2326 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ pdt.runtime.socket/src/org/cs3/pl/prolog/internal/socket/StandaloneSocketPrologInterface.java
@@ -0,0 +1,29 @@
+package org.cs3.pl.prolog.internal.socket;
+
+import org.cs3.pdt.runtime.preferences.PreferenceInitializer;
+
+public class StandaloneSocketPrologInterface extends SocketPrologInterface {
+
+	public StandaloneSocketPrologInterface(String name) {	
+		super(name);
+	}
+	
+	@Override
+	protected void initPath() {
+		;
+	}
+	
+	@Override
+	public void initOptions(){
+		setStandAloneServer("false");
+		setHost("localhost");
+		setExecutable(PreferenceInitializer.guessExecutableName());
+		setEnvironment(PreferenceInitializer.guessEnvironmentVariables());
+		setTimeout("15000");
+		setServerPort("9944");
+		setHidePlwin(true);
+		setCreateLogs(false);
+		setUseSessionPooling("true");
+		setServerLogDir(System.getProperty("java.io.tmpdir"));		
+	}
+}
