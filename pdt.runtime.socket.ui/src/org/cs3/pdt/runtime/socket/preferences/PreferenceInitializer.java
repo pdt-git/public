02a25f6d881cd4cf1623a15a998ed354205d972c
tobias.rho <tobias.rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2010-03-12 14:49:49 +0000

    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@2359 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ pdt.runtime.socket.ui/src/org/cs3/pdt/runtime/socket/preferences/PreferenceInitializer.java
@@ -0,0 +1,59 @@
+package org.cs3.pdt.runtime.socket.preferences;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.cs3.pdt.runtime.PrologRuntimePlugin;
+import org.cs3.pl.common.Debug;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#
+	 * initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		
+		PrologRuntimePlugin plugin = PrologRuntimePlugin.getDefault();
+		IPreferenceStore store = plugin.getPreferenceStore();
+		store.setDefault(SocketPrologInterfacePreferences.PREF_USE_POOL, "true");
+		store.setDefault(SocketPrologInterfacePreferences.PREF_CREATE_SERVER_LOGS, false);
+		store.setDefault(SocketPrologInterfacePreferences.PREF_PORT, 9944);
+		store.setDefault(SocketPrologInterfacePreferences.PREF_HIDE_PLWIN, true);
+		
+		
+		String location = "";
+		try {
+			location = getLocation();
+		} catch (IOException e) {
+			Debug.report(e);
+			Debug.error("Could not find plugin installation dir.");
+		}
+		store.setDefault(SocketPrologInterfacePreferences.PREF_SERVER_LOGDIR,location);
+	}
+
+	private String getLocation() throws IOException {
+		URL url = PrologRuntimePlugin.getDefault().getBundle().getEntry("/");
+		String location = getAbsolutePathForURL(url);
+		if (location.charAt(location.length() - 1) == File.separatorChar)
+			location = location.substring(0, location.length() - 1);
+		return location;
+	}
+
+	private String getAbsolutePathForURL(URL url) throws IOException {
+		String fileName = FileLocator.toFileURL(url).getFile();
+		File file = new File(fileName);
+		String absolutePath = file.getAbsolutePath();
+		return absolutePath;
+	}
+	
+}
