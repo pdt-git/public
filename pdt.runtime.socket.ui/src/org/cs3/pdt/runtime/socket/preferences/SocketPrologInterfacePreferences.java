a77172a84e2be878ddd6941da9f76c39c1ab9ea3
tobias.rho <tobias.rho@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2010-03-12 14:49:49 +0000

    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@2359 5ac03ce8-283d-0410-b4d3-cd106b6a6105

---
+++ pdt.runtime.socket.ui/src/org/cs3/pdt/runtime/socket/preferences/SocketPrologInterfacePreferences.java
@@ -0,0 +1,15 @@
+package org.cs3.pdt.runtime.socket.preferences;
+
+public interface SocketPrologInterfacePreferences {
+
+	/**
+	 * Copied from pdt.runtime.socket!
+	 */
+	public static final String PREF_PORT = "pif.port";
+	public static final String PREF_HIDE_PLWIN = "pif.hide_plwin";
+	public static final String PREF_SERVER_LOGDIR = "pif.server_log_dir";
+	public static final String PREF_MAIN_FILE = "pif.main_file";
+	public final static String PREF_USE_POOL = "pif.use_pool";
+	public static final String PREF_CREATE_SERVER_LOGS = "pif.create_logs";
+
+}
