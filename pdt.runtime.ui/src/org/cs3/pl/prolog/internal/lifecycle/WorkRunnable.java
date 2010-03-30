43ed73172ec4ac803c9c8bd0cd3790a659f67d3f
eva.stoewe <eva.stoewe@5ac03ce8-283d-0410-b4d3-cd106b6a6105>
2010-03-30 11:20:32 +0000

    deleted - should have stayed in pdt.runtime
    git-svn-id: https://svn.iai.uni-bonn.de/repos/IAI_Software/pdt/trunk@2402 5ac03ce8-283d-0410-b4d3-cd106b6a6105

--- pdt.runtime.ui/src/org/cs3/pl/prolog/internal/lifecycle/WorkRunnable.java
+++
@@ -1,7 +0,0 @@
-package org.cs3.pl.prolog.internal.lifecycle;
-
-import org.cs3.pl.prolog.PrologInterfaceException;
-
-public interface WorkRunnable {	
-	public void run() throws PrologInterfaceException;
-}
