7714db51a37126352408d97d52e4ef450017987e
Eva Stoewe <stoewe@iai.uni-bonn.de>
2010-04-28 11:39:35 +0200

    removed pdt.runtime.pifcom because it was not supported since tag end_of_pifcom_support

--- pdt.runtime.pifcom/src/org/cs3/pdt/runtime/pifcom/preferences/PIFComConstants.java
+++
@@ -1,8 +0,0 @@
-package org.cs3.pdt.runtime.pifcom.preferences;
-
-public class PIFComConstants {
-
-	public static final String PIF_PORT_UDP = "pif.udp_port";
-	public static final String PIF_PORT_TCP = "pif.tcp_port";
-
-}
