4fc0f99b710b90bd83589777527e1609fe46198e
Jan Nonnen <nonnen@cs.uni-bonn.de>
2010-08-25 14:31:06 +0200

    extracted actions into classes added wheel zoom

---
+++ yworks/src/pdt/y/view/actions/LoadAction.java
@@ -0,0 +1,58 @@
+package pdt.y.view.actions;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+
+import pdt.y.main.GraphPDTDemo;
+
+/**
+	 * Action that loads the current graph from a file in GraphML format.
+	 */
+	public class LoadAction extends AbstractAction {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 65359810633139537L;
+		JFileChooser chooser;
+		private GraphPDTDemo frame;
+
+		public LoadAction(GraphPDTDemo frame) {
+			super("Load...");
+			chooser = null;
+			this.frame = frame;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			if (chooser == null) {
+				chooser = new JFileChooser();
+				chooser.setAcceptAllFileFilterUsed(false);
+				chooser.addChoosableFileFilter(new FileFilter() {
+					public boolean accept(File f) {
+						return f.isDirectory() || f.getName().endsWith(".graphml");
+					}
+
+					public String getDescription() {
+						return "GraphML Format (.graphml)";
+					}
+				});
+			}
+			chooser.showOpenDialog(frame);
+			URL resource = null;
+			if (chooser.getSelectedFile() == null )
+				return;
+			
+			try {
+				resource = chooser.getSelectedFile().toURI().toURL();
+			} catch (MalformedURLException urlex) {
+				urlex.printStackTrace();
+			}
+			frame.loadGraph(resource);
+		}
+	}
+	
