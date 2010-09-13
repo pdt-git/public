579be951d4cb7c587a6e83c45af6373b7a823874
Eva Stoewe <stoewe@iai.uni-bonn.de>
2010-09-13 14:50:55 +0200

    Added code from a demo to get graphs more compact - does not work as intended at the moment

--- yworks/src/pdt/y/model/GraphLayout.java
+++ yworks/src/pdt/y/model/GraphLayout.java
@@ -1,11 +1,16 @@
 package pdt.y.model;
 
+import demo.view.hierarchic.AdaptNodeToLabelWidths;
+import demo.view.hierarchic.LabelAwareDrawingDistanceCalculator;
 import y.layout.CompositeLayoutStage;
 import y.layout.LayoutOrientation;
 import y.layout.LayoutStage;
 import y.layout.Layouter;
 import y.layout.OrientationLayouter;
 import y.layout.hierarchic.IncrementalHierarchicLayouter;
+import y.layout.hierarchic.incremental.DrawingDistanceCalculator;
+import y.layout.hierarchic.incremental.HierarchicLayouter;
+import y.layout.hierarchic.incremental.SimplexNodePlacer;
 import y.layout.router.ChannelEdgeRouter;
 import y.layout.router.OrthogonalEdgeRouter;
 
@@ -34,27 +39,55 @@ public class GraphLayout {
 
 
 	protected Layouter createCoreLayout() {
-		IncrementalHierarchicLayouter layout = new IncrementalHierarchicLayouter();
+		IncrementalHierarchicLayouter layouter = new IncrementalHierarchicLayouter();
 		
 		//set some options
-		layout.getNodeLayoutDescriptor().setMinimumLayerHeight(10);
-		layout.getNodeLayoutDescriptor().setMinimumDistance(10);
+		layouter.getNodeLayoutDescriptor().setMinimumLayerHeight(10);
+		layouter.getNodeLayoutDescriptor().setMinimumDistance(10);
 
+		
 		//use left-to-right layout orientation
 		OrientationLayouter ol = new OrientationLayouter();
 		ol.setOrientation(LayoutOrientation.BOTTOM_TO_TOP);
-		layout.setOrientationLayouter(ol);
-		layout.setBackloopRoutingEnabled(true);
-		layout.setFromScratchLayeringStrategy(IncrementalHierarchicLayouter.LAYERING_STRATEGY_HIERARCHICAL_TIGHT_TREE);
+		layouter.setOrientationLayouter(ol);
+		layouter.setBackloopRoutingEnabled(true);
+//		layouter.setFromScratchLayeringStrategy(IncrementalHierarchicLayouter.LAYERING_STRATEGY_HIERARCHICAL_TIGHT_TREE);
 		//layout.setFromScratchLayeringStrategy(IncrementalHierarchicLayouter.LAYERING_STRATEGY_HIERARCHICAL_TOPMOST);
-		layout.setGroupAlignmentPolicy(IncrementalHierarchicLayouter.POLICY_ALIGN_GROUPS_CENTER);
+//		layouter.setGroupAlignmentPolicy(IncrementalHierarchicLayouter.POLICY_ALIGN_GROUPS_CENTER);
 		//layout.setSubgraphLayouter(arg0);	// here is something for stages
-		layout.setGroupCompactionEnabled(true);
-		layout.setRecursiveGroupLayeringEnabled(true);
-		layout.setAutomaticEdgeGroupingEnabled(true);
+//		layouter.setGroupCompactionEnabled(true);
+		layouter.setRecursiveGroupLayeringEnabled(true);
+//		layouter.setAutomaticEdgeGroupingEnabled(true);
 		
+	    final AdaptNodeToLabelWidths stage = new AdaptNodeToLabelWidths();
+	    stage.setAdaptGroupNodesOnly(false);
+	    stage.setGroupLabelWidthAdjustment(20);
+
+	    layouter.prependStage(stage);
+	    final HierarchicLayouter hl = layouter.getHierarchicLayouter();
+	    final DrawingDistanceCalculator ddc = hl.getDrawingDistanceCalculator();
+	    // drawing distance calculator that calculates a minimum width for all
+	    // non-empty group nodes in such a way that node.x is less than or equal to
+	    // the minimum x-coordinate of all node labels and node.x + node.width is
+	    // greater than or equal to the maximum x-coordinate of all node labels
+	    //
+	    // this is done due to the fact that non-empty group nodes are an exception
+	    // to the general "no node resizing" policy of yFiles layout algorithms
+	    final LabelAwareDrawingDistanceCalculator laddc =
+	            new LabelAwareDrawingDistanceCalculator(ddc);
+	    laddc.setGroupLabelWidthAdjustment(0.0);
+	    hl.setDrawingDistanceCalculator(laddc);
+
+	    // horizontal group compaction tries to prevent
+	    // IncrementalHierarchicLayouter from being to generous when calculating
+	    // group node sizes for non-empty group nodes
+	    //if (options.getBool(OPTION_USE_HORIZONTAL_GROUP_COMPACTION)) {
+	      final SimplexNodePlacer snp = new SimplexNodePlacer();
+	      snp.setGroupCompactionStrategy(SimplexNodePlacer.GROUP_COMPACTION_MAX);
+	      layouter.setNodePlacer(snp);
+	    //}
 		
-		return layout;
+		return layouter;
 	}
 	
 	public Layouter getLayouter(){
