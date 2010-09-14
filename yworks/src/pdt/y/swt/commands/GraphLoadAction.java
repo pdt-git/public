2385a7463e3f03e7ef5d6d145a07732163e02b4a
Eva Stoewe <stoewe@iai.uni-bonn.de>
2010-09-14 16:26:19 +0200

    organized imports

--- yworks/src/pdt/y/swt/commands/GraphLoadAction.java
+++ yworks/src/pdt/y/swt/commands/GraphLoadAction.java
@@ -3,6 +3,9 @@ package pdt.y.swt.commands;
 import java.io.File;
 import java.net.MalformedURLException;
 
+import org.cs3.pdt.runtime.PrologInterfaceRegistry;
+import org.cs3.pl.prolog.PrologInterface;
+import org.cs3.pl.prolog.internal.AbstractPrologInterface;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.widgets.FileDialog;
@@ -47,8 +50,7 @@ public class GraphLoadAction  extends Action {
 			e.printStackTrace();
 		}
 
-		
-		
+	
 		
 	}
 }
