dfceb4d1297f95b7b7e71a7a847b22a53ac27b09
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-05-17 13:26:11 +0200

    PDTPlugin now has a static method getActivePage

--- pdt/src/org/cs3/pdt/internal/actions/OpenConsoleAction.java
+++ pdt/src/org/cs3/pdt/internal/actions/OpenConsoleAction.java
@@ -1,12 +1,12 @@
 package org.cs3.pdt.internal.actions;
 
+import org.cs3.pdt.PDTPlugin;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 
 public class OpenConsoleAction implements IWorkbenchWindowActionDelegate {
 
@@ -22,25 +22,16 @@ public class OpenConsoleAction implements IWorkbenchWindowActionDelegate {
 
 	@Override
 	public void run(IAction action) {
-		final IWorkbenchWindow activeWorkbenchWindow = PlatformUI
-				.getWorkbench().getActiveWorkbenchWindow();
-		if (activeWorkbenchWindow == null) {
-			return;
-		}
-
-		final IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
+		final IWorkbenchPage activePage = PDTPlugin.getActivePage();
 		if (activePage == null) {
 			return;
 		}
-
 		try {
 			activePage.showView("org.cs3.pdt.console.internal.views.PrologConsoleView");
 		} catch (PartInitException e) {
 //			UIUtils.logAndDisplayError(PDTPlugin.getDefault().getErrorMessageProvider(), UIUtils.getDisplay().getActiveShell(), 
 //					PDTCore.ERR_UNKNOWN, PDTCore.CX_UNKNOWN, e);
-
 		}
-
 	}
 
 	@Override
