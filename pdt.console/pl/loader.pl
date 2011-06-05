f741c2b3f355a911e6952a7729da7b6ab96b6549
Guenter Kniesel <gk@cs.uni-bonn.de>
2011-06-05 18:32:06 +0200

    Moved logtalk adapter for pdt to an own folder and fixed loading issue. Now one can use F9 / consult for logtalk files without having to cd to their directory manually.

---
+++ pdt.console/pl/loader.pl
@@ -0,0 +1,18 @@
+
+
+:- use_module(pdt_runtime_builder_analyzer('metafile_referencer.pl')).
+:- use_module(pdt_runtime_builder_analyzer(pdt_xref_experimental)).
+:- use_module(pdt_runtime_builder_analyzer(properties)).
+
+% Search, outline, etc. for SWI-Prolog:
+:- use_module(org_cs3_lp_utils(utils4modules)).
+:- use_module(lib_pdt_console_pl(pdtplugin)).
+	 
+% Search, outline, etc. for Logtalk:
+:- consult(lib_pdt_console_pl('lgt/loader.pl')).
+
+:- use_module(lib_pdt_console_pl(pdt_console_server)).
+
+% After loading this file call:                 
+% ?- pdt_current_console_server(Port).
+% (e.g. in "PrologConsoleView.java")
