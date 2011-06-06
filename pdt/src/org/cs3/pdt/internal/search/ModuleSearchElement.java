2a109e234256d306c27f67ee0205d14607dfa830
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-06-06 17:38:45 +0200

    files don't appear as an element in the search results but as an addition to the module

--- pdt/src/org/cs3/pdt/internal/search/ModuleSearchElement.java
+++ pdt/src/org/cs3/pdt/internal/search/ModuleSearchElement.java
@@ -1,47 +1,56 @@
 package org.cs3.pdt.internal.search;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.cs3.pdt.internal.views.lightweightOutline.PDTTreeElement;
 import org.eclipse.core.resources.IFile;
 
 public class ModuleSearchElement implements PDTTreeElement {
 	private String name;
+	private IFile file;
 	private List<PrologMatch> element = new ArrayList<PrologMatch>();
-	Set<FileTreeElement> files = new HashSet<FileTreeElement>();
+//	Set<FileTreeElement> files = new HashSet<FileTreeElement>();
+	List<PredicateElement> predicates = new ArrayList<PredicateElement>();
 	
 	public ModuleSearchElement(String name) {
 		this.name = name;
 	}
 	
 	public String getLabel() {
-		return name;
+		StringBuffer label = new StringBuffer(name);
+		label.append(" (");
+		label.append(file.getFullPath().toString());
+//		label.append(file.getLocation());
+		label.append(")");
+		return label.toString();
 	}
 	
 	public void addElement(PrologMatch elem) {
 		element.add(elem);
-		IFile file = ((PredicateElement)elem.getElement()).getFile();
-		boolean found = false; 
 		
-		for (FileTreeElement fileElement : files) {
-			if (fileElement.getFile().equals(file)) {
-				found = true;
-				fileElement.addChild((PredicateElement)elem.getElement());
-			}
-		}
-		if (!found) {
-			FileTreeElement fileElement = new FileTreeElement(file);
-			files.add(fileElement);
-			fileElement.addChild((PredicateElement)elem.getElement());
-		}
+		PredicateElement predicate = (PredicateElement)elem.getElement();
+		predicates.add(predicate);
+		
+		file = predicate.getFile();
+
+//		for (FileTreeElement fileElement : files) {
+//			if (fileElement.getFile().equals(file)) {
+//				found = true;
+//				fileElement.addChild((PredicateElement)elem.getElement());
+//			}
+//		}
+//		if (!found) {
+//			FileTreeElement fileElement = new FileTreeElement(file);
+//			files.add(fileElement);
+//			fileElement.addChild((PredicateElement)elem.getElement());
+//		}
 	}
 
 	@Override
 	public Object[] getChildren() {
-		FileTreeElement[] unsortedFiles = files.toArray(new FileTreeElement[files.size()]);
+		PredicateElement[] unsortedFiles = predicates.toArray(new PredicateElement[predicates.size()]);
+//		FileTreeElement[] unsortedFiles = files.toArray(new FileTreeElement[files.size()]);
 		return unsortedFiles;
 		
 //		return PDTCoreUtils.sortFileSet(unsortedFiles);
