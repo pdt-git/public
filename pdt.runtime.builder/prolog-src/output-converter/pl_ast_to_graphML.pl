20fe297c64c5a893885ba4c6a1884e21c6154d32
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-07-04 13:17:43 +0200

    pl_ast_to_graphML changed for changed arity of some T-facts

--- pdt.runtime.builder/prolog-src/output-converter/pl_ast_to_graphML.pl
+++ pdt.runtime.builder/prolog-src/output-converter/pl_ast_to_graphML.pl
@@ -77,6 +77,12 @@ write_graphML_keys(OutStream):-
   	nl(OutStream),
   	write(OutStream, '</key>'),
     nl(OutStream),
+    write(OutStream, '<key id="isDynamic" for="node" attr.name="isDeclaredMetaPredicate" attr.type="boolean">'),
+    nl(OutStream),
+    write(OutStream, '    <default>false</default>'),
+  	nl(OutStream),
+  	write(OutStream, '</key>'),
+    nl(OutStream),   
     write(OutStream, '<key id="frequency" for="edge" attr.name="frequency" attr.type="int">'),
     nl(OutStream),
     write(OutStream, '    <default>1</default>'),
@@ -135,18 +141,22 @@ write_predicate(Stream,Id,Functor,Arity,Module):-
 	write_data(Stream,'functor',Functor),
 	write_data(Stream,'arity',Arity),	
 	write_data(Stream,'moduleOfPredicate',Module),	
-	(	dynamicT(_,Functor,Arity,Module,_)
+	(	dynamicT(Id,_)
 	->	write_data(Stream,'isDynamic','true')
 	;	true
 	),
-	(	transparentT(_,Functor,Arity,Module,_)
+	(	transparentT(Id,_)
 	->	write_data(Stream,'isTransparent','true')
 	;	true
 	),	
-	(	multifileT(_,Functor,Arity,Module,_)
+	(	multifileT(Id,_)
 	->	write_data(Stream,'isMultifile','true')
 	;	true
 	),		
+	(	meta_predT(Id,_)
+	->	write_data(Stream,'isDeclaredMetaPredicate','true')
+	;	true
+	),	
 /*	start_graph_element(Stream),
 	write_clauses(Stream,FileName),
 	close_graph_element(Stream),
@@ -263,8 +273,8 @@ write_data(Stream,Key,Value):-
 	
 
 
-pl_test:-
-    pl_test(['Z:/pdt.git/pdt.runtime.ui/library'],'Z:/WorkspaceTeaching3/test6.graphml').           
+pl_test:-	
+    pl_test(['Z:/Git-Data/pdt.git/pdt.runtime.builder/prolog-src'],'Z:/Workspaces/WorkspaceFresh/test6.graphml').           
  
 pl_test(Project,Output):-
 	plparser_quick:generate_facts(Project),
