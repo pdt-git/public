f73285bb08ecdfd4b2195af9c6870928ff6432bb
Eva Stöwe <Katzazi@gmx.de>
2011-07-04 12:59:39 +0200

    added metapred_finder to loader and some functionality to run the code on all loaded files.

--- pdt.runtime.builder/prolog-src/load_builder.pl
+++ pdt.runtime.builder/prolog-src/load_builder.pl
@@ -1,1 +1,22 @@
 :- use_module('prolog_file_reader').
+:- use_module('analyzer/metapred_finder').
+
+
+
+pl_test:-
+    find_all_loaded_files(Project),
+    plparser_quick:generate_facts(Project).       
+    
+find_all_loaded_files(Project):-
+    findall(
+    	File,
+    	source_file(File),
+    	Project
+    ).	
+ 
+pl_test(Project):-
+	plparser_quick:generate_facts(Project).
+	
+	
+pl_test_fix:-	
+    pl_test(['C:/Data/Git-Data/pdt.git/pdt.runtime.builder/prolog-src']). 
