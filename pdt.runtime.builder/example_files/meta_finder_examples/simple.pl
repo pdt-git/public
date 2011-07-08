1cf5d8a4d3c0e0c296318161e2ec7e2ee5f8f823
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-07-08 17:41:16 +0200

    added some more examples for meta-predicates - and explanations to them

--- pdt.runtime.builder/example_files/meta_finder_examples/simple.pl
+++ pdt.runtime.builder/example_files/meta_finder_examples/simple.pl
@@ -1,45 +1,62 @@
 :- module(simple, []).
 
-a(A):-
+a(A):-			%direct meta-argument
     call(A).
     
     
-b(B):-
+b(B):-			%direct unification
     A=B,
     call(A).
     
-c(C):-
+c(C):-			%some other stuff happens
     a,
     b,
     call(C).
   
-d(D):-
+d(D):-			%some other stuff happens with or
     a;
     call(D).
     
-e(E):-
+e(E):-			%meta call in meta argument
     call(call(E)).
     
-f(F):-
+f(F):-			%again other stuff happens but with non-atoms
     blub(A),
     bla(A),
     call(F).
     
-g(G):-
+g(G):-			%stuff happens around (not only before the meta-predicate-call
     a,
     call(G),
     b.
  
-h(H):-
+h(H):-			%indirect unification
     h(H, b) = h(B, _X),
     call(B).
     
-i(H,X):-
+h2(H,X):-		%X is also a meta-argument -> h2(0,0)
+    bla(H, H) = bla(X, B),
+    call(B).    
+
+i(H,X):-		%another arguement X which is not a meta-argument -> i(0,?)
     bla(H, b) = bla(B, X),
     call(B).
     
-%j(J):-
-%    d(J).
-
-z(Z):-	%non_meta
+j(J):-			%call to user-defined meta-predicate
+    d(J).
+    
+k(K):-			%meta-predicate-call in true-branch of a decision
+    (	bla(K,a) = bla(D,a)
+    ->	call(D)
+    ;	fail
+    ).
+    
+l(L,M,C):-	%(only L not M is meta-argument, even if they would be aliased in the condition)
+    (	bla(M,C) = bla(L,a) 	%meta-predicate-call in else-branch of decision
+	->	a
+	;	call(L)
+	).
+	
+	
+z(Z):-			%non_meta
 	Z = 4.
