f590c4a29bf3612f0992d9fb5591abaaa3f7d8ed
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-07-13 17:12:08 +0200

    minor change for debugging: added a varialbe to be able to test the queries to prolog

--- pdt/src/org/cs3/pdt/internal/editors/PLMarkerUtils.java
+++ pdt/src/org/cs3/pdt/internal/editors/PLMarkerUtils.java
@@ -33,103 +33,104 @@ public class PLMarkerUtils {
 		if ("info".equals(severity)) {
 			return IMarker.SEVERITY_INFO;
 		}
-	
+
 		throw new IllegalArgumentException("cannot map severity constant: "
 				+ severity);
 	}
 
 	private static void addMarkers(final IFile file) throws PrologInterfaceException {
-			Job j = new Job("update markers") {
-				@Override
-				protected IStatus run(IProgressMonitor monitor) {
-					PrologSession session =null;
-					try {
-					
-						final IDocument doc = PDTCoreUtils.getDocument(file);
-						session = PrologConsolePlugin.getDefault().getPrologConsoleService().getActivePrologConsole().getPrologInterface().getSession();
-						Thread.sleep(500); // wait for the prolog messages to complete (TODO: wait until parsing is finished)
-						List<Map<String, Object>> msgs = session.queryAll("pdtplugin:errors_and_warnings(Kind,Line,Length,Message)");
-						for (Map<String, Object> msg : msgs) {
-							int severity=0;
-							try {
-								severity = mapSeverity(((String)msg.get("Kind")));
-							} catch(IllegalArgumentException e){
-								continue;
-							}
-							IMarker marker = file.createMarker(IMarker.PROBLEM);
-	
-							marker.setAttribute(IMarker.SEVERITY, severity);
-	
-							String msgText = (String)msg.get("Message");
-							int start = doc.getLineOffset(Integer.parseInt((String)msg.get("Line"))-1);
-							int end = start +Integer.parseInt((String)msg.get("Length"));
-							if(severity==IMarker.SEVERITY_ERROR && msgText.startsWith("Exported procedure ")&& msgText.endsWith(" is not defined\n")){
-								start = end= 0;
-							}
-	//						marker.setAttribute(IMarker.CHAR_START, start);
-	//						marker.setAttribute(IMarker.CHAR_END, end);
-							MarkerUtilities.setCharStart(marker, start);
-							MarkerUtilities.setCharEnd(marker, end);
-	
-							marker.setAttribute(IMarker.MESSAGE, msgText);
-						}
-						
-						List<Map<String, Object>> msgsSmells = session.queryAll("smell_marker_pdt(Name, Description, QuickfixDescription, QuickfixAction, '" + file.getRawLocation().toPortableString().toLowerCase() + "', Start, Length)");
-						for (Map<String, Object> msg : msgsSmells) {
-							IMarker marker = file.createMarker(IMarker.PROBLEM);
-							marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
-							marker.setAttribute(PDTMarker.SMELL_NAME, msg.get("Name").toString());
-							marker.setAttribute(PDTMarker.QUICKFIX_DESCRIPTION, msg.get("QuickfixDescription").toString());
-	
-							String msgText = (String)msg.get("Description");
-							int startPl = Integer.parseInt(msg.get("Start").toString());
-							int start =PDTCoreUtils.convertLogicalToPhysicalOffset(doc,startPl);
-							int length = Integer.parseInt(msg.get("Length").toString());
-									
-	//						marker.setAttribute(IMarker.CHAR_START, start);
-	//						marker.setAttribute(IMarker.CHAR_END, (start+length));
-							MarkerUtilities.setCharStart(marker, start);
-							MarkerUtilities.setCharEnd(marker, start+length);
-	
-							marker.setAttribute(PDTMarker.QUICKFIX_ACTION, msg.get("QuickfixAction".toString()));
-							marker.setAttribute(IMarker.MESSAGE, msgText);
-						}
-						
-						
-						session.queryOnce("deactivate_warning_and_error_tracing");
-					} catch (Exception e) {
-						Debug.report(e);
-						return Status.CANCEL_STATUS;
-					} finally {
-						if(session!=null)session.dispose();
-					}
-					return Status.OK_STATUS;
-				}
-			};
-			j.setRule(file);
-			j.schedule();
-				
-			 
-		}
-
-	public static void updateFileMarkers(IFile file ) throws CoreException {
-		if( PDTCoreUtils.getPrologProject(file)==null &&
-			PrologConsolePlugin.getDefault().getPrologConsoleService().getActivePrologConsole()!= null){ 
+		Job j = new Job("update markers") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
 				PrologSession session =null;
 				try {
+
+					final IDocument doc = PDTCoreUtils.getDocument(file);
 					session = PrologConsolePlugin.getDefault().getPrologConsoleService().getActivePrologConsole().getPrologInterface().getSession();
-					session.queryOnce("activate_warning_and_error_tracing");
-	
-					file.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
-					executeConsult(file);
-					addMarkers(file);
-					
-				}catch(Exception e) {
+					Thread.sleep(500); // wait for the prolog messages to complete (TODO: wait until parsing is finished)
+					List<Map<String, Object>> msgs = session.queryAll("pdtplugin:errors_and_warnings(Kind,Line,Length,Message)");
+					for (Map<String, Object> msg : msgs) {
+						int severity=0;
+						try {
+							severity = mapSeverity(((String)msg.get("Kind")));
+						} catch(IllegalArgumentException e){
+							continue;
+						}
+						IMarker marker = file.createMarker(IMarker.PROBLEM);
+
+						marker.setAttribute(IMarker.SEVERITY, severity);
+
+						String msgText = (String)msg.get("Message");
+						int start = doc.getLineOffset(Integer.parseInt((String)msg.get("Line"))-1);
+						int end = start +Integer.parseInt((String)msg.get("Length"));
+						if(severity==IMarker.SEVERITY_ERROR && msgText.startsWith("Exported procedure ")&& msgText.endsWith(" is not defined\n")){
+							start = end= 0;
+						}
+						//						marker.setAttribute(IMarker.CHAR_START, start);
+						//						marker.setAttribute(IMarker.CHAR_END, end);
+						MarkerUtilities.setCharStart(marker, start);
+						MarkerUtilities.setCharEnd(marker, end);
+
+						marker.setAttribute(IMarker.MESSAGE, msgText);
+					}
+						
+					String query = "smell_marker_pdt(Name, Description, QuickfixDescription, QuickfixAction, '" + file.getRawLocation().toPortableString().toLowerCase() + "', Start, Length)";
+					List<Map<String, Object>> msgsSmells = session.queryAll(query);
+					for (Map<String, Object> msg : msgsSmells) {
+						IMarker marker = file.createMarker(IMarker.PROBLEM);
+						marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
+						marker.setAttribute(PDTMarker.SMELL_NAME, msg.get("Name").toString());
+						marker.setAttribute(PDTMarker.QUICKFIX_DESCRIPTION, msg.get("QuickfixDescription").toString());
+
+						String msgText = (String)msg.get("Description");
+						int startPl = Integer.parseInt(msg.get("Start").toString());
+						int start =PDTCoreUtils.convertLogicalToPhysicalOffset(doc,startPl);
+						int length = Integer.parseInt(msg.get("Length").toString());
+
+						//						marker.setAttribute(IMarker.CHAR_START, start);
+						//						marker.setAttribute(IMarker.CHAR_END, (start+length));
+						MarkerUtilities.setCharStart(marker, start);
+						MarkerUtilities.setCharEnd(marker, start+length);
+
+						marker.setAttribute(PDTMarker.QUICKFIX_ACTION, msg.get("QuickfixAction".toString()));
+						marker.setAttribute(IMarker.MESSAGE, msgText);
+					}
+
+
+					session.queryOnce("deactivate_warning_and_error_tracing");
+				} catch (Exception e) {
 					Debug.report(e);
+					return Status.CANCEL_STATUS;
 				} finally {
 					if(session!=null)session.dispose();
 				}
-	
+				return Status.OK_STATUS;
+			}
+		};
+		j.setRule(file);
+		j.schedule();
+
+
+	}
+
+	public static void updateFileMarkers(IFile file ) throws CoreException {
+		if( PDTCoreUtils.getPrologProject(file)==null &&
+				PrologConsolePlugin.getDefault().getPrologConsoleService().getActivePrologConsole()!= null){ 
+			PrologSession session =null;
+			try {
+				session = PrologConsolePlugin.getDefault().getPrologConsoleService().getActivePrologConsole().getPrologInterface().getSession();
+				session.queryOnce("activate_warning_and_error_tracing");
+
+				file.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+				executeConsult(file);
+				addMarkers(file);
+
+			}catch(Exception e) {
+				Debug.report(e);
+			} finally {
+				if(session!=null)session.dispose();
+			}
+
 		}
 	}
 
