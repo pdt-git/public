4ad3b93356d1748602c043d79ec8805d54c3f729
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-07-14 18:09:54 +0200

    some cleaning up

--- pdt.runtime.builder/prolog-src/prolog_file_reader.pl
+++ pdt.runtime.builder/prolog-src/prolog_file_reader.pl
@@ -65,34 +65,23 @@ update_facts(File, Project):-
         
 derive_edges:-
     forall( 
-    	(	literalT(LId,_,CId,Module,Functor,Arity)
-    	;	metaT(LId,_,CId,Module,Functor,Arity)
-    	),
+    	literalT(LId,_,_,Module,Functor,Arity),
     	( 	(	(	predicateT_ri(Functor,Arity,Module,Id)
     			->	true
-    			;	(	predicateT_ri(Functor,Arity,_DefModule,Id),
-    					visible_in_module(Id, Module)
-%    					(	visible_in_module(Id, Module)
-%    					->	true
-%    					;	(	clauseT(CId,ParentId,_Module,_Functor,_Arity),
-%    							fileT(ParentId, File, _),	
-%    							format('visibility of ~w:~w/~w for Module ~w not found; calling File: ~w~n', [_DefModule, Functor, Arity, Module, File])
-%    						)
-%    					)
+   				;	(	predicateT_ri(Functor ,Arity ,_DecModule, Id),
+   						visible_in_module(Id, Module)
    					)
+   					%get_predicate_referenced_as(Module, Functor, Arity, Id)
    				)
    			->	assert(call_edge(Id,LId))	
 			;	(	functor(Term, Functor, Arity),
-    		  		(	(	%format('Call to ~w:~w/~w~n', [Module, Functor, Arity]),
-    		  				declared_in_module(Module, Term, DefModule),
-    		  				%format('Defined in ~w~n', [DefModule]),
+    	  			(	(	declared_in_module(Module, Term, DefModule),
     		  				predicate_property(DefModule:Term, built_in)
-    		  			)
-    		  		->	assert(call_built_in(Functor, Arity, DefModule, LId))
-    		  		;	%format('Call to ~w ~w:~w/~w -> Not found.~n', [Module, DefModule, Functor, Arity])
-    		  			true			%TODO: here is a possible place to create a warning as soon as it's reduced to "real" problems... 
-    		  		)
-    		  	)
+    	  				)
+    	  			->	assert(call_built_in(Functor, Arity, DefModule, LId))
+    	  			;	true			%TODO: here is a possible place to create a warning as soon as it's reduced to "real" problems... 
+    	  			)
+   		  		)
 			)
     	)
     ).
