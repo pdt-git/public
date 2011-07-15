731ffc3208a69ea06c91c641d628bbe392e30195
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-07-15 17:02:52 +0200

    moved cross-referenceing predicate derive_edges to new file cross_reference_builder.pl

--- pdt.runtime.builder/prolog-src/prolog_file_reader.pl
+++ pdt.runtime.builder/prolog-src/prolog_file_reader.pl
@@ -7,7 +7,8 @@
 :- use_module(load_graph).
 :- use_module(modules_and_visibility).
 :- use_module(literal_parser).
-:- use_module(org_cs3_lp_utils(utils4modules)).
+:- use_module(cross_reference_builder).
+
 
 
 generate_facts(Project):-
@@ -30,6 +31,18 @@ generate_facts(Project):-
 	writeln('generate edges'),
 	time(derive_edges).    
 	
+update_facts(File, Project):-				
+	cleanup_nodes(File),
+	cleanup_computed_facts,
+   	walking_file_list(Project,parse,1),	
+	build_load_graph,
+    derive_all_predicates,
+	derive_onloads,
+	compute_all_predicate_properties,
+	compute_visibility_graph,
+	parse_bodies,
+	derive_edges.
+	
 %update_facts(File, Project):-				
 %	format('cleaning up facts for ~w~n',File),
 %	cleanup_nodes(File),
@@ -51,37 +64,3 @@ generate_facts(Project):-
 %	writeln('generate edges'),
 %	time(derive_edges).
 	
-update_facts(File, Project):-				
-	cleanup_nodes(File),
-	cleanup_computed_facts,
-   	walking_file_list(Project,parse,1),	
-	build_load_graph,
-    derive_all_predicates,
-	derive_onloads,
-	compute_all_predicate_properties,
-	compute_visibility_graph,
-	parse_bodies,
-	derive_edges.
-        
-derive_edges:-
-    forall( 
-    	literalT(LId,_,_,Module,Functor,Arity),
-    	( 	(	(	predicateT_ri(Functor,Arity,Module,Id)
-    			->	true
-   				;	(	predicateT_ri(Functor ,Arity ,_DecModule, Id),
-   						visible_in_module(Id, Module)
-   					)
-   					%get_predicate_referenced_as(Module, Functor, Arity, Id)
-   				)
-   			->	assert(call_edge(Id,LId))	
-			;	(	functor(Term, Functor, Arity),
-    	  			(	(	declared_in_module(Module, Term, DefModule),
-    		  				predicate_property(DefModule:Term, built_in)
-    	  				)
-    	  			->	assert(call_built_in(Functor, Arity, DefModule, LId))
-    	  			;	true			%TODO: here is a possible place to create a warning as soon as it's reduced to "real" problems... 
-    	  			)
-   		  		)
-			)
-    	)
-    ).
