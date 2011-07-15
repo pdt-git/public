8fdecdf9901354c249810795e135361c5d235c96
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-07-15 17:03:33 +0200

    minor clean-ups

--- pdt.runtime.builder/prolog-src/analyzer/edge_counter.pl
+++ pdt.runtime.builder/prolog-src/analyzer/edge_counter.pl
@@ -7,19 +7,11 @@
 count_call_edges_between_predicates:-
     retractall(call_edges_for_predicates(_,_,_)),
 	forall(	call_edge(TargetId, SourceLiteralId),
-			(	(	(	literalT(SourceLiteralId,_,SourceRule,_,_,_)
-    				;	metaT(SourceLiteralId,_,SourceRule,_,_,_)
-    				),
+			(	(	literalT(SourceLiteralId,_,SourceRule,_,_,_),
     				pred_edge(SourceRule,SourceId)
-    				%clauseT(SourceRule,_,SourceModule,SourceFunctor,SourceArity),
-    				%predicateT_ri(SourceFunctor,SourceArity,SourceModule,SourceId),
- 
-    				%clauseT(TargetRuleId,_,TargetModule,TargetFunctor,TargetArity),
-    				%predicateT_ri(TargetFunctor,TargetArity,TargetModule,TargetId)
-    				%predicateT(TargetPredId,_,_,_,_)
-				)
+   				)
 			->	inc_call_edges_for_predicates(SourceId,TargetId)
-			;	format('Problem with call-edge: ~w, ~w~n',[SourceLiteralId, TargetId])
+			;	format('Problem with call-edge: ~w -> ~w~n',[SourceLiteralId, TargetId])
 			)
 		).	
 
@@ -30,4 +22,3 @@ inc_call_edges_for_predicates(SourceID,TargetID):-
     assert(call_edges_for_predicates(SourceID,TargetID,New_Counter)).
 inc_call_edges_for_predicates(SourceID,TargetID):-  
     assert(call_edges_for_predicates(SourceID,TargetID,1)).
-    %format('call_edges_for_predicates(~w,~w,1)~n',[SourceID,TargetID]).
