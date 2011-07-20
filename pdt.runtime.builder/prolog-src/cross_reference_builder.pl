c323e0e5327b5d4034ec58b188e5545a90c298d6
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-07-20 17:35:19 +0200

    added some more ways to compute cross references (to be able to only look for those relevant to a changed or new predicate)

--- pdt.runtime.builder/prolog-src/cross_reference_builder.pl
+++ pdt.runtime.builder/prolog-src/cross_reference_builder.pl
@@ -1,4 +1,5 @@
-:- module(cross_reference_builder, [derive_edges/0]).
+:- module(cross_reference_builder, [derive_edges/0,
+									cross_references_for_predicate/1]).
 
 :- ensure_loaded(modules_and_visibility).
 :- ensure_loaded(parse_util).
@@ -7,22 +8,40 @@
 derive_edges:-
     forall( 
     	literalT(LId,_,_,Module,Functor,Arity),
-    	( 	(	(	predicateT_ri(Functor,Arity,Module,Id)
-    			->	true
-   				;	(	predicateT_ri(Functor ,Arity ,_DecModule, Id),
-   						visible_in_module(Id, Module)
-   					)
-   					%get_predicate_referenced_as(Module, Functor, Arity, Id)
-   				)
-   			->	assert(call_edge(Id,LId))	
-			;	(	functor(Term, Functor, Arity),
-    	  			(	(	declared_in_module(Module, Term, DefModule),
-    		  				predicate_property(DefModule:Term, built_in)
-    	  				)
-    	  			->	assert(call_built_in(Functor, Arity, DefModule, LId))
-    	  			;	true			%TODO: here is a possible place to create a warning as soon as it's reduced to "real" problems... 
-    	  			)
-   		  		)
-			)
-    	)
+		find_reference_for(LId,Module,Functor,Arity)
     ).
+
+
+cross_references_for_predicate(PredId):-
+    predicateT(PredId,_,Functor,Arity,_),
+    visible_in_module(PredId, Module),
+    forall(
+    	literalT_ri(Functor,Arity,Module,LitId),
+    	assert(call_edge(PredId,LitId))
+    	).
+cross_references_for_predicate(PredId):-
+    forall(
+    	(	pred_edge(ClauseId,PredId),
+    		literalT(LId,_,ClauseId,Module,Functor,Arity)
+    	),
+    	find_reference_for(LId,Module,Functor,Arity)
+    ).
+    
+find_reference_for(LId,Module,Functor,Arity):-    	
+    (	(	predicateT_ri(Functor,Arity,Module,Id)
+    	->	true
+   		;	(	predicateT_ri(Functor ,Arity ,_DecModule, Id),
+   				visible_in_module(Id, Module)
+   			)
+   					%get_predicate_referenced_as(Module, Functor, Arity, Id)
+   		)
+   	->	assert(call_edge(Id,LId))	
+	;	(	functor(Term, Functor, Arity),
+    		(	(	declared_in_module(Module, Term, DefModule),
+    			  	predicate_property(DefModule:Term, built_in)
+    		  	)
+    		->	assert(call_built_in(Functor, Arity, DefModule, LId))
+    		;	true			%TODO: here is a possible place to create a warning as soon as it's reduced to "real" problems...
+    		)
+   		)
+	).
