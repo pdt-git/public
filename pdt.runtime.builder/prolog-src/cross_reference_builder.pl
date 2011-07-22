aa5d1bf605d19269826a2ebe88a5e2ba45e882f3
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-07-22 20:04:08 +0200

    crossreferncer: added a catch for possible errors while consilting prolog about the goals of the literals (you never know what people put there)

--- pdt.runtime.builder/prolog-src/cross_reference_builder.pl
+++ pdt.runtime.builder/prolog-src/cross_reference_builder.pl
@@ -36,12 +36,19 @@ find_reference_for(LId,Module,Functor,Arity):-
    					%get_predicate_referenced_as(Module, Functor, Arity, Id)
    		)
    	->	assert(call_edge(Id,LId))	
-	;	(	functor(Term, Functor, Arity),
-    		(	(	declared_in_module(Module, Term, DefModule),
-    			  	predicate_property(DefModule:Term, built_in)
-    		  	)
-    		->	assert(call_built_in(Functor, Arity, DefModule, LId))
-    		;	true			%TODO: here is a possible place to create a warning as soon as it's reduced to "real" problems...
-    		)
-   		)
+	;	catch(	(	(	functor(Term, Functor, Arity),
+    					declared_in_module(Module, Term, DefModule),
+    					/*(	var(Module)
+    					->	
+    					;	*/predicate_property(DefModule:Term, built_in)
+    			  		%)
+    		  		)
+    			->	assert(call_built_in(Functor, Arity, DefModule, LId))
+    			;	true			%TODO: here is a possible place to create a warning as soon as it's reduced to "real" problems...
+    			),
+    			Error,
+    			(	format('Problem with crossref -> Module: ~w, Functor: ~w, Arity: ~w, LId: ~w, Error: ~n',[Module,Functor,Arity,LId, Error]),
+    				true 
+    			)
+   			)
 	).
