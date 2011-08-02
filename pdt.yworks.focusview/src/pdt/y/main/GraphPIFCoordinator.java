a38fb3260e957b53f69ecffdacac89d5c63c9937
Eva Stöwe <stoewe@iai.uni-bonn.de>
2011-08-02 17:56:52 +0200

    refactoring so that it is possible to send queries to the current pif

--- pdt.yworks.focusview/src/pdt/y/main/GraphPIFCoordinator.java
+++ pdt.yworks.focusview/src/pdt/y/main/GraphPIFCoordinator.java
@@ -2,6 +2,7 @@ package pdt.y.main;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.Map;
 import java.util.Set;
 
 import org.cs3.pdt.PDTPlugin;
@@ -27,9 +28,10 @@ public class GraphPIFCoordinator implements ISelectionChangedListener{
 	private static final String FILE_TO_CONSULT = "pl_ast_to_graphML";
 	private static final String PATH_ALIAS = "pdt_runtime_builder_graphml_creator";
 	private static final String NAME_OF_HELPING_FILE = "pdt-focus-help.graphml";
-	  
+
 	private File helpFile;
 	private PDTGraphSwingStandalone view;
+	private PrologInterface pif;
 
 
 	public GraphPIFCoordinator(PDTGraphSwingStandalone view) {
@@ -37,17 +39,17 @@ public class GraphPIFCoordinator implements ISelectionChangedListener{
 		PrologRuntimeUIPlugin plugin=PrologRuntimeUIPlugin.getDefault();
 		ResourceFileLocator locator = plugin.getResourceLocator();
 		helpFile = locator.resolve(NAME_OF_HELPING_FILE);
-		//System.out.println("Location of file .graphml file: "+helpFile.toString());		
+		//System.out.println("Location of file .graphml file: "+helpFile.toString());
 		PDTPlugin.getDefault().addSelectionChangedListener(this);
 	}
 
-//	private void getWorkspaceLocation() {
-//		IWorkspaceRoot wsRoot = ResourcesPlugin.getWorkspace().getRoot();
-//		workspaceLocation = wsRoot.getLocation().toString();
-//		System.out.println(workspaceLocation);
-//	}
-//	
-	
+	//	private void getWorkspaceLocation() {
+	//		IWorkspaceRoot wsRoot = ResourcesPlugin.getWorkspace().getRoot();
+	//		workspaceLocation = wsRoot.getLocation().toString();
+	//		System.out.println(workspaceLocation);
+	//	}
+	//
+
 	@Override
 	public void selectionChanged(SelectionChangedEvent event) {
 		ISelection selection = event.getSelection();
@@ -57,7 +59,7 @@ public class GraphPIFCoordinator implements ISelectionChangedListener{
 		}
 
 	}
-	
+
 	public void queryPrologForGraphFacts(String focusFileForParsing) {
 		//PrologInterface pif=PluginActivator.getDefault().getPrologInterface();
 		String prologNameOfFileToConsult = PATH_ALIAS+"("+FILE_TO_CONSULT+")";
@@ -65,17 +67,17 @@ public class GraphPIFCoordinator implements ISelectionChangedListener{
 		try {
 			PrologConsole activeConsole = PrologConsolePlugin.getDefault().getPrologConsoleService().getActivePrologConsole();
 			if(activeConsole!= null){
-				PrologInterface pif = getPifForActiveConsole(activeConsole);
-				
-				PrologSession session = pif.getSession(PrologInterface.LEGACY);
-				session.queryOnce("consult("+prologNameOfFileToConsult+").");
-			
-			//List<String> folderList/* = collectPrologFilesInWorkspace()*/;
-			//folderToParse = convertJavaListToPrologListString(folderList);
-			
-				String query = "write_focus_to_graphML('"+focusFileForParsing+"','"+Util.prologFileName(helpFile)+"').";
-				session.queryOnce(query);
-			
+				pif = getPifForActiveConsole(activeConsole);
+
+				String query = "consult("+prologNameOfFileToConsult+").";
+				sendQueryToCurrentPiF(query);
+
+				//List<String> folderList/* = collectPrologFilesInWorkspace()*/;
+				//folderToParse = convertJavaListToPrologListString(folderList);
+
+				query = "write_focus_to_graphML('"+focusFileForParsing+"','"+Util.prologFileName(helpFile)+"').";
+				sendQueryToCurrentPiF(query);
+
 				view.loadGraph(helpFile.toURI().toURL());
 			}
 		} catch (PrologException e1) {
@@ -87,19 +89,26 @@ public class GraphPIFCoordinator implements ISelectionChangedListener{
 		}
 	}
 
+	public Map<String, Object> sendQueryToCurrentPiF(String query)
+			throws PrologInterfaceException {
+		PrologSession session = pif.getSession(PrologInterface.LEGACY);
+		Map<String, Object> result = session.queryOnce(query);
+		return result;
+	}
+
 	public PrologInterface getPifForActiveConsole(PrologConsole activeConsole) {
 		PrologInterface pif = activeConsole.getPrologInterface();
 		PrologInterfaceRegistry pifRegistry = PrologRuntimePlugin.getDefault().getPrologInterfaceRegistry();
 		String pifKey = pifRegistry.getKey(pif);
 		Set<Subscription> subscriptions = pifRegistry.getSubscriptionsForPif(pifKey);
-		
+
 		if (ownSubscriptionMissing(subscriptions)) {
 			FocusViewSubscription mySubscription = FocusViewSubscription.newInstance(pifKey);
 			pif = PrologRuntimeUIPlugin.getDefault().getPrologInterface(mySubscription);
 		}
 		return pif;
 	}
-	
+
 	private boolean ownSubscriptionMissing(Set<Subscription> subscriptions) {
 		for (Subscription subscription : subscriptions) {
 			if (subscription instanceof FocusViewSubscription)
@@ -107,53 +116,53 @@ public class GraphPIFCoordinator implements ISelectionChangedListener{
 		}
 		return true;
 	}
-	
-//	private String selectFolderToParse() {		
-//		Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
-//		// File standard dialog
-//		DirectoryDialog folderDialog = new DirectoryDialog(shell);
-//		folderDialog.setText("Select Folder");
-//		// Open Dialog and save result of selection
-//		String selectedPath = folderDialog.open();
-//		File file = new File(selectedPath);
-//		String prologNameOfSelectedPath = Util.prologFileName(file);
-//		
-//		return prologNameOfSelectedPath;
-//	}
-//	
-//	private List<String> collectPrologFilesInWorkspace() {
-//		final List<String> fileList = new ArrayList<String>();
-//		try {
-//			ResourcesPlugin.getWorkspace().getRoot().accept(new IResourceVisitor(){
-//
-//				@Override 
-//				public boolean visit(IResource resource) throws CoreException {
-//					if (resource instanceof IProject && resource.isAccessible()) {
-//						IPath path = resource.getLocation();
-//						File file = path.toFile();
-//						
-//						fileList.add(Util.prologFileName(file));
-//						System.out.println("Folder: "+file.toString());
-//						return false;
-//					}
-//					return true;
-//				}
-//			});
-//		} catch (CoreException e) {
-//		};
-//		System.out.println("fertig mit sammeln");
-//		return fileList;
-//	}
-	
-//	private String convertJavaListToPrologListString(List<String> stringList) {
-//		StringBuffer buffer = new StringBuffer();
-//		for (String elem : stringList) {
-//			buffer.append(elem);
-//			buffer.append("','");
-//		}
-//		buffer.delete(buffer.length()-3, buffer.length());
-//		System.out.println("String to write:"+buffer.toString());
-//		return buffer.toString();
-//	}
+
+	//	private String selectFolderToParse() {
+	//		Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
+	//		// File standard dialog
+	//		DirectoryDialog folderDialog = new DirectoryDialog(shell);
+	//		folderDialog.setText("Select Folder");
+	//		// Open Dialog and save result of selection
+	//		String selectedPath = folderDialog.open();
+	//		File file = new File(selectedPath);
+	//		String prologNameOfSelectedPath = Util.prologFileName(file);
+	//
+	//		return prologNameOfSelectedPath;
+	//	}
+	//
+	//	private List<String> collectPrologFilesInWorkspace() {
+	//		final List<String> fileList = new ArrayList<String>();
+	//		try {
+	//			ResourcesPlugin.getWorkspace().getRoot().accept(new IResourceVisitor(){
+	//
+	//				@Override
+	//				public boolean visit(IResource resource) throws CoreException {
+	//					if (resource instanceof IProject && resource.isAccessible()) {
+	//						IPath path = resource.getLocation();
+	//						File file = path.toFile();
+	//
+	//						fileList.add(Util.prologFileName(file));
+	//						System.out.println("Folder: "+file.toString());
+	//						return false;
+	//					}
+	//					return true;
+	//				}
+	//			});
+	//		} catch (CoreException e) {
+	//		};
+	//		System.out.println("fertig mit sammeln");
+	//		return fileList;
+	//	}
+
+	//	private String convertJavaListToPrologListString(List<String> stringList) {
+	//		StringBuffer buffer = new StringBuffer();
+	//		for (String elem : stringList) {
+	//			buffer.append(elem);
+	//			buffer.append("','");
+	//		}
+	//		buffer.delete(buffer.length()-3, buffer.length());
+	//		System.out.println("String to write:"+buffer.toString());
+	//		return buffer.toString();
+	//	}
 
 }
