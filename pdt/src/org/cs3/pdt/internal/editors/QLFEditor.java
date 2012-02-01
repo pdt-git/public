aa8a8591b11c64b11f2bcd8b03282c85aa502afd
Fabian Noth <noth@iai.uni-bonn.de>
2012-02-01 12:54:08 +0100

    updated qlf editor

--- pdt/src/org/cs3/pdt/internal/editors/QLFEditor.java
+++ pdt/src/org/cs3/pdt/internal/editors/QLFEditor.java
@@ -1,15 +1,24 @@
 
 package org.cs3.pdt.internal.editors;
 
+import java.io.File;
+
+import org.cs3.pdt.PDTUtils;
+import org.cs3.pdt.internal.ImageRepository;
 import org.cs3.pdt.internal.actions.ConsultAction;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.part.MultiPageEditorPart;
 
 public class QLFEditor extends MultiPageEditorPart {
@@ -17,28 +26,96 @@ public class QLFEditor extends MultiPageEditorPart {
 	@Override
 	protected void createPages() {
 		Composite composite = new Composite(getContainer(), SWT.NONE);
+
+		IPath newPath = getCurrentIFile().getLocation().removeFileExtension().addFileExtension("pl");
+		final File plFile = newPath.toFile();
+		boolean plFileExists = plFile.exists();
 		
-		GridLayout layout = new GridLayout();
-		composite.setLayout(layout);
-		layout.numColumns = 2;
-
-		Button consultButton = new Button(composite, SWT.NONE);
-		GridData gd = new GridData(GridData.BEGINNING);
-		gd.horizontalSpan = 2;
-		consultButton.setLayoutData(gd);
-		consultButton.setText("Consult QLF file");
-		consultButton.addListener(SWT.Selection, new Listener() {
-
-			@Override
-			public void handleEvent(Event event) {
-
-				new ConsultAction().consultFromActiveEditor();
+		try {
+			FormLayout thisLayout = new FormLayout();
+			composite.setLayout(thisLayout);
+			composite.setSize(233, 272);
+			{
+				Label iconLabel = new Label(composite, SWT.NONE);
+				FormData label1LData = new FormData();
+				label1LData.left = new FormAttachment(0, 1000, 16);
+				label1LData.top = new FormAttachment(0, 1000, 12);
+				label1LData.width = 21;
+				label1LData.height = 18;
+				iconLabel.setLayoutData(label1LData);
+				iconLabel.setImage(ImageRepository.getImage(ImageRepository.MESSAGE_INFO));
 			}
-			
-		});
-		setPartName(getEditorInput().getName());
+			{
+				Label label2 = new Label(composite, SWT.NONE);
+				FormData label2LData = new FormData();
+				label2LData.left =  new FormAttachment(0, 1000, 38);
+				label2LData.top =  new FormAttachment(0, 1000, 15);
+				label2LData.width = 300;
+				label2LData.height = 15;
+				label2.setLayoutData(label2LData);
+				String labelText = "Can't edit binary file. ";
+				if (plFileExists) {
+					labelText += "Source file available.";
+				} else {
+					labelText += "No source file available.";
+				}
+				label2.setText(labelText);
+			}
+			{
+				Button buttonEdit = new Button(composite, SWT.PUSH | SWT.CENTER);
+				FormData buttonEditData = new FormData();
+				buttonEditData.left =  new FormAttachment(0, 1000, 101);
+				buttonEditData.top =  new FormAttachment(0, 1000, 42);
+				buttonEditData.width = 92;
+				buttonEditData.height = 25;
+				buttonEdit.setLayoutData(buttonEditData);
+				buttonEdit.setText("Edit Source File");
+				buttonEdit.setEnabled(plFileExists);
+				
+				buttonEdit.addListener(SWT.Selection, new Listener() {
+					@Override
+					public void handleEvent(Event event) {
+						
+						PDTUtils.openInEditor(plFile.toString());
+						
+					}
+				});
+			}
+			{
+				Button buttonConsult = new Button(composite, SWT.PUSH | SWT.CENTER);
+				FormData buttonConsultData = new FormData();
+				buttonConsultData.left =  new FormAttachment(0, 1000, 12);
+				buttonConsultData.top =  new FormAttachment(0, 1000, 42);
+				buttonConsultData.width = 77;
+				buttonConsultData.height = 25;
+				buttonConsult.setLayoutData(buttonConsultData);
+				buttonConsult.setText("Consult QLF");
 
+				buttonConsult.addListener(SWT.Selection, new Listener() {
+					@Override
+					public void handleEvent(Event event) {
+
+						new ConsultAction().consultFromActiveEditor();
+					}
+					
+				});
+			}
+			composite.layout();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		setPartName(getEditorInput().getName());
 		addPage(composite);
+	}
+	
+
+	private IFile getCurrentIFile() {
+		if (getEditorInput() instanceof IFileEditorInput) {
+			IFileEditorInput input = (IFileEditorInput) getEditorInput();
+			return input.getFile();
+		}
+		return null;
 	}
 	
 	@Override
