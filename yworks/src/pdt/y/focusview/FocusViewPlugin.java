356e59f6a3ff74c4144f076d0883b8129b97767f
Andreas Becker <beckera@cs.uni-bonn.de>
2012-02-21 22:19:01 +0100

    moved FocusView and FocusViewCoordinator into FocusViewPlugin (the Focus View class!). this solves the size problem with the focus view when displaying the graph for a file for the first time.

--- yworks/src/pdt/y/focusview/FocusViewPlugin.java
+++ yworks/src/pdt/y/focusview/FocusViewPlugin.java
@@ -1,5 +1,20 @@
 package pdt.y.focusview;
 
+import static org.eclipse.ui.IWorkbenchCommandConstants.FILE_SAVE;
+import static org.eclipse.ui.IWorkbenchCommandConstants.FILE_SAVE_ALL;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.JComponent;
+
+import org.cs3.pdt.PDTPlugin;
+import org.cs3.pdt.internal.editors.PDTChangedFileInformation;
+import org.eclipse.albireo.core.SwingControl;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IExecutionListener;
+import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -9,25 +24,44 @@ import org.eclipse.jface.preference.IPreferencePage;
 import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.jface.preference.PreferenceManager;
 import org.eclipse.jface.preference.PreferenceNode;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.ToolTip;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.ui.progress.UIJob;
 
 import pdt.y.internal.ui.ToolBarAction;
+import pdt.y.main.PDTGraphView;
+import pdt.y.model.GraphDataHolder;
 import pdt.y.preferences.EdgeAppearancePreferences;
 import pdt.y.preferences.FileAppearancePreferences;
 import pdt.y.preferences.PredicateAppearancePreferences;
 import pdt.y.preferences.PredicateLayoutPreferences;
 import pdt.y.preferences.PreferenceConstants;
 import pdt.y.preferences.PreferencePage;
+import y.base.Node;
+import y.view.HitInfo;
+import y.view.NodeLabel;
+import y.view.ViewMode;
 
 
 
@@ -37,6 +71,7 @@ public class FocusViewPlugin extends ViewPart {
 	private Composite viewContainer;
 	private Label info;
 	private String infoText = "", statusText = "";
+	private FocusViewCoordinator focusViewCoordinator;
 	
 	public FocusViewPlugin() {
 	}
@@ -64,7 +99,7 @@ public class FocusViewPlugin extends ViewPart {
 		
 		initButtons(parent);
 		
-		new FocusViewCoordinator(this, viewContainer);
+		focusViewCoordinator = new FocusViewCoordinator();
 	}
 
 	protected void initGraphNotLoadedLabel() {
@@ -226,5 +261,274 @@ public class FocusViewPlugin extends ViewPart {
 		    }
 		}.schedule();
 	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		PDTPlugin.getDefault().removeSelectionChangedListener(focusViewCoordinator);
+	}
+	
+	public class FocusView extends SwingControl {
 
+		private final String FOCUS_VIEW_IS_OUTDATED = "[FocusView is outdated]";
+		
+		private final PDTGraphView pdtGraphView;
+		private final GraphPIFLoader pifLoader;
+		private final String filePath;
+		
+		private boolean isDirty = false;
+		
+		public FocusView(final String filePath) {
+			super(viewContainer, SWT.NONE); 
+			
+			this.filePath = filePath;
+			
+			this.pdtGraphView = new PDTGraphView();
+			
+			this.pifLoader = new GraphPIFLoader(pdtGraphView);
+			
+			pdtGraphView.addViewMode(new OpenInEditorViewMode(pdtGraphView, pifLoader));
+			pdtGraphView.addViewMode(new HoverTrigger(getShell()));
+		}	
+		
+		public String getFilePath() {
+			return filePath;
+		}
+		
+		public boolean isEmpty() {
+			return pdtGraphView.isEmpty();
+		}
+		
+		public void setDirty() {
+			FocusViewPlugin.this.setStatusText(FOCUS_VIEW_IS_OUTDATED);
+			isDirty = true;
+		}
+		
+		public boolean isDirty() {
+			return isDirty;
+		}
+		
+		public List<String> getDependencies() {
+			return pifLoader.getDependencies();
+		}
+
+		public void reload() {
+			pifLoader.queryPrologForGraphFacts(filePath);
+			FocusViewPlugin.this.setStatusText("");
+			
+			isDirty = false;
+		}
+		
+		public void updateLayout() {
+			pdtGraphView.updateLayout();
+		}
+
+		@Override
+		protected JComponent createSwingComponent() {
+			return pdtGraphView;
+		}
+
+		@Override
+		public Composite getLayoutAncestor() {
+			return viewContainer;
+		}
+
+		private final class HoverTrigger extends ViewMode {
+
+			private final ToolTip t;
+			
+			public HoverTrigger(Shell parent) {
+				t = new ToolTip(parent, SWT.NONE);
+				t.setVisible(false);
+			}
+			
+			public void mouseMoved(final double x, final double y) {
+				super.mouseMoved(x, y);
+				
+				new UIJob("Updating status") {
+					
+					@Override
+					public IStatus runInUIThread(IProgressMonitor monitor) {
+						updateStatus(x, y);
+						return Status.OK_STATUS;
+					}
+				
+				}.schedule();
+			}
+
+			protected void updateStatus(double x, double y) {
+				HitInfo hitInfo = getHitInfo(x, y);
+				
+				if (hitInfo.hasHitNodes()) {
+					Node node = hitInfo.getHitNode();
+					NodeLabel label = pdtGraphView.getGraph2D().getRealizer(node).getLabel();
+					
+					StringBuilder sb = new StringBuilder(); 
+
+					GraphDataHolder data = pdtGraphView.getDataHolder();
+
+					if (data.isModule(node)) {
+						sb.append("Module: ");
+					} else if (data.isFile(node)) {
+						sb.append("File: ");
+					} else if (data.isPredicate(node)) {
+						sb.append("Predicate: ");
+					}
+
+					sb.append(label.getText());
+					
+					if (data.isExported(node)) {
+						sb.append(" [Exported]");
+					}
+
+					if (data.isDynamicNode(node)) {
+						sb.append(" [Dynamic]");
+					}
+
+					if (data.isUnusedLocal(node)) {
+						sb.append(" [Unused]");
+					}
+					
+					String text = sb.toString();
+
+					FocusViewPlugin.this.setInfoText(text);
+					
+					if (PreferencePage.isShowToolTip() && text.startsWith("Predicate")) {
+						Point location = Display.getCurrent().getCursorLocation();
+						location.x += 10;
+						location.y += 10;
+						t.setLocation(location);
+						t.setMessage(text.substring(11));
+						t.setVisible(true);
+					}
+					else {
+						t.setVisible(false);
+					}
+				} else {
+					FocusViewPlugin.this.setInfoText("");
+					t.setVisible(false);
+				}
+			}
+		}
+	}
+
+	public class FocusViewCoordinator implements ISelectionChangedListener, IExecutionListener {
+		
+		final HashMap<String, FocusView> views = new HashMap<String, FocusView>();
+		
+		FocusView currentFocusView;
+
+		public FocusViewCoordinator() {
+			PDTPlugin.getDefault().addSelectionChangedListener(this);
+			
+			ICommandService service = (ICommandService) PlatformUI
+				.getWorkbench().getService(ICommandService.class);
+		    service.addExecutionListener(this);
+		}
+		
+		@Override
+		public void selectionChanged(SelectionChangedEvent event) {
+			ISelection selection = event.getSelection();
+			
+			if (selection instanceof PDTChangedFileInformation) {
+			
+				final PDTChangedFileInformation fileInfo = (PDTChangedFileInformation)selection;
+				
+				if (currentFocusView == null 
+						|| !currentFocusView.getFilePath().equals(fileInfo.getPrologFileName())) {
+					
+					new UIJob("Update Focus View") {
+					    public IStatus runInUIThread(IProgressMonitor monitor) {
+					    	
+					    	FocusView f = swichFocusView(fileInfo.getPrologFileName());
+					    	
+					    	if (f.isEmpty()){
+					    		FocusViewPlugin.this.setStatusText("[Please activate prolog console, set focus on file and press F9 to load graph]");
+					    	}
+					        
+					    	return Status.OK_STATUS;
+					    }
+					}.schedule();
+				}
+				if (currentFocusView != null 
+						&& currentFocusView.isEmpty()) {
+					currentFocusView.reload();
+				}
+			}
+		}
+		
+		private FocusView swichFocusView(String path) {
+			currentFocusView = views.get(path);
+			if (currentFocusView == null) {
+				currentFocusView = new FocusView(path);
+
+				views.put(path, currentFocusView);
+			}
+			
+			FocusViewPlugin.this.setCurrentFocusView(currentFocusView);
+			refreshCurrentView();
+			
+			return currentFocusView;
+		}
+
+		private void setDirtyFocusView(String location) {
+			if (location == null)
+				return;
+			
+			String path = location.toLowerCase().replace('\\', '/');
+			if (!path.endsWith(".pl"))
+				return;
+			
+			for (FocusView f : views.values()) {
+				for (String d : f.getDependencies()) {
+					if (path.equals(d)) {
+						f.setDirty();
+						break;
+					}
+				}
+			}
+		}
+
+		protected void refreshCurrentView() {
+			if (PreferencePage.isAutomaticUpdate()
+					&& currentFocusView.isDirty()) {
+				currentFocusView.reload();
+			}
+		}
+		
+		@Override
+		public void notHandled(String commandId, NotHandledException exception) { }
+
+		@Override
+		public void postExecuteFailure(String commandId,
+				ExecutionException exception) { }
+
+		@Override
+		public void preExecute(String commandId, ExecutionEvent event) { }
+		
+		@Override
+		public void postExecuteSuccess(String commandId, Object returnValue) { 
+			IWorkbenchPage wb = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+			if (commandId.equals(FILE_SAVE)) {
+				setDirtyFocusView(getFileLocation(wb.getActiveEditor()));
+			}
+			else if (commandId.equals(FILE_SAVE_ALL)) {
+				IEditorPart[] editors = wb.getEditors();
+				for (IEditorPart e : editors) {
+					setDirtyFocusView(getFileLocation(e));
+				}
+			}
+			refreshCurrentView();
+		}
+
+		private String getFileLocation(IEditorPart editor) {
+	        if (editor != null) {
+	            IEditorInput input = editor.getEditorInput();
+	            if (input instanceof IFileEditorInput) {
+	                return ((IFileEditorInput)input).getFile().getLocation().toOSString();
+	            }
+	        }
+	        return null;
+		}
+	}
 }
