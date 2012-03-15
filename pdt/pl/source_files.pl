1bdb515aa4fc9f23f9b887f4815c0dbcfa18673e
Andreas Becker <beckera@cs.uni-bonn.de>
2012-03-15 13:34:26 +0100

    added 'modified since last consult' state for the decoration of consulted files, the icon of the file is yellow instead of green

--- pdt/pl/source_files.pl
+++ pdt/pl/source_files.pl
@@ -1,4 +1,4 @@
-:- module(source_files, [pdt_source_files/1]).
+:- module(source_files, [pdt_source_files/1, pdt_source_file/2]).
 
 
 %% pdt_source_files(String) is nondet.
@@ -10,4 +10,14 @@ pdt_source_files(String) :-
 		Files),
 	ctc_lists:list_2_comma_separated_list(Files, String).
 
-	
+pdt_source_file(File, State) :-
+	source_file(File),
+	(	exists_file(File)
+	->	source_file_property(File, 	modified(ModifiedAtConsult)),
+		time_file(File, ModifiedNow),
+		(	ModifiedNow > ModifiedAtConsult
+		->	State = old
+		;	State = current
+		)
+	;	State = current
+	).
