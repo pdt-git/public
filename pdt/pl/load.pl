ae99518c365ba0a9fbe0b134e2ff8b0524ea8df2
Guenter Kniesel <gk@cs.uni-bonn.de>
2012-05-06 21:08:57 +0200

    Adapted pdt/pl/load.pl file to changed paths after moving files around.

--- pdt/pl/load.pl
+++ pdt/pl/load.pl
@@ -6,27 +6,26 @@
 %
 % All rights reserved. This program is  made available under the terms
 % of the Eclipse Public License v1.0 which accompanies this distribution,
-% and is available at http://www.eclipse.org/legal/epl-v10.html 
+% and is available at http://www.eclipse.org/legal/epl-v10.html
 
-%:- module(pdtplugin,[]). 
+%:- module(pdtplugin,[]).
 
-:- reexport(split_file_path).
-:- reexport(pdt_search).                % find definitions, declarations (and autocompletion)
-:- consult(lgt/loader.pl')).% Search, outline, etc. for Logtalk:
+:- use_module(split_file_path).
+:- use_module(pdt_search).                       % Search, outline and autocompletion
+:- consult('lgt/loader.pl').                   % Search, outline etc. for Logtalk
 
-:- reexport(pdt_editor_reload).         % pdt_reload/1 & friends
-:- reexport(pdt_editor_manual_entry).   % for quick outline (find_pred/5)
-%:-reexport(pdt_editor_highlighting).   % no noeed to include here, used (directly) only in PLScanner.java
+:- use_module(editor/pdt_editor_reload).         % pdt_reload/1 & friends
+:- use_module(editor/pdt_editor_manual_entry).   % for quick outline (find_pred/5)
+%:-use_module(editor/pdt_editor_highlighting).   % no noeed to include here, used (directly) only in PLScanner.java
 
-
-:- reexport(pdt_editor_edit_hook).    % Contribution to SWIPL's prolog_edit:edit_source(Location)
-:- reexport(pdt_editor_breakpoints).  % API for pdt/.../PDTBreakpointHandler.java
-:- reexport(pdt_entry_points).        % API for pdt/... 
+:- use_module(editor/pdt_editor_edit_hook).    % Contribution to SWIPL's prolog_edit:edit_source(Location)
+:- use_module(editor/pdt_editor_breakpoints).  % API for pdt/.../PDTBreakpointHandler.java
+:- use_module(pdt_entry_points).        % API for pdt/...
                                       % and for pdt.console/.../GenerateLoadFileWizard.java
-:- reexport(source_files).            % API for PDTConsultDecoratorContributor.java
+:- use_module(source_files).            % API for PDTConsultDecoratorContributor.java
 :- consult(smell_api).
 
 % TODO: Load here or just in pdt_search.pl???
-:- use_module(pdt_runtime_builder_analyzer(properties)).
-:- use_module(pdt_runtime_builder_analyzer('metafile_referencer.pl')).
-:- use_module(pdt_prolog_library(utils4modules)).
+%:- use_module(pdt_runtime_builder_analyzer(properties)).
+%:- use_module(pdt_runtime_builder_analyzer('metafile_referencer.pl')).
+%:- use_module(pdt_prolog_library(utils4modules)).
