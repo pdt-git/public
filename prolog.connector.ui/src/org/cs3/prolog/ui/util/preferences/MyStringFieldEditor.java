a44f82e8b0f93857ef57a25ab045de292d1cea46
Andreas Becker <beckera@cs.uni-bonn.de>
2012-06-07 19:01:37 +0200

    Added further FieldEditor adapters.

---
+++ prolog.connector.ui/src/org/cs3/prolog/ui/util/preferences/MyStringFieldEditor.java
@@ -0,0 +1,35 @@
+package org.cs3.prolog.ui.util.preferences;
+
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Composite;
+
+public class MyStringFieldEditor extends StringFieldEditor implements FieldEditorForStructuredPreferencePage {
+
+    private Composite parent;
+
+	public MyStringFieldEditor(String name, String labelText, int width, int strategy, Composite parent) {
+        super(name, labelText, width, strategy, parent);
+        this.parent = parent;
+    }
+
+    public MyStringFieldEditor(String name, String labelText, int width, Composite parent) {
+        super(name, labelText, width, parent);
+        this.parent = parent;
+    }
+
+    public MyStringFieldEditor(String name, String labelText, Composite parent) {
+    	super(name, labelText, parent);
+    	this.parent = parent;
+    }
+    
+	@Override
+	public void adjustColumns(int numColumns) {
+		adjustForNumColumns(numColumns);
+	}
+
+	@Override
+	public Composite getParent() {
+		return parent;
+	}
+
+}
