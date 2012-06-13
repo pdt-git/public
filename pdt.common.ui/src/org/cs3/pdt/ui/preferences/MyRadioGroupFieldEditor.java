4dd1626377b06005c3e3e1c062f1be84cc737bbf
Guenter Kniesel <gk@cs.uni-bonn.de>
2012-06-13 14:43:10 +0200

    Changes picked from master

---
+++ pdt.common.ui/src/org/cs3/pdt/ui/preferences/MyRadioGroupFieldEditor.java
@@ -0,0 +1,25 @@
+package org.cs3.pdt.ui.preferences;
+
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
+import org.eclipse.swt.widgets.Composite;
+
+public class MyRadioGroupFieldEditor extends RadioGroupFieldEditor implements FieldEditorForStructuredPreferencePage {
+	
+	public MyRadioGroupFieldEditor(String name, String labelText, int numColumns, String[][] labelAndValues, Composite parent) {
+		super(name,labelText,numColumns,labelAndValues,parent,true);
+    	this.parent = parent;
+    }
+    
+	@Override
+	public void adjustColumns(int numColumns) {
+		adjustForNumColumns(numColumns);
+	}
+
+    private Composite parent;
+	
+	@Override
+	public Composite getParent() {
+		return parent;
+	}
+	
+}
