8f23f2afe526cde90ff1e5e42295ed84f9e8f1e0
Fabian Noth <noth@iai.uni-bonn.de>
2012-07-08 15:43:28 +0200

    added PrologSourceLocation class

---
+++ pdt.core/src/org/cs3/pl/metadata/PrologSourceLocation.java
@@ -0,0 +1,22 @@
+package org.cs3.pl.metadata;
+
+public class PrologSourceLocation {
+      private String filePath;
+      private int line;
+      
+      
+      public PrologSourceLocation(String filePath, int line){
+    	  this.filePath=filePath;
+    	  this.line=line;
+      }
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public int getLine() {
+		return line;
+	}
+
+
+}
