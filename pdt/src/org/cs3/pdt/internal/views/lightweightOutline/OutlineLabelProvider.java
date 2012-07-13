6b3a746d82b42d8846c323ee90b44b28e35692d4
Andreas Becker <beckera@cs.uni-bonn.de>
2012-07-13 17:40:14 +0200

    Changed icon for occurences in the prolog outline.

--- pdt/src/org/cs3/pdt/internal/views/lightweightOutline/OutlineLabelProvider.java
+++ pdt/src/org/cs3/pdt/internal/views/lightweightOutline/OutlineLabelProvider.java
@@ -2,15 +2,14 @@ package org.cs3.pdt.internal.views.lightweightOutline;
 
 import org.cs3.pdt.internal.ImageRepository;
 import org.cs3.pdt.internal.structureElements.OutlineModuleElement;
-import org.cs3.pdt.internal.structureElements.PrologTreeElement;
 import org.cs3.pdt.internal.structureElements.PredicateOccuranceElement;
+import org.cs3.pdt.internal.structureElements.PrologTreeElement;
 import org.cs3.pdt.metadata.Predicate;
 import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 
 class OutlineLabelProvider extends LabelProvider implements IColorProvider/*, IStyledLabelProvider*/ {
@@ -42,14 +41,9 @@ class OutlineLabelProvider extends LabelProvider implements IColorProvider/*, IS
 				return ImageRepository.getImage(ImageRepository.PACKAGE);
 		}
 		if (element instanceof PredicateOccuranceElement) {
-			ISharedImages sharedImagaes = PlatformUI.getWorkbench().getSharedImages();
-			return setCategoryImage(sharedImagaes);
+			return ImageRepository.getImage(ImageRepository.SEARCH_MATCH);
 		}
 		return null;
-	}
-
-	private Image setCategoryImage(ISharedImages sharedImagaes) {
-		return sharedImagaes.getImage(ISharedImages.IMG_DEF_VIEW);
 	}
 
 	@Override
