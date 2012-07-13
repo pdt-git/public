b9fe91239d07718f2dd29391d0aca75924084503
Andreas Becker <beckera@cs.uni-bonn.de>
2012-07-13 18:10:28 +0200

    Added empty OutlineFileElement.

---
+++ pdt/src/org/cs3/pdt/internal/structureElements/OutlineFileElement.java
@@ -0,0 +1,23 @@
+package org.cs3.pdt.internal.structureElements;
+
+public class OutlineFileElement implements PrologTreeElement {
+
+	@Override
+	public boolean hasChildren() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] getChildren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
