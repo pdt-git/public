cd1e7809d9081d867851ef160c330fe5d78943a6
Fabian Noth <noth@iai.uni-bonn.de>
2012-07-27 17:27:12 +0200

    updated license header

--- CreateLicenseHeader/src/checklicense/utils/Utils.java
+++ CreateLicenseHeader/src/checklicense/utils/Utils.java
@@ -4,7 +4,7 @@
  * Author: Fabian Noth
  * WWW: http://sewiki.iai.uni-bonn.de/research/pdt/start
  * Mail: pdt@lists.iai.uni-bonn.de
- * Copyright (C): 2012, CS Dept. III, University of Bonn
+ * Copyright (C): 2004-2012, CS Dept. III, University of Bonn
  * 
  * All rights reserved. This program is  made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution,
