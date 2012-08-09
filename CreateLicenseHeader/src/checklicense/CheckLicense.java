a04c5054c57d0019ac8b7189c81750b44f4834c3
Fabian Noth <noth@iai.uni-bonn.de>
2012-08-09 18:08:48 +0200

    added possiibility to set/add authors, added filter for files with authors

--- CreateLicenseHeader/src/checklicense/CheckLicense.java
+++ CreateLicenseHeader/src/checklicense/CheckLicense.java
@@ -24,15 +24,38 @@ public class CheckLicense {
 
 	public static final String NO_HEADER = "no header";
 	public static final String NO_AUTHOR = "no author";
+	public static final String HEADER_WITH_AUTHOR = "header with author";
 	
 	private String licenseTop;
-	private String license;
-	private String licenseComplete;
+	private String licenseBottom;
+	private String licenseNoAuthor;
+	private String authorLine;
+
+	private final HashMap<String, String> authorShortcuts = new HashMap<>();
 	
 	public CheckLicense() {
-		licenseTop = Utils.readFileToString(new File("data/license_top.txt"));
-		license = Utils.readFileToString(new File("data/license.txt"));
-		licenseComplete = licenseTop + license;
+		String license = Utils.readFileToString(new File("data\\license.txt"));
+		
+		licenseNoAuthor = license.replaceAll("\\$\\$\\$.*\\$\\$\\$", "");
+		licenseNoAuthor = licenseNoAuthor.replace("\n\n", "\n");
+		
+		licenseTop = license.substring(0, license.indexOf("$$$"));
+		licenseBottom = license.substring(license.lastIndexOf("$$$")+4);
+		
+		authorLine = license.substring(license.indexOf("$$$") + 3, license.lastIndexOf(" $$$"));
+		
+		createAuthorList();
+	}
+
+	public void createAuthorList() {
+		String authors = Utils.readFileToString(new File("data/authors.txt"));
+		String[] authorArray = authors.split("\n");
+		for (String author : authorArray) {
+			String[] split = author.split("\\|");
+			if (split.length == 2) {
+				authorShortcuts.put(split[0], split[1]);
+			}
+		}
 	}
 	
 	public HashMap<String, ArrayList<String>> checkFiles(String pathName) {
@@ -43,11 +66,13 @@ public class CheckLicense {
 			dummyList.add("no valid directory");
 			result.put(NO_HEADER, dummyList);
 			result.put(NO_AUTHOR, dummyList);
+			result.put(HEADER_WITH_AUTHOR, dummyList);
 			return result;
 		}
 		final HashMap<String, ArrayList<String>> result = new HashMap<>();
 		result.put(NO_HEADER, new ArrayList<String>());
 		result.put(NO_AUTHOR, new ArrayList<String>());
+		result.put(HEADER_WITH_AUTHOR, new ArrayList<String>());
 		checkFiles(root, result);
 		return result;
 	}
@@ -67,53 +92,120 @@ public class CheckLicense {
 	private void checkFile(File f, HashMap<String, ArrayList<String>> list) {
 		String oldCode = Utils.readFileToString(f);
 		
-		if (!hasHeader(oldCode)) {
+		if (hasNoHeader(oldCode)) {
 			list.get(NO_HEADER).add(f.getPath());
-		} else if(hasHeaderWithoutAuthor(oldCode)) {
+		} else if (hasHeaderWithoutAuthor(oldCode)) {
 			list.get(NO_AUTHOR).add(f.getPath());
+		} else if (hasHeaderWithAuthor(oldCode)) {
+			list.get(HEADER_WITH_AUTHOR).add(f.getPath());
 		}
 	}
 
+	public boolean hasNoHeader(String code) {
+		return !code.startsWith(licenseTop) || !code.contains(licenseBottom);
+	}
+	
 	public boolean hasHeaderWithoutAuthor(String oldCode) {
-		return oldCode.startsWith(licenseComplete);
+		return oldCode.startsWith(licenseNoAuthor);
 	}
 
-	public boolean hasHeader(String oldCode) {
-		return oldCode.startsWith(licenseTop) && oldCode.contains(license);
+	public boolean hasHeaderWithAuthor(String oldCode) {
+		return oldCode.startsWith(licenseTop + authorLine) && oldCode.contains(licenseBottom);
 	}
 	
 	private boolean hasValidFileExtension(File f) {
 		String filename = f.getName().toLowerCase();
-		return filename.endsWith("pl") || filename.endsWith("java");
+		return filename.endsWith("pl") || filename.endsWith("pro") || filename.endsWith("java");
 	}
-
+	
 	public void addHeaderToFile(String filePath, String author) {
 		File f = new File(filePath);
 		String oldContent = Utils.readFileToString(f);
 		
-		if (hasHeader(oldContent) && !hasHeaderWithoutAuthor(oldContent)) {
-			// there is already a header with an author, do nothing
-			return;
-		}
-		
-		if (hasHeader(oldContent) && author.isEmpty()) {
-			// there already is a simple header and no author, do nothing
-			return;
-		}
 		
 		StringBuffer newContent = new StringBuffer();
 		newContent.append(licenseTop);
 		if (!author.isEmpty()) {
-			newContent.append(" * Author: " + author + "\n");
+			newContent.append(" * Author: ");
+			newContent.append(createAuthorString(author));
+			newContent.append("\n");
 		}
-		newContent.append(license);
+		newContent.append(licenseBottom);
 		newContent.append("\n");
+
+		int licenseIndex = oldContent.indexOf(licenseBottom);
 		// remove license if it is already there
-		// this is the case if we just want to add an author
-		newContent.append(oldContent.replace(licenseComplete + "\n", ""));
+		if (licenseIndex > -1) {
+			String filteredContent = oldContent.substring(licenseIndex + licenseBottom.length() + 1);
+			newContent.append(filteredContent);
+		} else {
+			newContent.append(oldContent);
+		}
+		
 		Utils.writeToFile(f, newContent.toString());
 	}
-	
-	
 
+	private String createAuthorString(String author) {
+		StringBuffer result = new StringBuffer();
+		
+		String[] splitted = author.split(",");
+		
+		for(int i=0; i<splitted.length; i++) {
+			String name = splitted[i].trim();
+			if (i > 0) {
+				result.append(", ");
+			}
+			if (authorShortcuts.containsKey(name.toLowerCase())) {
+				result.append(authorShortcuts.get(name.toLowerCase()));
+			} else {
+				result.append(name);
+			}
+			
+		}
+		
+		return result.toString();
+	}
+
+	public void addAuthorToFile(String filePath, String newAuthors) {
+		File f = new File(filePath);
+		String oldContent = Utils.readFileToString(f);
+		
+		if (hasNoHeader(oldContent) || hasHeaderWithoutAuthor(oldContent)) {
+			// if there are no authors, adding and seting an author is the same
+			addHeaderToFile(filePath, newAuthors);
+		} else {
+			// parse authors from file
+			int authorLineIndex = oldContent.indexOf(authorLine);
+			String oldAuthors = oldContent.substring(authorLineIndex + authorLine.length(), oldContent.indexOf("\n", authorLineIndex));
+			String authors = oldAuthors + ", " + newAuthors;
+			String[] splittedAuthors = authors.split(",");
+			ArrayList<String> authorsList = new ArrayList<>();
+			// add new authors (if they are not in the header yet)
+			for (String name : splittedAuthors) {
+				name = name.trim();
+				if (authorShortcuts.containsKey(name)) {
+					name = authorShortcuts.get(name);
+				}
+				
+				if (!authorsList.contains(name)) {
+					authorsList.add(name);
+				}
+			}
+
+			// save file
+			StringBuffer finalAuthorList = new StringBuffer();
+			boolean first = true;
+			for (String s : authorsList) {
+				if (first) {
+					first = false;
+				} else {
+					finalAuthorList.append(", ");
+				}
+				finalAuthorList.append(s);
+			}
+			addHeaderToFile(filePath, finalAuthorList.toString());
+			
+		}
+	}
+	
 }
