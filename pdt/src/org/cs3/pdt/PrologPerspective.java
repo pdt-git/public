70ddb7e0aa96916a7cbe4ccbbd9a933a6ce8ad91
Andreas Becker <beckera@cs.uni-bonn.de>
2012-08-17 14:45:39 +0200

    Updated Prolog Perspective: - readded the context view - added problems and search view

--- pdt/src/org/cs3/pdt/PrologPerspective.java
+++ pdt/src/org/cs3/pdt/PrologPerspective.java
@@ -15,10 +15,14 @@ package org.cs3.pdt;
 
 import org.cs3.pdt.console.internal.views.PrologConsoleView;
 import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.search.ui.NewSearchUI;
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class PrologPerspective implements IPerspectiveFactory {
+	
+	public static final String CONSOLE_FOLDER = "prolog.perspective.console.folder";
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
@@ -34,14 +38,18 @@ public class PrologPerspective implements IPerspectiveFactory {
 	public void defineLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 
-		layout.addView(JavaUI.ID_PACKAGES, IPageLayout.LEFT, (float) 0.2, editorArea);
-		layout.addView(PrologConsoleView.HOOK_ID, IPageLayout.BOTTOM, (float) 0.65, editorArea);
-		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, (float) 0.8, editorArea);
+//		layout.addView(JavaUI.ID_PACKAGES, IPageLayout.LEFT, (float) 0.2, editorArea);
+//		layout.addView(PrologConsoleView.HOOK_ID, IPageLayout.BOTTOM, (float) 0.65, editorArea);
+//		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, (float) 0.8, editorArea);
 		
 //		layout.addView(PrologConsoleView.HOOK_ID, IPageLayout.BOTTOM, (float) 0.65, editorArea);
-//		layout.addView("pdt.view.focus", IPageLayout.RIGHT, (float) 0.5, PrologConsoleView.HOOK_ID);
-//		layout.addView(JavaUI.ID_PACKAGES, IPageLayout.LEFT, (float) 0.2, editorArea);
-//		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, (float) 0.8, editorArea);
+		IFolderLayout bottomFolder = layout.createFolder(CONSOLE_FOLDER, IPageLayout.BOTTOM, 0.65f, editorArea);
+		bottomFolder.addView(PrologConsoleView.HOOK_ID);
+		bottomFolder.addView(IPageLayout.ID_PROBLEM_VIEW);
+		bottomFolder.addView(NewSearchUI.SEARCH_VIEW_ID);
+		layout.addView("pdt.view.focus", IPageLayout.RIGHT, 0.5f, CONSOLE_FOLDER);
+		layout.addView(JavaUI.ID_PACKAGES, IPageLayout.LEFT, 0.2f, editorArea);
+		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, 0.8f, editorArea);
 	}
 	
 }
