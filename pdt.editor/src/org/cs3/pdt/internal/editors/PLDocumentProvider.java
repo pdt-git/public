f69ebba50777999fbad6ba768732fbe7ed94fb77
Andreas Becker <beckera@cs.uni-bonn.de>
2012-08-27 15:59:44 +0200

    Modified PLDocumentProvider. This should fix the wrong dirty state of the prolog editor after undoing all operations.

--- pdt.editor/src/org/cs3/pdt/internal/editors/PLDocumentProvider.java
+++ pdt.editor/src/org/cs3/pdt/internal/editors/PLDocumentProvider.java
@@ -14,30 +14,55 @@
 
 package org.cs3.pdt.internal.editors;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
-import org.eclipse.ui.editors.text.FileDocumentProvider;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 
-public class PLDocumentProvider extends FileDocumentProvider {
+public class PLDocumentProvider extends TextFileDocumentProvider {
+
+//	@Override
+//	protected IDocument createDocument(Object element) throws CoreException {
+//		IDocument document = super.createDocument(element);
+//		if (document != null) {
+//			IDocumentPartitioner partitioner =
+//				new FastPartitioner(
+//					new PLPartitionScanner(),
+//					new String[] {
+//						PLPartitionScanner.PL_MULTI_COMMENT,
+//						PLPartitionScanner.PL_COMMENT,
+//						PLPartitionScanner.PL_SINGLE_QUOTED_STRING,
+//						PLPartitionScanner.PL_DOUBLE_QUOTED_STRING});
+//			partitioner.connect(document);
+//			document.setDocumentPartitioner(partitioner);
+//		}
+//		return document;
+//	}
+	
+	private IDocumentPartitioner partitioner;
 
 	@Override
-	protected IDocument createDocument(Object element) throws CoreException {
-		IDocument document = super.createDocument(element);
-		if (document != null) {
-			IDocumentPartitioner partitioner =
-				new FastPartitioner(
+	public IDocument getDocument(Object element) {
+		IDocument document = super.getDocument(element);
+		IDocumentPartitioner partitioner2 = getPartitioner();
+		if (document.getDocumentPartitioner() != partitioner2) {
+			partitioner2.connect(document);
+			document.setDocumentPartitioner(partitioner2);
+		}
+		return document;
+	}
+	
+	private IDocumentPartitioner getPartitioner() {
+		if (partitioner == null) {
+			partitioner = new FastPartitioner(
 					new PLPartitionScanner(),
 					new String[] {
 						PLPartitionScanner.PL_MULTI_COMMENT,
 						PLPartitionScanner.PL_COMMENT,
 						PLPartitionScanner.PL_SINGLE_QUOTED_STRING,
 						PLPartitionScanner.PL_DOUBLE_QUOTED_STRING});
-			partitioner.connect(document);
-			document.setDocumentPartitioner(partitioner);
 		}
-		return document;
+		return partitioner;
 	}
 }
 
