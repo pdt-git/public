a7b826781313275657746f3e011546027ae84119
Andreas Becker <beckera@cs.uni-bonn.de>
2012-08-29 15:40:30 +0200

    Reset PLDocumentProvider.

--- pdt.editor/src/org/cs3/pdt/internal/editors/PLDocumentProvider.java
+++ pdt.editor/src/org/cs3/pdt/internal/editors/PLDocumentProvider.java
@@ -14,56 +14,32 @@
 
 package org.cs3.pdt.internal.editors;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
-import org.eclipse.ui.editors.text.TextFileDocumentProvider;
+import org.eclipse.ui.editors.text.FileDocumentProvider;
 
-public class PLDocumentProvider extends TextFileDocumentProvider {
-
-//	@Override
-//	protected IDocument createDocument(Object element) throws CoreException {
-//		IDocument document = super.createDocument(element);
-//		if (document != null) {
-//			IDocumentPartitioner partitioner =
-//				new FastPartitioner(
-//					new PLPartitionScanner(),
-//					new String[] {
-//						PLPartitionScanner.PL_MULTI_COMMENT,
-//						PLPartitionScanner.PL_COMMENT,
-//						PLPartitionScanner.PL_SINGLE_QUOTED_STRING,
-//						PLPartitionScanner.PL_DOUBLE_QUOTED_STRING});
-//			partitioner.connect(document);
-//			document.setDocumentPartitioner(partitioner);
-//		}
-//		return document;
-//	}
-	
-	private IDocumentPartitioner partitioner;
+public class PLDocumentProvider extends FileDocumentProvider {
 
 	@Override
-	public IDocument getDocument(Object element) {
-		IDocument document = super.getDocument(element);
-		IDocumentPartitioner partitioner2 = getPartitioner();
-		if (document.getDocumentPartitioner() != partitioner2) {
-			partitioner2.connect(document);
-			document.setDocumentPartitioner(partitioner2);
-		}
-		return document;
-	}
-	
-	private IDocumentPartitioner getPartitioner() {
-		if (partitioner == null) {
-			partitioner = new FastPartitioner(
+	protected IDocument createDocument(Object element) throws CoreException {
+		IDocument document = super.createDocument(element);
+		if (document != null) {
+			IDocumentPartitioner partitioner =
+				new FastPartitioner(
 					new PLPartitionScanner(),
 					new String[] {
 						PLPartitionScanner.PL_MULTI_COMMENT,
 						PLPartitionScanner.PL_COMMENT,
 						PLPartitionScanner.PL_SINGLE_QUOTED_STRING,
 						PLPartitionScanner.PL_DOUBLE_QUOTED_STRING});
+			partitioner.connect(document);
+			document.setDocumentPartitioner(partitioner);
 		}
-		return partitioner;
+		return document;
 	}
+	
 }
 
 
