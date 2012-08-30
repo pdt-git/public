3bcc756f866961e53284f0573de6248831a73c9a
Andreas Becker <beckera@cs.uni-bonn.de>
2012-08-30 12:00:56 +0200

    Updated tutorial project.

--- PDT Tutorial/search/search_demo.pl
+++ PDT Tutorial/search/search_demo.pl
@@ -5,10 +5,16 @@
 % - search_demo (local): the clause below is found and this result is "local" because it is started from here
 % - search_demo_sub (sub): this module imports search_demo and redefines abc/1, therefore this result is marked as sub
 % - other_module (invisible): this module also defines a predicate abc/1, but this is not related to search_demo:abc/1
+%
+% You can also search globally for definitions of abc/1 (in the menu Search->Search... or CTRL+H from the editor).
+% In this case you should find the same three results, but these have no visibility since the global search does not 
+% have any context. 
 abc(0).
 
 % Select abc and call "Find References" from the context menu.
-% You should find both clauses of p/1 as result because both clauses contain a term p(_).
+% You should find both clauses of p/1 as result because both clauses contain a term abc(_).
+% 
+% You can also start a global search for abc/1 which gives the same results.
 p(X) :-
 	abc(X).
 
