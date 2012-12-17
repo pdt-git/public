248ddd6fcd21d510743ad44f520c54e388a65ce1
Andreas Becker <beckera@cs.uni-bonn.de>
2012-12-17 13:05:47 +0100

    Modified query in comment.

--- prolog.library/pl/metainference/load.pl
+++ prolog.library/pl/metainference/load.pl
@@ -5,5 +5,5 @@
 /* 
 declared_in_module(Module, Name, Arity, Module), functor(Head, Name, Arity), infer_meta(Module:Head, MetaSpec), infer_meta_predicate(Module:Head, MetaSpec2), MetaSpec \== MetaSpec2.
 
-pmi:inferred_meta_pred2(_, M, MetaSpec), predicate_property(M:MetaSpec, file(File)), atom_concat('l:/work/beckera/git-repos/jtransformer', _, File), \+ predicate_property(M:MetaSpec, meta_predicate(_)).
+pmi:inferred_meta_pred(_, M, MetaSpec), predicate_property(M:MetaSpec, file(File)), atom_concat('l:/work/beckera/git-repos/jtransformer', _, File), \+ predicate_property(M:MetaSpec, meta_predicate(_)).
 */
