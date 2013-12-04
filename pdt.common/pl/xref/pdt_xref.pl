3b0ce443fdc474d4b684f3214e46dbe769d4f47f
Andreas Becker <beckera@iai.uni-bonn.de>
2013-12-04 12:01:21 +0100

    Fixed wrong variable name.

--- pdt.common/pl/xref/pdt_xref.pl
+++ pdt.common/pl/xref/pdt_xref.pl
@@ -61,7 +61,7 @@ assert_result(IsAlias, QGoal-clause_term_position(Ref, TermPosition)) :-
 assert_result(_,_). 
 
 %% find_reference_to(+Functor,+Arity,DefFile, DefModule,+ExactMatch,RefModule,RefName,RefArity,RefFile,Position,NthClause,Kind,?PropertyList)
-find_reference_to(Term, _File, Line, ExactMatch, RefModule, RefName, RefArity, RefFile, Position, PropertyList) :-
+find_reference_to(Term, _File, _FileLine, ExactMatch, RefModule, RefName, RefArity, RefFile, Position, PropertyList) :-
 	(	Term = predicate(SearchMod, _, Functor, _, Arity)
 	;	Term = module(SearchMod)
 	),
