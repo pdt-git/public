293bdb47251c6d3817f2c5e474863515a60fb2a4
Andreas Becker <beckera@cs.uni-bonn.de>
2014-05-16 13:04:01 +0200

    Moved pdt.plunit related actions from pdt.console and pdt.editor to pdt.plunit. This removes the remaining dependencies to the jdt.

--- pdt.plunit/src/org/cs3/plunit/actions/OpenJUnitWrapperAction.java
+++ pdt.plunit/src/org/cs3/plunit/actions/OpenJUnitWrapperAction.java
@@ -11,11 +11,10 @@
  * 
  ****************************************************************************/
 
-package org.cs3.pdt.console.internal.actions;
+package org.cs3.plunit.actions;
 
-import org.cs3.pdt.console.ConsoleModel;
-import org.cs3.pdt.console.PrologConsole;
-import org.cs3.pdt.console.PrologConsolePlugin;
+import org.cs3.pdt.common.PDTCommonUtil;
+import org.cs3.prolog.common.logging.Debug;
 import org.eclipse.jdt.junit.model.ITestCaseElement;
 import org.eclipse.jdt.junit.model.ITestSuiteElement;
 import org.eclipse.jface.action.IAction;
@@ -23,26 +22,22 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ITreeSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
 
-public class RunUnitTestOnConsoleAction  implements IObjectActionDelegate {
-	
+public class OpenJUnitWrapperAction implements IObjectActionDelegate {
 
-	private String unitName;
-	private String testName;
+	private String fileName;
+	private int lineNumber;
 
 
 
 	@Override
 	public void run(IAction action) {
-//		String[] fileNum= args[1].split(";");
-//		fileName = fileNum[0];
-		PrologConsole console = PrologConsolePlugin.getDefault().getPrologConsoleService().getActivePrologConsole();
-		ConsoleModel model = console.getModel();
-		model.setLineBuffer(" ");
-		model.commitLineBuffer();
-		model.setLineBuffer("run_tests(" + unitName +":"+ testName + ").");
-		model.commitLineBuffer();
-
+		try {
+			PDTCommonUtil.selectInEditor(lineNumber, fileName, false);
+		} catch (PartInitException e) {
+			Debug.report(e);
+		}
 	}
 
 	@Override
@@ -54,8 +49,25 @@ public class RunUnitTestOnConsoleAction  implements IObjectActionDelegate {
 		ITreeSelection tree = (ITreeSelection)selection;
 		
 		ITestCaseElement tc = (ITestCaseElement)tree.getFirstElement();
-		unitName = ((ITestSuiteElement)tc.getParentContainer()).getSuiteTypeName();
-		testName = tc.getTestMethodName().substring(2,tc.getTestMethodName().length()-1).split(":")[0];
+		fileName = ((ITestSuiteElement)tc.getParentContainer().getParentContainer()).getSuiteTypeName();
+		String[] args= tc.getTestMethodName().substring(6,tc.getTestMethodName().length()-1).split(":");
+//		String[] fileNum= args[1].split(";");
+//		fileName = fileNum[0];
+		if(tc.getFailureTrace()!=null && tc.getFailureTrace().getTrace().startsWith("java.lang.AssertionError: Failed assertion in line ")){
+			String trace = tc.getFailureTrace().getTrace();
+			trace = trace.substring("java.lang.AssertionError: Failed Assertion in Line ".length(),trace.length());
+			StringBuffer buf = new StringBuffer();
+			int i = 0;
+			while(trace.charAt(i) >='0' && trace.charAt(i) <='9' ){
+				buf.append(trace.charAt(i));
+				i++;
+			}
+			lineNumber = Integer.parseInt(buf.toString());
+						
+		} else
+
+		lineNumber = Integer.parseInt(args[1]);
+		
 	}
 
 	@Override
