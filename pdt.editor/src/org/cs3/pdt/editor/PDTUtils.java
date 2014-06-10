b35485020f8b1d05c1bdbd76291bd3b09f423e48
Fabian Noth <fabian.noth@gmx.de>
2014-06-10 14:26:39 +0200

    renamed packages in pdt editor

---
+++ pdt.editor/src/org/cs3/pdt/editor/PDTUtils.java
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * This file is part of the Prolog Development Tool (PDT)
+ * 
+ * Author: Lukas Degener (among others)
+ * WWW: http://sewiki.iai.uni-bonn.de/research/pdt/start
+ * Mail: pdt@lists.iai.uni-bonn.de
+ * Copyright (C): 2004-2012, CS Dept. III, University of Bonn
+ * 
+ * All rights reserved. This program is  made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ ****************************************************************************/
+
+package org.cs3.pdt.editor;
+
+
+import org.cs3.prolog.common.Util;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+public final class PDTUtils {
+
+	public static String getPrologFileName(IFile file) {
+		String enclFile = file.getRawLocation().toPortableString();
+		if (Util.isWindows()) {
+			enclFile = enclFile.toLowerCase();
+		}
+
+		IPath filepath = new Path(enclFile);
+		return Util.prologFileName(filepath.toFile());
+	}
+
+}
+
+
---
+++ pdt.editor/src/org/cs3/pdt/editor/internal/editors/EditorConsultListener.java
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * This file is part of the Prolog Development Tool (PDT)
+ * 
+ * WWW: http://sewiki.iai.uni-bonn.de/research/pdt/start
+ * Mail: pdt@lists.iai.uni-bonn.de
+ * Copyright (C): 2004-2012, CS Dept. III, University of Bonn
+ * 
+ * All rights reserved. This program is  made available under the terms
+ * of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ ****************************************************************************/
+
+package org.cs3.pdt.editor.internal.editors;
+
+import java.util.List;
+
+import org.cs3.prolog.pif.PrologInterface;
+import org.cs3.prolog.pif.PrologInterfaceException;
+import org.cs3.prolog.pif.service.ConsultListener;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class EditorConsultListener implements ConsultListener {
+
+	@Override
+	public void beforeConsult(PrologInterface pif, List<IFile> files, IProgressMonitor monitor) throws PrologInterfaceException {
+		monitor.beginTask("", 1);
+		monitor.done();
+	}
+
+	@Override
+	public void afterConsult(PrologInterface pif, List<IFile> files, List<String> allConsultedFiles, IProgressMonitor monitor) throws PrologInterfaceException {
+		PLMarkerUtils.addMarkers(pif, allConsultedFiles, monitor);
+	}
+	
+}
+
+
