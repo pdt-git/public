b8e964fa69aa485fb9490f5f3eec2eb4dc1bb0f5
Fabian Noth <fabian.noth@gmx.de>
2014-07-17 15:25:59 +0200

    HelpDialog Update

--- pdt.graphicalviews/src/org/cs3/pdt/graphicalviews/focusview/HelpDialog.java
+++ pdt.graphicalviews/src/org/cs3/pdt/graphicalviews/focusview/HelpDialog.java
@@ -31,30 +31,54 @@ public class HelpDialog extends ApplicationWindow {
 	@Override
 	protected Control createContents(Composite parent) {
 
-		Composite compi = new Composite(parent, SWT.NONE);
-		compi.setLayout(new GridLayout(1,false));
-
-		Composite masterComposite = new Composite(compi , SWT.NONE);
-		masterComposite.setLayout(new GridLayout(1, false));
+		Composite masterComposite = new Composite(parent, SWT.NONE);
+		masterComposite.setLayout(new GridLayout(1,true));
+		
+//		Composite masterComposite = new Composite(compi , SWT.NONE);
+//		masterComposite.setLayout(new GridLayout(1, false));
 		/*
 		 * Icon Legend
 		 */
-		Group nodeGroup = createGroup(masterComposite, "Predicates", 4);
-
-		addLegendItem(nodeGroup, "default.png", "Normal");
-		addLegendItem(nodeGroup, "pred_dynamic.png", "Dynamic");
-		addLegendItem(nodeGroup, "pred_declared_meta.png", "Declared meta");
-		addLegendItem(nodeGroup, "pred_inferred_meta.png", "Inferred meta");
 		
-		Group callGroup = createGroup(masterComposite, "Calls", 2);
-		addLegendItem(callGroup, "call_normal.png", "Normal call");
-		addLegendItem(callGroup, "call_meta.png", "Metacall");
-		addLegendItem(callGroup, "call_db.png", "assert/retract");
+		Group predicateGroup = createGroup(masterComposite, "Predicates", 3);
+		addLegendIcon(predicateGroup, "pred_normal.png");
+		addLegendIcon(predicateGroup, "pred_meta.png");
+		addLegendIcon(predicateGroup, "pred_dynamic.png");
+		addLegendLabel(predicateGroup, "Normal predicate");
+		addLegendLabel(predicateGroup, "Meta predicate");
+		addLegendLabel(predicateGroup, "Dynamic predicate");
+		
+		Group callGroup = createGroup(masterComposite, "Calls", 3);
+		addLegendIcon(callGroup, "call_normal.png");
+		addLegendIcon(callGroup, "call_meta.png");
+		addLegendIcon(callGroup, "call_dynamic.png");
+		addLegendLabel(callGroup, "Normal call");
+		addLegendLabel(callGroup, "Metacall");
+		addLegendLabel(callGroup, "Dynamic call\n(assert/retract)");
 
-		Group visibilityGroup = createGroup(masterComposite, "Visibility", 2);
-		addLegendItem(visibilityGroup, "default.png", "Exported Predicate");
-		addLegendItem(visibilityGroup, "visibility_local.png", "Local Predicate");
-		addLegendItem(visibilityGroup, "visibility_local_uncalled.png", "Local Predicate, not called by any exported predicate");
+//		Group visibilityGroup = createGroup(masterComposite, "Visibility", 2);
+//		addLegendIcon(visibilityGroup, "visibility_exported.png");
+//		addLegendIcon(visibilityGroup, "visibility_local.png");
+//		addLegendLabel(visibilityGroup, "Exported Predicate");
+//		addLegendLabel(visibilityGroup, "Local Predicate");
+//
+//		Group deadCodeGroup = createGroup(masterComposite, "Dead code detection", 2);
+//		addLegendIcon(deadCodeGroup, "pred_normal.png");
+//		addLegendIcon(deadCodeGroup, "pred_dead.png");
+//		addLegendLabel(deadCodeGroup, "Not dead\n(called and/or exported)");
+//		addLegendLabel(deadCodeGroup, "Dead\n(Uncalled and not exported)");
+		
+		
+		
+		// alternative
+		Group visibilityGroup = createGroup(masterComposite, "Visibility and Dead code", 3);
+		addLegendIcon(visibilityGroup, "pred_normal_exported.png");
+		addLegendIcon(visibilityGroup, "pred_normal_local.png");
+		addLegendIcon(visibilityGroup, "pred_dead.png");
+		addLegendLabel(visibilityGroup, "Exported Predicate");
+		addLegendLabel(visibilityGroup, "Local Predicate\n(not exported)");
+		addLegendLabel(visibilityGroup, "Local and uncalled predicate\n(Dead code)");
+
 		
 		Button close = new Button(masterComposite, SWT.PUSH);
 		close.setText("Close");
@@ -65,19 +89,34 @@ public class HelpDialog extends ApplicationWindow {
 			}
 		});
 
-		return compi;
+		return masterComposite;
 	}
 	
 	private Group createGroup(Composite masterComposite, String title, int cols) {
-		Group group = new Group(masterComposite,SWT.SHADOW_ETCHED_IN);
-		GridLayout layout = new GridLayout(cols, false);
-		GridData data = new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL);
+		Group group = new Group(masterComposite,SWT.NONE);
+		GridLayout layout = new GridLayout(cols, true);
+		GridData data = new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL | GridData.VERTICAL_ALIGN_CENTER | GridData.HORIZONTAL_ALIGN_CENTER | GridData.HORIZONTAL_ALIGN_FILL);
 		group.setLayout(layout);
 		group.setLayoutData(data);
 		group.setText(title);
 		return group;
 	}
 
+	private void addLegendIcon(Group legendGroup, String iconName) {
+		Label iconLabel = new Label(legendGroup, SWT.NONE);
+		iconLabel.setImage(ImageRepository.getImageDescriptor("help\\" + iconName).createImage());
+		GridData data = new GridData(GridData.CENTER, GridData.CENTER, true, true);
+		iconLabel.setLayoutData(data);
+	}
+	
+	private void addLegendLabel(Group legendGroup, String label) {
+		Label textLabel = new Label(legendGroup, SWT.NONE);
+		textLabel.setText(label);
+		textLabel.setAlignment(SWT.CENTER);
+		GridData data = new GridData(GridData.CENTER, GridData.BEGINNING, true, true);
+		textLabel.setLayoutData(data);
+	}
+	
 	private void addLegendItem(Group legendGroup, String iconName, String label) {
 		Label iconLabel = new Label(legendGroup, SWT.NONE);
 		iconLabel.setImage(ImageRepository.getImageDescriptor("help\\" + iconName).createImage());
