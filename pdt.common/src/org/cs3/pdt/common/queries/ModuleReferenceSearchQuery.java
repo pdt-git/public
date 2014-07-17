21bb294b257d370af4f2311b885abab269c9e54b
Andreas Becker <beckera@iai.uni-bonn.de>
2014-07-17 11:37:14 +0200

    Added missing scope handling in the module reference search.

--- pdt.common/src/org/cs3/pdt/common/queries/ModuleReferenceSearchQuery.java
+++ pdt.common/src/org/cs3/pdt/common/queries/ModuleReferenceSearchQuery.java
@@ -41,6 +41,7 @@ public class ModuleReferenceSearchQuery extends PDTSearchQuery {
 		String query = bT(PDTCommonPredicates.FIND_MODULE_REFERENCE,
 				getGoal(),
 				Boolean.toString(isExactMatch()),
+				getProjectPath() == null ? "_" : getProjectPath(),
 				"RefFile",
 				"RefLine",
 				"RefModule",
@@ -63,7 +64,12 @@ public class ModuleReferenceSearchQuery extends PDTSearchQuery {
 		if (prop instanceof Vector<?>) {
 			properties = (Vector<String>)prop;
 		}
+		
 		IFile file = findFile(m.get("RefFile").toString());
+		if (getProject() != null && !getProject().equals(file.getProject())) {
+			return null;
+		}
+		
 		String offsetOrLine = m.get("RefLine").toString();
 		
 		PrologMatch match = null;
