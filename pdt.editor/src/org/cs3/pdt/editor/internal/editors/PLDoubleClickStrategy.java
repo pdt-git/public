ede72dcd6304dc805cd539804e8cc5a5f1e89ef8
Andreas Becker <beckera@iai.uni-bonn.de>
2014-09-02 15:12:06 +0200

    Fixed double click selection in the Prolog editor.

--- pdt.editor/src/org/cs3/pdt/editor/internal/editors/PLDoubleClickStrategy.java
+++ pdt.editor/src/org/cs3/pdt/editor/internal/editors/PLDoubleClickStrategy.java
@@ -31,58 +31,9 @@ public class PLDoubleClickStrategy implements ITextDoubleClickStrategy {
 
 		fText = part;
 
-		if (!selectComment(pos)) {
-			selectWord(pos);
-		}
+		selectWord(pos);
 	}
-	protected boolean selectComment(int caretPos) {
-		IDocument doc = fText.getDocument();
-		int startPos, endPos;
-
-		try {
-			int pos = caretPos;
-			char c = ' ';
-
-			while (pos >= 0) {
-				c = doc.getChar(pos);
-				if (c == '\\') {
-					pos -= 2;
-					continue;
-				}
-				if (c == Character.LINE_SEPARATOR || c == '\"')
-					break;
-				--pos;
-			}
-
-			if (c != '\"')
-				return false;
-
-			startPos = pos;
-
-			pos = caretPos;
-			int length = doc.getLength();
-			c = ' ';
-
-			while (pos < length) {
-				c = doc.getChar(pos);
-				if (c == Character.LINE_SEPARATOR || c == '\"')
-					break;
-				++pos;
-			}
-			if (c != '\"')
-				return false;
-
-			endPos = pos;
-
-			int offset = startPos + 1;
-			int len = endPos - offset;
-			fText.setSelectedRange(offset, len);
-			return true;
-		} catch (BadLocationException x) {
-		}
-
-		return false;
-	}
+	
 	protected boolean selectWord(int caretPos) {
 
 		IDocument doc = fText.getDocument();
