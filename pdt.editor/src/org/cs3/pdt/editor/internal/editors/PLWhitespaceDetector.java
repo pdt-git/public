343ff5308e5195d21d773fdab5725b3fd2a49aa6
Andreas Becker <beckera@iai.uni-bonn.de>
2014-12-02 12:06:36 +0100

    Added ^ to the editor white space detector.

--- pdt.editor/src/org/cs3/pdt/editor/internal/editors/PLWhitespaceDetector.java
+++ pdt.editor/src/org/cs3/pdt/editor/internal/editors/PLWhitespaceDetector.java
@@ -22,7 +22,7 @@ public class PLWhitespaceDetector implements IWhitespaceDetector {
 	public boolean isWhitespace(char c) {
 		return (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == ',' || c == ':' ||
 				c == '-' || c == '+' || c == ';' || c == '\\' || c == '/' ||  c == '=' ||
-				c == '(' ||  c == ')' || c == '[' ||  c == ']' ||  c == '|');
+				c == '(' ||  c == ')' || c == '[' ||  c == ']' ||  c == '|' || c == '^');
 	}
 }
 
